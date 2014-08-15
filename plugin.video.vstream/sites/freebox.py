d4bdfc07518d354fac8da842e98f3e6fd8c2e1e7
LordVenom <a_furbyz@hotmail.com>
2014-08-15 14:35:43 +0200

    version 0.1.0

---
+++ plugin.video.vstream/sites/freebox.py
@@ -0,0 +1,77 @@
+#-*- coding: utf-8 -*-
+#Venom.
+from resources.lib.gui.hoster import cHosterGui
+from resources.lib.handler.hosterHandler import cHosterHandler
+from resources.lib.gui.gui import cGui
+from resources.lib.gui.guiElement import cGuiElement
+from resources.lib.handler.inputParameterHandler import cInputParameterHandler
+from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.parser import cParser
+from resources.lib.util import cUtil
+from resources.lib.player import cPlayer
+import re, urllib2
+
+SITE_IDENTIFIER = 'freebox'
+SITE_NAME = 'Freebox'
+
+URL_MAIN = 'http://mafreebox.freebox.fr/freeboxtv/playlist.m3u'
+
+def load():
+    oGui = cGui()
+    
+    list = []
+    chaine = None
+    num=None
+    liste = urllib2.urlopen(URL_MAIN)
+
+    for i in liste:
+        ligne = i.strip()
+        ligne=re.sub(r'#EXTVLCOPT.*',r'',ligne)
+        if ligne:
+            if chaine:
+                list.append( (num,chaine,ligne) )              
+                oOutputParameterHandler = cOutputParameterHandler()
+                oOutputParameterHandler.addParameter('siteUrl', ligne)
+                oOutputParameterHandler.addParameter('siteTitle', str(chaine))
+                __createMenuEntry(oGui, 'play', chaine, 'tv.png', '', '', 0, oOutputParameterHandler)
+                chaine=None
+            if ligne[:7]=="#EXTINF":
+                line=ligne.split(" - ")
+                chaine = line[-1]
+                num=int(line[0].split(",")[-1])
+  
+    oGui.setEndOfDirectory()
+
+def __createMenuEntry(oGui, sFunction, sLabel, sIcon, sThumbnail, sDesc, sMeta='', oOutputParameterHandler = ''):
+    oGuiElement = cGuiElement()
+    oGuiElement.setSiteName(SITE_IDENTIFIER)
+    oGuiElement.setFunction(sFunction)
+    oGuiElement.setTitle(sLabel)
+    oGuiElement.setIcon(sIcon)
+    oGuiElement.setThumbnail(sThumbnail)
+    oGuiElement.setDescription(cUtil().removeHtmlTags(sDesc))
+    oGuiElement.setMeta(sMeta)
+    
+    oGui.addFolder(oGuiElement, oOutputParameterHandler)
+    
+def play():
+    oGui = cGui()
+
+    oInputParameterHandler = cInputParameterHandler()
+    sUrl = oInputParameterHandler.getValue('siteUrl')
+    sTitle = oInputParameterHandler.getValue('siteTitle')
+    
+    oGuiElement = cGuiElement()
+    oGuiElement.setSiteName(SITE_IDENTIFIER)
+    oGuiElement.setTitle(sTitle)
+    oGuiElement.setMediaUrl(sUrl)
+
+    oPlayer = cPlayer()
+    oPlayer.clearPlayList()
+    oPlayer.addItemToPlaylist(oGuiElement)
+    oPlayer.startPlayer()
+    return
+        
+    oGui.setEndOfDirectory()
+    
---
+++ plugin.video.vstream/sites/trash/bundesliga_de.py
@@ -0,0 +1,99 @@
+from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
+from resources.lib.handler.inputParameterHandler import cInputParameterHandler
+from resources.lib.parser import cParser
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.gui.guiElement import cGuiElement
+from resources.lib.gui.gui import cGui
+from resources.lib.player import cPlayer
+
+SITE_IDENTIFIER = 'bundesliga_de'
+SITE_NAME = 'Bundesliga.de'
+
+URL_MAIN = 'http://www.bundesliga.de'
+URL_TV = 'http://www.bundesliga.de/de/bundesliga-tv/navigation.php?area='
+URL_GET_STREAM = 'http://btd-flv-lbwww-01.odmedia.net/bundesliga/'
+
+def load():
+    
+    cGui().showInfo('HS', SITE_NAME, 5)
+    return False
+    
+    oGui = cGui()
+    __createMainMenuItem(oGui, 'Aktuell', 'aktuell')    
+    __createMainMenuItem(oGui, 'Spieltag', 'spieltag')
+    __createMainMenuItem(oGui, 'Stars', 'stars')
+    __createMainMenuItem(oGui, 'Insider', 'insider')
+    __createMainMenuItem(oGui, 'Historie', 'historie')
+    __createMainMenuItem(oGui, 'Vereine', 'vereine')
+    oGui.setEndOfDirectory()
+
+def __createMainMenuItem(oGui, sTitle, sPlaylistId):
+    oGuiElement = cGuiElement()
+    oGuiElement.setSiteName(SITE_IDENTIFIER)
+    oGuiElement.setFunction('listVideos')
+    oGuiElement.setTitle(sTitle)
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('playlistId', sPlaylistId)
+    oGui.addFolder(oGuiElement, oOutputParameterHandler)
+
+def listVideos():
+    oGui = cGui()
+
+    oInputParameterHandler = cInputParameterHandler()
+    if (oInputParameterHandler.exist('playlistId')):
+        sPlaylistId = oInputParameterHandler.getValue('playlistId')
+
+        sUrl = URL_TV + str(sPlaylistId)
+                
+        sPattern = '<div class="zeile">.*?<img src="([^"]+)" id="bild" class="previewImg".*?<a href="javascript:showVideoSnippet\(\'(.*?)\'\).*?<div class="describe">(.*?)</div>'
+      
+        oRequest = cRequestHandler(sUrl)
+        sHtmlContent = oRequest.request()
+       
+        oParser = cParser()
+        aResult = oParser.parse(sHtmlContent, sPattern)
+        
+        if (aResult[0] == True):
+            for aEntry in aResult[1]:
+                oGuiElement = cGuiElement()
+                oGuiElement.setSiteName(SITE_IDENTIFIER)
+                oGuiElement.setFunction('play')
+                oGuiElement.setTitle(aEntry[2])
+                sThumbnail = URL_MAIN + str(aEntry[0])
+                oGuiElement.setThumbnail(sThumbnail)
+
+                sUrl = URL_MAIN + str(aEntry[1])
+                
+                oOutputParameterHandler = cOutputParameterHandler()
+                oOutputParameterHandler.addParameter('sUrl', sUrl)
+                oGui.addFolder(oGuiElement, oOutputParameterHandler)
+
+    oGui.setEndOfDirectory()
+
+def play():
+    oGui = cGui()
+
+    oInputParameterHandler = cInputParameterHandler()
+    if (oInputParameterHandler.exist('sUrl')):
+        sUrl = oInputParameterHandler.getValue('sUrl')
+
+        oRequest = cRequestHandler(sUrl)
+        sHtmlContent = oRequest.request()
+
+        sPattern = 'ake_playlist.php%3Fflv%3D(.*?)%26'
+
+        oParser = cParser()
+        aResult = oParser.parse(sHtmlContent, sPattern)
+        
+        if (aResult[0] == True):
+            sUrl = URL_GET_STREAM + str(aResult[1][0])
+            
+            oGuiElement = cGuiElement()
+            oGuiElement.setSiteName(SITE_IDENTIFIER)
+            oGuiElement.setMediaUrl(sUrl)
+
+            oPlayer = cPlayer()
+            oPlayer.addItemToPlaylist(oGuiElement)
+            oPlayer.startPlayer()
+        return
+    oGui.setEndOfDirectory()
