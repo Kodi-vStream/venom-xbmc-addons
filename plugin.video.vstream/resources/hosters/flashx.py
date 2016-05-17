1a17c342885327f35915a68fffd950e68f7f1282
TmpName <gihubtmp@yopmail.com>
2016-05-17 22:06:43 +0200

    Update flashx.py

--- plugin.video.vstream/resources/hosters/flashx.py
+++ plugin.video.vstream/resources/hosters/flashx.py
@@ -50,51 +50,7 @@ class cHoster(iHoster):
     def getPattern(self):
         return ''
         
-    def __getIdFromUrl(self, sUrl):
-        sPattern = "http://((?:www.|play.)?flashx.tv)/(?:embed-)?([0-9a-zA-Z/-]+)(?:.html)?"
-        oParser = cParser()
-        aResult = oParser.parse(sUrl, sPattern)
-        if (aResult[0] == True):
-            return aResult[1][0][1]
-
-        return ''
-        
-    def GetHost(self,sUrl):
-        oParser = cParser()
-        sPattern = 'http:\/\/(.+?)\/'
-        aResult = oParser.parse(sUrl, sPattern)
-        if aResult[0]:
-            return aResult[1][0]
-        return ''
-
-    def setUrl(self, sUrl):
-        self.__sUrl = str(sUrl)
-
-    def checkUrl(self, sUrl):
-        return True
-
-    def __getUrl(self, media_id):
-        return ''
-
-    def getMediaLink(self):
-        return self.__getMediaLinkForGuest()
-
-    def __getMediaLinkForGuest(self):
-        
-        #on recupere le host atuel
-        HOST = self.GetHost(self.__sUrl)
- 
-        #on recupere l'ID
-        sId = self.__getIdFromUrl(self.__sUrl)
-        
-        #on recompose l'url
-        #web_url = 'http://' + HOST + '/fxplay-%s.html' % sId
-        #web_url = 'http://' + HOST + '/fxplaynew-%s.html' % sId
-        web_url = 'http://' + HOST + '/playthis-' + sId + '.html'
-        
-        #on ne garde que les chiffres
-        sId = re.sub(r'-.+', '', sId)
-
+    def GetRedirectHtml(self,web_url,sId):
         headers = {
         #'Host' : HOST,
         'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0',
@@ -131,13 +87,74 @@ class cHoster(iHoster):
          
             #pas de redirection on annulle
             if not (redirection_target):
-                return False, False
+                return False
                 
             web_url = redirection_target
             
-            MaxRedirection = MaxRedirection - 1 
-  
-        oParser = cParser()   
+            MaxRedirection = MaxRedirection - 1
+            
+        return sHtmlContent       
+
+    def __getIdFromUrl(self, sUrl):
+        sPattern = "http://((?:www.|play.)?flashx.tv)/(?:embed-)?([0-9a-zA-Z/-]+)(?:.html)?"
+        oParser = cParser()
+        aResult = oParser.parse(sUrl, sPattern)
+        if (aResult[0] == True):
+            return aResult[1][0][1]
+
+        return ''
+        
+    def GetHost(self,sUrl):
+        oParser = cParser()
+        sPattern = 'http:\/\/(.+?)\/'
+        aResult = oParser.parse(sUrl, sPattern)
+        if aResult[0]:
+            return aResult[1][0]
+        return ''
+
+    def setUrl(self, sUrl):
+        self.__sUrl = str(sUrl)
+
+    def checkUrl(self, sUrl):
+        return True
+
+    def __getUrl(self, media_id):
+        return ''
+
+    def getMediaLink(self):
+        return self.__getMediaLinkForGuest()
+
+    def __getMediaLinkForGuest(self):
+        
+        oParser = cParser()
+        
+        #on recupere le host atuel
+        HOST = self.GetHost(self.__sUrl)
+ 
+        #on recupere l'ID
+        sId = self.__getIdFromUrl(self.__sUrl)
+        
+        #on ne garde que les chiffres
+        sId = re.sub(r'-.+', '', sId)
+        
+        #on cherche la vraie url
+        #web_url = 'http://' + HOST + '/playit-' + sId + '.html'
+        sHtmlContent = self.GetRedirectHtml(self.__sUrl,sId)
+        sPattern = 'href="(http:\/\/www\.flashx[^"]+)'
+        aResult = re.findall(sPattern,sHtmlContent)
+        if aResult:
+            web_url = aResult[0]
+        else:
+            return False,False
+        
+        sHtmlContent = self.GetRedirectHtml(web_url,sId)
+        
+        if not sHtmlContent:
+            return False,False
+
+        #fh = open('c:\\test.txt', "w")
+        #fh.write(sHtmlContent)
+        #fh.close() 
             
         #Lien code ??
         sPattern = "(\s*eval\s*\(\s*function(?:.|\s)+?)<\/script>"
@@ -147,10 +164,6 @@ class cHoster(iHoster):
             xbmc.log( "lien code")
             sUnpacked = cPacker().unpack(aResult[0])
             sHtmlContent = sUnpacked  
-  
-        #fh = open('c:\\test.txt', "w")
-        #fh.write(sHtmlContent)
-        #fh.close() 
   
         #decodage classique
         sPattern = '{file:"([^",]+)",label:"([^"<>,]+)"}'
@@ -185,3 +198,4 @@ class cHoster(iHoster):
             return True, api_call
             
         return False, False
+        
