1ad3f705aca85d88d69854cd8f73ce391da24164
TmpName <gihubtmp@yopmail.com>
2016-05-28 23:27:04 +0200

    Update themoviedb_org.py

--- plugin.video.vstream/resources/sites/themoviedb_org.py
+++ plugin.video.vstream/resources/sites/themoviedb_org.py
@@ -511,10 +511,14 @@ def showHosters():
     sMovieTitle = unicode(sMovieTitle, 'utf-8')#converti en unicode pour aider aux convertions
     sMovieTitle = unicodedata.normalize('NFD', sMovieTitle).encode('ascii', 'ignore').decode("unicode_escape")#vire accent et '\'
     sMovieTitle = sMovieTitle.encode("utf-8").lower() #on repasse en utf-8
+    
+    sMovieTitle = re.sub('\(.+?\)',' ', sMovieTitle) #vire les tags entre parentheses
+    
     #modif venom si le titre comporte un - il doit le chercher
-    #sMovieTitle = re.sub(r'[^(\w| )]', ' ', sMovieTitle) #vire les caracteres a la con qui peuvent trainer
+    sMovieTitle = re.sub(r'[^a-z -]', ' ', sMovieTitle) #vire les caracteres a la con qui peuvent trainer
+    
     sMovieTitle = re.sub('( |^)(le|la|les|du|au|a|l)( |$)',' ', sMovieTitle) #vire les articles
-    sMovieTitle = re.sub('\(.+?\)',' ', sMovieTitle) #vire les parentheses
+
     sMovieTitle = re.sub(' +',' ',sMovieTitle) #vire les espaces multiples et on laisse les espaces sans modifs car certains codent avec %20 d'autres avec +
     #print 'apres ' + sMovieTitle
 
