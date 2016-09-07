de91250c1a9e08d7915d383c3d0b224ea9edd0a3
TmpName <gihubtmp@yopmail.com>
2016-09-07 17:29:57 +0200

    Update film_streaming_co.py

--- plugin.video.vstream/resources/sites/film_streaming_co.py
+++ plugin.video.vstream/resources/sites/film_streaming_co.py
@@ -180,7 +180,8 @@ def showMovies(sSearch = ''):
             oOutputParameterHandler.addParameter('siteUrl', sNextPage)
             oGui.addNext(SITE_IDENTIFIER, 'showMovies', '[COLOR teal]Next >>>[/COLOR]' , oOutputParameterHandler)
  
-    oGui.setEndOfDirectory()
+    if not sSearch:
+        oGui.setEndOfDirectory()
          
 def __checkForNextPage(sHtmlContent):
     
