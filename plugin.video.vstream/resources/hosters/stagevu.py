db2ffb67fbfcaadbfe92b569a1c88515fd7bfcc8
johngf <john59650@live.fr>
2016-10-20 21:10:58 +0200

    Create stagevu.py
    sur allucee

---
+++ plugin.video.vstream/resources/hosters/stagevu.py
@@ -0,0 +1,72 @@
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.parser import cParser
+from resources.lib.config import cConfig
+from resources.hosters.hoster import iHoster
+
+class cHoster(iHoster):
+
+    def __init__(self):
+        self.__sDisplayName = 'Stagevu'
+        self.__sFileName = self.__sDisplayName
+        self.__sHD = ''
+
+    def getDisplayName(self):
+        return  self.__sDisplayName
+
+    def setDisplayName(self, sDisplayName):
+        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]'+self.__sDisplayName+'[/COLOR]'
+
+    def setFileName(self, sFileName):
+        self.__sFileName = sFileName
+
+    def getFileName(self):
+        return self.__sFileName
+
+    def getPluginIdentifier(self):
+        return 'stagevu'
+
+    def setHD(self, sHD):
+        self.__sHD = ''
+
+    def getHD(self):
+        return self.__sHD
+
+    def isDownloadable(self):
+        return True
+
+    def isJDownloaderable(self):
+        return True
+
+    def getPattern(self):
+        return '';
+        
+    def __getIdFromUrl(self, sUrl):
+        return ''
+
+    def setUrl(self, sUrl):
+        self.__sUrl = str(sUrl)
+
+    def checkUrl(self, sUrl):
+        return True
+
+    def getUrl(self):
+        return self.__sUrl
+
+    def getMediaLink(self):
+        return self.__getMediaLinkForGuest()
+
+    def __getMediaLinkForGuest(self):        
+        oRequest = cRequestHandler(self.__sUrl)
+        sHtmlContent = oRequest.request()
+        
+        sPattern = '<embed type=".+?" src="([^"]+)"'
+        
+        oParser = cParser()
+        aResult = oParser.parse(sHtmlContent, sPattern)
+
+
+        if (aResult[0] == True):
+            api_call = aResult[1][0]
+            return True, api_call
+            
+        return False, False
