98065908b75c6e42ab65c0b75d8668951aaa6541
johngf <john59650@live.fr>
2016-10-30 08:30:23 +0100

    Update vimple.py

--- plugin.video.vstream/resources/hosters/vimple.py
+++ plugin.video.vstream/resources/hosters/vimple.py
@@ -59,13 +59,14 @@ class cHoster(iHoster):
         return self.__getMediaLinkForGuest()
 
     def __getMediaLinkForGuest(self):
-        
-        req = urllib2.Request(self.__sUrl)
+
+        headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+        req = urllib2.Request(self.__sUrl,None,headers)
         response = urllib2.urlopen(req)
         sHtmlContent = response.read()
         head = response.headers
         response.close()
-        
+
         oParser = cParser()
         
         cookies = ''
@@ -76,11 +77,9 @@ class cHoster(iHoster):
             if (aResult[0] == True):
                 for cook in aResult[1]:
                     cookies = cookies + cook[0] + '=' + cook[1]+ ';'
-        
         #Get link
         sPattern = '"video":\[{"default":true,"url":"([^"]+?)"}]'
         aResult = oParser.parse(sHtmlContent, sPattern)
-
 
         if (aResult[0] == True):
             url = aResult[1][0]
