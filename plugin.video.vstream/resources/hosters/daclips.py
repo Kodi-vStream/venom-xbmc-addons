0403fcc14346ab42bbf32058c51680caa6e08b7b
johngf <john59650@live.fr>
2016-10-31 13:12:03 +0100

    Create daclips.py

---
+++ plugin.video.vstream/resources/hosters/daclips.py
@@ -0,0 +1,87 @@
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.parser import cParser
+from resources.lib.gui.gui import cGui
+from resources.hosters.hoster import iHoster
+import re
+#meme code que gorillavid
+class cHoster(iHoster):
+
+    def __init__(self):
+        self.__sDisplayName = 'Daclips'
+        self.__sFileName = self.__sDisplayName
+
+    def getDisplayName(self):
+        return  self.__sDisplayName
+
+    def setDisplayName(self, sDisplayName):
+        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]'+self.__sDisplayName+'[/COLOR]'
+
+    def setFileName(self, sFileName):
+        self.__sFileName = sFileName
+
+    def getFileName(self):
+        return self.__sFileName
+
+    def getPluginIdentifier(self):
+        return 'daclips'
+
+    def isDownloadable(self):
+        return True
+
+    def isJDownloaderable(self):
+        return True
+
+    def getPattern(self):
+        return ''
+        
+    def __getIdFromUrl(self,sUrl):
+        sPattern = 'http://daclips.in/embed-([^<]+)-'
+        oParser = cParser()
+        aResult = oParser.parse(self.__sUrl, sPattern)
+        if (aResult[0] == True):
+            return aResult[1][0]
+
+        return ''
+        
+    def __getKey(self):
+        return ''
+
+    def setUrl(self, sUrl):
+        self.__sUrl = str(sUrl)
+
+    def checkUrl(self, sUrl):
+        return True
+
+    def getUrl(self):
+        return self.__sUrl
+
+    def getMediaLink(self):
+        return self.__getMediaLinkForGuest()
+
+    def __getMediaLinkForGuest(self):
+
+        oParser = cParser()
+
+        id = self.__getIdFromUrl(self.__sUrl)
+
+        url = 'http://daclips.in/' + id
+        oRequest = cRequestHandler(url)
+        sHtmlContent = oRequest.request()
+        sPattern =  '<input type="hidden" name="([^"]+)" value="([^"]+)"'
+        aResult = oParser.parse(sHtmlContent, sPattern)
+
+        if (aResult[0] == True): 
+            oRequest = cRequestHandler(url)
+            oRequest.setRequestType(cRequestHandler.REQUEST_TYPE_POST)
+            for aEntry in aResult[1]:
+                oRequest.addParameters(aEntry[0], aEntry[1])     
+            oRequest.addParameters('referer', url)
+            sHtmlContent = oRequest.request()
+            r2 = re.search('file: "([^"]+)",', sHtmlContent)
+            if (r2):
+                api_call = r2.group(1)
+                
+        if (api_call):
+            return True, api_call
+
+        return False , False
