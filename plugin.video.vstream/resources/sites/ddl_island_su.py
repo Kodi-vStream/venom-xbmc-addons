cabf1e5811b1ae740c1fc49321d0db2f86bd99ca
chataigne73 <chataigne73@gmail.com>
2016-12-04 18:51:19 +0100

    Update ddl_island_su.py

--- plugin.video.vstream/resources/sites/ddl_island_su.py
+++ plugin.video.vstream/resources/sites/ddl_island_su.py
@@ -583,7 +583,7 @@ def DecryptddlProtect(url):
     
     #Si ca demande le captcha
     if 'value="Submit form"' in sHtmlContent:
-        
+        DeleteCookie('protect_ddl_island.su')	
         cookies = oRequestHandler.GetCookies()
         
         #save cookies
