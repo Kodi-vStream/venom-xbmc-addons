04fea36c52113f92a2566741eb8098a0da3a34c8
Kodigoal <Kodigoal@users.noreply.github.com>
2016-12-17 22:32:16 +0100

    Add files via upload
    Rubrique Sport / Source Tvrex_net
    Live match NBA : Juste listage des matchs...
    Replay match NBA saison en cours + ex playoffs 2016
    Recherche ok.

---
+++ plugin.video.vstream/resources/sites/Tvrex_net.py
@@ -0,0 +1,224 @@
+#-*- coding: utf-8 -*-
+#Venom.
+from resources.lib.gui.hoster import cHosterGui
+from resources.lib.handler.hosterHandler import cHosterHandler
+from resources.lib.gui.gui import cGui
+from resources.lib.gui.guiElement import cGuiElement
+from resources.lib.handler.inputParameterHandler import cInputParameterHandler
+from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.config import cConfig
+from resources.lib.parser import cParser
+#from resources.lib.util import cUtil
+
+
+SITE_IDENTIFIER = 'Tvrex_net' 
+SITE_NAME = 'Tvrex.net'
+SITE_DESC = 'NBA Live/Replay'
+
+URL_MAIN = 'http://tvrex.net'
+
+URL_SEARCH = ('http://tvrex.net/?s=', 'showMovies')
+FUNCTION_SEARCH = 'showMovies'
+
+
+SPORT_SPORTS = ('http://', 'ReplayTV')
+
+def load():
+    
+    oGui = cGui()
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', 'http://venom/') 
+    oGui.addDir(SITE_IDENTIFIER, 'showSearch', 'Recherche', 'search.png', oOutputParameterHandler)
+    
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', SPORT_SPORTS[0])
+    oGui.addDir(SITE_IDENTIFIER, SPORT_SPORTS[1], 'Replay NBA Games', 'news.png', oOutputParameterHandler)
+    
+    oGui.setEndOfDirectory()
+
+def showSearch():
+    
+    oGui = cGui()
+
+    sSearchText = oGui.showKeyBoard()
+    if (sSearchText != False):
+        sUrl = URL_SEARCH[0] + sSearchText  
+        showMovies(sUrl)
+        oGui.setEndOfDirectory()
+        return  
+
+def ReplayTV():
+
+    oGui = cGui()
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', 'http://venom/') 
+    oGui.addDir(SITE_IDENTIFIER, 'showSearch', 'Recherche', 'search.png', oOutputParameterHandler)
+    
+    
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', URL_MAIN + '/live')
+    oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'Live NBA Games', 'search.png', oOutputParameterHandler)
+    
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', URL_MAIN + '/category/nba/')
+    oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'Replay NBA Games', 'search.png', oOutputParameterHandler)
+    
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', URL_MAIN + '/category/2016-nba-playoffs/')
+    oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'Replay NBA 2016 Playoffs', 'tv.png', oOutputParameterHandler)
+    
+    #oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', URL_MAIN + '/category/2015-nba-playoffs/')
+    #oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'Replay NBA 2015 Playoffs', 'tv.png', oOutputParameterHandler)
+    
+    #oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', URL_MAIN + '/category/nba/all-star-weekend/')
+    #oGui.addDir(SITE_IDENTIFIER, 'showMovies', 'Replay NBA All Star Weekend', 'tv.png', oOutputParameterHandler)
+            
+    oGui.setEndOfDirectory()  
+    
+ 
+
+
+def showMovies(sSearch = ''):
+    
+    oGui = cGui()
+    
+    if sSearch:
+        sUrl = sSearch
+    else:
+        oInputParameterHandler = cInputParameterHandler()
+        sUrl = oInputParameterHandler.getValue('siteUrl')
+   
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request()
+    
+    
+    if '/live' in sUrl:
+
+        oGui.addText(SITE_IDENTIFIER, '[COLOR olive]Live NBA Games[/COLOR]')
+        oGui.addText(SITE_IDENTIFIER,'[COLOR olive]Programme des Matchs:[/COLOR]')
+        
+        sPattern = '<td><a href="(http://tvrex.net/live/.+?)">(.+?)</a></td><td>(.+?)</td></tr><tr><td>' 
+    
+    else:
+
+        oGui.addText(SITE_IDENTIFIER, '[COLOR olive]Replay NBA Games[/COLOR]')
+        sPattern = '<a href="([^"]+)">\s*<img src="[^"]+" data-hidpi="(.+?)\?.+?" alt="(.+?)" width=".+?"'
+
+    if URL_SEARCH[0] in sUrl:
+        
+        sPattern = '<a href="([^"]+)"><img src="[^"]+" data-hidpi="(.+?)\?.+?" alt="(.+?)"'
+    
+    
+   
+    oParser = cParser()
+    aResult = oParser.parse(sHtmlContent, sPattern)
+    
+    sDate2 = ''
+    
+    if (aResult[0] == True):
+        total = len(aResult[1])
+        
+        dialog = cConfig().createDialog(SITE_NAME)
+        
+        for aEntry in aResult[1]:
+            cConfig().updateDialog(dialog, total)
+            
+            
+            sTitle = aEntry[2]
+            sUrl2 = aEntry[0]
+            sThumbnail = aEntry[1]
+
+            if '/live/' in sUrl2:
+                sTitle = aEntry[1] + ' ' + '[COLOR coral]' +  ' (' + aEntry[2] + ') ' + '[/COLOR]'
+                sTitle = sTitle.replace('@', 'vs')
+            
+            if 'category/nba' in sUrl:
+                sTitle2 = sTitle.split(" – ")
+                sTitle = sTitle2[0]
+                sDate =  sTitle2[1]
+
+                if (sDate2 != sDate):
+                   oGui.addText(SITE_IDENTIFIER,'[COLOR olive]' + sDate + '[/COLOR]')
+                   sDate2 = sDate
+            
+           
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl', sUrl2) 
+            oOutputParameterHandler.addParameter('sMovieTitle', sTitle)
+            oOutputParameterHandler.addParameter('sThumbnail', sThumbnail)
+
+            
+
+            oGui.addMovie(SITE_IDENTIFIER, 'showHosters', sTitle, '', sThumbnail, sUrl2, oOutputParameterHandler)
+                
+                
+            
+            
+        cConfig().finishDialog(dialog)
+           
+        sNextPage = __checkForNextPage(sHtmlContent)
+        if (sNextPage != False):
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl', sNextPage)
+            oGui.addDir(SITE_IDENTIFIER, 'showMovies', '[COLOR teal]Next >>>[/COLOR]', 'next.png', oOutputParameterHandler)
+            
+
+    if not sSearch:
+        oGui.setEndOfDirectory()
+
+
+def __checkForNextPage(sHtmlContent):
+    
+    oParser = cParser()
+    
+    sPattern = "<span class='current'>.+?</span><a href='(.+?)' class='inactive'"
+    
+    aResult = oParser.parse(sHtmlContent, sPattern)
+    
+    if (aResult[0] == True):
+        return aResult[1][0]
+
+    return False
+    
+
+def showHosters():
+    
+    oGui = cGui() 
+    oInputParameterHandler = cInputParameterHandler() 
+    sUrl = oInputParameterHandler.getValue('siteUrl')  
+    sMovieTitle = oInputParameterHandler.getValue('sMovieTitle')
+    sThumbnail = oInputParameterHandler.getValue('sThumbnail')
+    
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request();
+          
+    
+    oParser = cParser()
+    
+    sPattern = '<a href="(https://open.+?)" target="_blank">(.+?)</a>'
+    
+    aResult = oParser.parse(sHtmlContent, sPattern)
+    
+     
+    oGui.addText(SITE_IDENTIFIER,sMovieTitle)
+    oGui.addText(SITE_IDENTIFIER,'[COLOR olive]Qualités disponibles:[/COLOR]')
+    
+    if (aResult[0] == True):
+        for aEntry in aResult[1]:
+            
+            sHosterUrl = aEntry[0]
+            sTitle = aEntry[1]
+            oHoster = cHosterGui().checkHoster(sHosterUrl)
+            if (oHoster != False):
+                oHoster.setDisplayName(sTitle)
+                oHoster.setFileName(sTitle)
+                cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumbnail) 
+                
+                
+    oGui.setEndOfDirectory()
+ 
