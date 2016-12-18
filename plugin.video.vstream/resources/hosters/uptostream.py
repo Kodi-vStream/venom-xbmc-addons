4bea4ea3747ddb4a8df30aa7659af0e48844cdcf
TmpName <gihubtmp@yopmail.com>
2016-12-18 11:47:38 +0100

    Update uptostream.py

--- plugin.video.vstream/resources/hosters/uptostream.py
+++ plugin.video.vstream/resources/hosters/uptostream.py
@@ -5,7 +5,7 @@ from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.lib.gui.gui import cGui
 from resources.hosters.hoster import iHoster
-import urllib,xbmcgui,xbmc
+import urllib,xbmcgui,xbmc,re
 
 class cHoster(iHoster):
 
