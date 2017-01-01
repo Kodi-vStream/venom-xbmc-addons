8413504fc16c9f2b04f4839d01cf3e8dede39438
TmpName <gihubtmp@yopmail.com>
2017-01-01 16:41:50 +0100

    Update requestHandler.py

--- plugin.video.vstream/resources/lib/handler/requestHandler.py
+++ plugin.video.vstream/resources/lib/handler/requestHandler.py
@@ -44,6 +44,13 @@ class cRequestHandler:
 
     def addParameters(self, sParameterKey, mParameterValue):
         self.__aParamaters[sParameterKey] = mParameterValue
+        
+    #egg addMultipartFiled('sess_id':sId,'upload_type':'url','srv_tmp_url':sTmp)
+    def addMultipartFiled(self,fields ):
+        mpartdata = MPencode(fields)
+        self.__aParamaters = mpartdata[1]
+        self.addHeaderEntry('Content-Type', mpartdata[0] )
+        self.addHeaderEntry('Content-Length', len(mpartdata[1]))
 
     def getResponseHeader(self):
         return self.__sResponseHeader
@@ -150,3 +157,46 @@ class cRequestHandler:
     def getHeaderLocationUrl(self):        
         opened = urllib.urlopen(self.__sUrl)
         return opened.geturl()
+
+#******************************************************************************
+#from https://github.com/eliellis/mpart.py
+
+def MPencode(fields):
+    import mimetypes
+    random_boundary = __randy_boundary()
+    content_type = "multipart/form-data, boundary=%s" % random_boundary
+
+    form_data = []
+    
+    if fields:
+        for (key, value) in fields.iteritems():
+            if not hasattr(value, 'read'):
+                itemstr = '--%s\r\nContent-Disposition: form-data; name="%s"\r\n\r\n%s\r\n' % (random_boundary, key, value)
+                form_data.append(itemstr)
+            elif hasattr(value, 'read'):
+                with value:
+                    file_mimetype = mimetypes.guess_type(value.name)[0] if mimetypes.guess_type(value.name)[0] else 'application/octet-stream'
+                    itemstr = '--%s\r\nContent-Disposition: form-data; name="%s"; filename="%s"\r\nContent-Type: %s\r\n\r\n%s\r\n' % (random_boundary, key, value.name, file_mimetype, value.read())
+                form_data.append(itemstr)
+            else:
+                raise Exception(value, 'Field is neither a file handle or any other decodable type.')
+    else:
+        pass
+
+    form_data.append('--%s--\r\n' % random_boundary)
+
+    return content_type, ''.join(form_data)
+
+def __randy_boundary(length=10,reshuffle=False):
+    import random,string
+    
+    character_string = string.letters+string.digits
+    boundary_string = []
+    for i in range(0,length):
+        rand_index = random.randint(0,len(character_string) - 1)
+        boundary_string.append(character_string[rand_index])
+    if reshuffle:
+        random.shuffle(boundary_string)
+    else:
+        pass
+    return ''.join(boundary_string) 
