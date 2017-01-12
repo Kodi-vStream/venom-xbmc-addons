9e66aed6c279e185f6e6950f430fab2da0b8fc25
Lord Venom <Twitter_@LordVenom57>
2017-01-12 23:15:04 +0100

    update tmdb

--- plugin.video.vstream/resources/lib/tmdb.py
+++ plugin.video.vstream/resources/lib/tmdb.py
@@ -2,10 +2,19 @@
 #Code de depart par AnthonyBloomer
 #Modif pour vStream
 
-import json
+from resources.lib.config import cConfig
+
+import json, os
 import pprint
 from urllib import quote_plus, urlopen, urlencode
 import xbmc
+
+try:
+    from sqlite3 import dbapi2 as sqlite
+    cConfig().log('SQLITE 3 as DB engine') 
+except:
+    from pysqlite2 import dbapi2 as sqlite
+    cConfig().log('SQLITE 2 as DB engine') 
 
 
 class obj:
@@ -23,9 +32,112 @@ class cTMDb:
         self.lang = lang
         self.poster = 'https://image.tmdb.org/t/p/w396'
         self.fanart = 'https://image.tmdb.org/t/p/w1280'
+        self.cache = os.path.join(cConfig().getSettingCache(),'video_cache.db')
+        
+        try:
+            if not os.path.exists(self.cache):
+                f = open(self.cache,'w')
+                f.close()
+                self.__createdb()
+        except:
+            pass
+            
+        try:
+            self.db = sqlite.connect(self.cache)
+            self.dbcur = self.db.cursor()
+        except:
+            pass
 
-    def get_config(self):
-        return self._call('configuration', '')
+    def __createdb(self):
+
+        sql_create = "CREATE TABLE IF NOT EXISTS movie ("\
+                           "imdb_id VARCHAR(10), "\
+                           "tmdb_id VARCHAR(10), "\
+                           "title TEXT, "\
+                           "year INTEGER,"\
+                           "director TEXT, "\
+                           "writer TEXT, "\
+                           "tagline TEXT, cast TEXT,"\
+                           "rating FLOAT, "\
+                           "votes TEXT, "\
+                           "duration TEXT, "\
+                           "plot TEXT,"\
+                           "mpaa TEXT, "\
+                           "premiered TEXT, "\
+                           "genre TEXT, "\
+                           "studio TEXT,"\
+                           "thumb_url TEXT, "\
+                           "cover_url TEXT, "\
+                           "trailer_url TEXT, "\
+                           "backdrop_url TEXT,"\
+                           "imgs_prepacked TEXT,"\
+                           "overlay INTEGER,"\
+                           "UNIQUE(imdb_id, tmdb_id, title, year)"\
+                           ");"
+        self.dbcur.execute(sql_create)
+       
+        sql_create = "CREATE TABLE IF NOT EXISTS tvshow_meta ("\
+                           "imdb_id VARCHAR(10), "\
+                           "tvdb_id VARCHAR(10), "\
+                           "title TEXT, "\
+                           "year INTEGER,"\
+                           "cast TEXT,"\
+                           "rating FLOAT, "\
+                           "duration TEXT, "\
+                           "plot TEXT,"\
+                           "mpaa TEXT, "\
+                           "premiered TEXT, "\
+                           "genre TEXT, "\
+                           "studio TEXT,"\
+                           "status TEXT,"\
+                           "banner_url TEXT, "\
+                           "cover_url TEXT,"\
+                           "trailer_url TEXT, "\
+                           "backdrop_url TEXT,"\
+                           "imgs_prepacked TEXT,"\
+                           "overlay INTEGER,"\
+                           "UNIQUE(imdb_id, tvdb_id, title)"\
+                           ");"
+                           
+        self.dbcur.execute(sql_create)
+       
+        sql_create = "CREATE TABLE IF NOT EXISTS season_meta ("\
+                           "imdb_id VARCHAR(10), "\
+                           "tvdb_id VARCHAR(10), " \
+                           "season INTEGER, "\
+                           "cover_url TEXT,"\
+                           "overlay INTEGER,"\
+                           "UNIQUE(imdb_id, tvdb_id, season)"\
+                           ");"
+                           
+        self.dbcur.execute(sql_create)
+        
+        sql_create = "CREATE TABLE IF NOT EXISTS episode_meta ("\
+                           "imdb_id VARCHAR(10), "\
+                           "tvdb_id VARCHAR(10), "\
+                           "episode_id VARCHAR(10), "\
+                           "season INTEGER, "\
+                           "episode INTEGER, "\
+                           "title TEXT, "\
+                           "director TEXT, "\
+                           "writer TEXT, "\
+                           "plot TEXT, "\
+                           "rating FLOAT, "\
+                           "premiered TEXT, "\
+                           "poster TEXT, "\
+                           "overlay INTEGER, "\
+                           "UNIQUE(imdb_id, tvdb_id, episode_id, title)"\
+                           ");"
+                           
+        self.dbcur.execute(sql_create)
+        return
+        
+    def __del__(self):
+        ''' Cleanup db when object destroyed '''
+        try:
+            self.dbcur.close()
+            self.dbcon.close()
+        except: pass
 
     # Get the basic movie information for a specific movie id.
     def get_movie(self, movie_id, append_to_response="append_to_response=trailers,images,casts,translations"):
@@ -103,7 +215,8 @@ class cTMDb:
         return arr
 
     # Search for movies by title.
-    def search(self, name, year='', page=1):
+    def search_movie(self, name, year='', page=1):
+    
         meta = {}
         
         if year:
@@ -129,6 +242,13 @@ class cTMDb:
         
         #[arr.append(obj(**res)) for res in result['results']]
         return meta
+        
+            # Search for TV shows by title.
+    def search_tv(self, term, page=1):
+        arr = []
+        result = self._call('search/tv', 'query=' + quote_plus(term) + '&page=' + str(page))
+        [arr.append(obj(**res)) for res in result['results']]
+        return arr
 
     # Get the similar movies for a specific movie id.
     def similar(self, id, page=1):
@@ -144,13 +264,6 @@ class cTMDb:
     # Get the latest TV show id.
     def get_latest_tv_show(self):
         return obj(**self._call('tv/latest', ''))
-
-    # Search for TV shows by title.
-    def search_tv(self, term, page=1):
-        arr = []
-        result = self._call('search/tv', 'query=' + quote_plus(term) + '&page=' + str(page))
-        [arr.append(obj(**res)) for res in result['results']]
-        return arr
 
     # Get the similar TV shows for a specific tv id.
     def similar_shows(self, id, page=1):
@@ -225,6 +338,56 @@ class cTMDb:
             
     
         return _meta
+        
+    def get_meta(self, media_type, name, imdb_id='', tmdb_id='', year='', overlay=6, update=False):
+        '''
+        Main method to get meta data for movie or tvshow. Will lookup by name/year 
+        if no IMDB ID supplied.       
+        
+        Args:
+            media_type (str): 'movie' or 'tvshow'
+            name (str): full name of movie/tvshow you are searching
+        Kwargs:
+            imdb_id (str): IMDB ID        
+            tmdb_id (str): TMDB ID
+            year (str): 4 digit year of video, recommended to include the year whenever possible
+                        to maximize correct search results.
+            overlay (int): To set the default watched status (6=unwatched, 7=watched) on new videos
+                        
+        Returns:
+            DICT of meta data or None if cannot be found.
+        '''
+       
+        xbmc.log('vstream Meta', 0)
+        xbmc.log('Attempting to retrieve meta data for %s: %s %s %s %s' % (media_type, name, year, imdb_id, tmdb_id), 0)
+        #recherche dans la base de donner
+        if not update:
+            if tmdb_id:
+                meta = self._cache_lookup_by_id(media_type, tmdb_id=tmdb_id)
+            else:
+                meta = self._cache_lookup_by_name(media_type, name, year)
+        else:
+            meta = {}
+            
+        #recherche online
+
+        if not meta:
+            
+            if media_type=='movie':
+                if tmbd_id:
+                    self.get_movie(tmdb_id)                
+                else:
+                    self.search_movie(self, name, year)
+            elif media_type=='tvshow':
+                meta = self._get_tvdb_meta(imdb_id, name, year)
+            
+            #ecrit dans le cache
+            self._cache_save_video_meta(meta, name, media_type, overlay)
+            
+        #meta = self.__format_meta(media_type, meta, name)
+        meta = self._format(meta)
+        
+        return meta
 
     def _call(self, action, append_to_response):
         url = '%s%s?api_key=%s&%s&language=%s' % (self.URL, action, self.api_key, append_to_response, self.lang)
