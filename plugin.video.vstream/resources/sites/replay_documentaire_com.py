8b3862507476add85ce03261f6abec5bbdf3bfab
zombiB <zombivert@gmail.com>
2017-02-06 16:06:31 +0100

    Create replay_documentaire_com.py
    Revoir très facilement tous vos documentaires en replay

---
+++ plugin.video.vstream/resources/sites/replay_documentaire_com.py
@@ -0,0 +1,169 @@
+#-*- coding: utf-8 -*-
+#zombi.
+from resources.lib.gui.hoster import cHosterGui
+from resources.lib.handler.hosterHandler import cHosterHandler
+from resources.lib.gui.gui import cGui
+from resources.lib.gui.guiElement import cGuiElement
+from resources.lib.handler.inputParameterHandler import cInputParameterHandler
+from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.parser import cParser
+from resources.lib.config import cConfig
+from resources.lib.util import cUtil
+import re
+
+SITE_IDENTIFIER = 'replay_documentaire_com'
+SITE_NAME = 'replay-documentaire.com'
+SITE_DESC = 'Revoir très facilement tous vos documentaires en replay streaming'
+
+URL_MAIN = 'http://www.replay-documentaire.com/'
+
+
+DOC_NEWS = ('http://www.replay-documentaire.com/cat%C3%A9gories/documentaire/', 'showMovies')
+DOC_DOCS = ('http://', 'load')
+
+REPLAYTV_NEWS = ('http://www.replay-documentaire.com/', 'showMovies')
+REPLAYTV_REPLAYTV = ('http://', 'load')
+REPLAYTV_GENRES = (True, 'showGenres')
+
+URL_SEARCH = ('http://replay-streaming.com/?s=', 'showMovies')
+FUNCTION_SEARCH = 'showMovies'
+
+def load():
+    oGui = cGui()
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', 'http://venom/')
+    oGui.addDir(SITE_IDENTIFIER, 'showSearch', 'Recherche', 'search.png', oOutputParameterHandler)
+    
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', DOC_NEWS[0])
+    oGui.addDir(SITE_IDENTIFIER, DOC_NEWS[1], 'Documentaires', 'doc.png', oOutputParameterHandler)
+    
+    
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', 'http://frenchstream.org/les-plus-vues')
+    oGui.addDir(SITE_IDENTIFIER, 'showGenres', 'Documentaires Genres', 'genres.png', oOutputParameterHandler)    
+            
+    oGui.setEndOfDirectory()
+  
+def showSearch():
+    oGui = cGui()
+    sSearchText = oGui.showKeyBoard()
+    if (sSearchText != False):
+            sUrl = 'http://replay-streaming.com/?s='+sSearchText  
+            showMovies(sUrl)
+            oGui.setEndOfDirectory()
+            return  
+    
+    
+def showGenres():
+    oGui = cGui()
+    oInputParameterHandler = cInputParameterHandler()
+    sUrl = oInputParameterHandler.getValue('siteUrl')
+ 
+    liste = []
+    liste.append( ["telefilm","http://replay-streaming.com/categorie/telefilm/"] )
+    liste.append( ["spectacle","http://replay-streaming.com/categorie/spectacle/"] )
+    liste.append( ["concert","http://replay-streaming.com/categorie/concert/"] )
+    liste.append( ["series","http://replay-streaming.com/categorie/series/"] )
+    liste.append( ["telefoot","http://replay-streaming.com/categorie/telefoot/"] )
+                
+    for sTitle,sUrl in liste:
+        
+        oOutputParameterHandler = cOutputParameterHandler()
+        oOutputParameterHandler.addParameter('siteUrl', sUrl)
+        oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'genres.png', oOutputParameterHandler)
+       
+    oGui.setEndOfDirectory() 
+
+
+def showMovies(sSearch = ''):
+    oGui = cGui()
+    if sSearch:
+      sUrl = sSearch
+    else:
+        oInputParameterHandler = cInputParameterHandler()
+        sUrl = oInputParameterHandler.getValue('siteUrl')
+   
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request();
+    sPattern = '>([^<]+)</a></h2><p class="post-meta">.*?<a href="([^<]+)"> <img.*?src="([^<]+)" class="attachment-tie-medium size-tie-medium wp-post-image" alt=".*?"/> <span class="fa overlay-icon"></span> </a></div><div class="entry"><p>([^<]+)</p> <a class="more-link"'
+    
+    oParser = cParser()
+    aResult = oParser.parse(sHtmlContent, sPattern)
+    if (aResult[0] == True):
+        total = len(aResult[1])
+        dialog = cConfig().createDialog(SITE_NAME)
+        for aEntry in aResult[1]:
+            cConfig().updateDialog(dialog, total)
+            if dialog.iscanceled():
+                break
+            
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl', str(aEntry[1]))
+            oOutputParameterHandler.addParameter('sMovieTitle', str(aEntry[0]))
+            oOutputParameterHandler.addParameter('sThumbnail', str(aEntry[2]))
+            oGui.addMisc(SITE_IDENTIFIER, 'showHosters', aEntry[0], 'doc.png', aEntry[2], aEntry[3], oOutputParameterHandler)
+
+        cConfig().finishDialog(dialog)
+            
+        sNextPage = __checkForNextPage(sHtmlContent)
+        if (sNextPage != False):
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl', sNextPage)
+            oGui.addDir(SITE_IDENTIFIER, 'showMovies', '[COLOR teal]Next >>>[/COLOR]', 'next.png', oOutputParameterHandler)
+
+    if not sSearch:
+        oGui.setEndOfDirectory()
+
+
+def __checkForNextPage(sHtmlContent):
+    sPattern = 'id="tie-next-page"> <a href="([^<]+)">'
+    oParser = cParser()
+    aResult = oParser.parse(sHtmlContent, sPattern)
+    if (aResult[0] == True):
+        #print aResult[1][0]
+        return aResult[1][0]
+
+    return False
+    
+
+def showHosters():
+    oGui = cGui()
+    oInputParameterHandler = cInputParameterHandler()
+    sUrl = oInputParameterHandler.getValue('siteUrl')
+    sMovieTitle = oInputParameterHandler.getValue('sMovieTitle')
+    sThumbnail = oInputParameterHandler.getValue('sThumbnail')
+    
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request();
+    #sHtmlContent = sHtmlContent.replace('<iframe src="//www.facebook.com/plugins/like.php','').replace('<iframe src="http://www.facebook.com/plugins/likebox.php','')
+               
+        
+    sPattern = 'data-lazy-src="([^<]+)/.*?.mp4" scrolling="no"'
+    oParser = cParser()
+    aResult = oParser.parse(sHtmlContent, sPattern)
+    if (aResult[0] == True):
+        total = len(aResult[1])
+        dialog = cConfig().createDialog(SITE_NAME)
+        for aEntry in aResult[1]:
+            cConfig().updateDialog(dialog, total)
+            if dialog.iscanceled():
+                break
+            
+            url = str(aEntry)
+            if url.startswith('//'):
+                url = 'http:' + url
+            
+            sHosterUrl = url
+            oHoster = cHosterGui().checkHoster(sHosterUrl)
+            if (oHoster != False):
+                oHoster.setDisplayName(sMovieTitle)
+                oHoster.setFileName(sMovieTitle)
+                cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumbnail)
+
+        cConfig().finishDialog(dialog) 
+                
+    oGui.setEndOfDirectory()
+    
