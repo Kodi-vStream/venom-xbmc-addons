c009d634f337f5b41987fc7ba3e4e34ab3eb2268
johngf <john59650@live.fr>
2017-03-21 08:06:08 +0100

    Update kepliz_com.py

--- plugin.video.vstream/resources/sites/kepliz_com.py
+++ plugin.video.vstream/resources/sites/kepliz_com.py
@@ -1,9 +1,7 @@
 #-*- coding: utf-8 -*-
 #Venom.
 from resources.lib.gui.hoster import cHosterGui
-from resources.lib.handler.hosterHandler import cHosterHandler
 from resources.lib.gui.gui import cGui
-from resources.lib.gui.guiElement import cGuiElement
 from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
 from resources.lib.handler.requestHandler import cRequestHandler
