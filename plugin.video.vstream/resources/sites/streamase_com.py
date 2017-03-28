ca8dff4dce549cb93e3deb149206f98f5f3ffaca
johngf <john59650@live.fr>
2017-03-28 16:33:19 +0200

    Update streamase_com.py

--- plugin.video.vstream/resources/sites/streamase_com.py
+++ plugin.video.vstream/resources/sites/streamase_com.py
@@ -2,11 +2,7 @@
 
 from resources.lib.config import cConfig
 from resources.lib.gui.hoster import cHosterGui
-from resources.lib.handler.rechercheHandler import cRechercheHandler
-from resources.lib.handler.hosterHandler import cHosterHandler
 from resources.lib.gui.gui import cGui
-from resources.lib.favourite import cFav
-from resources.lib.gui.guiElement import cGuiElement
 from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
 from resources.lib.handler.requestHandler import cRequestHandler
@@ -15,8 +11,7 @@ from resources.lib.util import cUtil
 
 from resources.lib.config import GestionCookie
 
-import urllib,re,urllib2
-import xbmcgui
+import re
 import xbmc
 import xbmcaddon,os
 
