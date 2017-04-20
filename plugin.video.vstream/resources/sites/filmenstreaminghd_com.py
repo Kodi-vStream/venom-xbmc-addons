2a8a5e8f44181d89b67c159af8f4fa3b9d69bc00
razorex <individuel@free.fr>
2017-04-20 08:35:37 +0200

    Ajout de menus
    Ajout de menu Animés (Genres) et Animés (Thèmes)

--- plugin.video.vstream/resources/sites/filmenstreaminghd_com.py
+++ plugin.video.vstream/resources/sites/filmenstreaminghd_com.py
@@ -14,7 +14,7 @@ import urllib2
 
 SITE_IDENTIFIER = 'filmenstreaminghd_com'
 SITE_NAME = 'FilmEnStreamingHD'
-SITE_DESC = 'Films/Séries/Animés en streaming'
+SITE_DESC = 'Films, Séries & Animés en streaming'
 
 URL_MAIN = 'http://www.filmenstreaminghd.com/'
 
@@ -22,11 +22,14 @@ MOVIE_MOVIE = (URL_MAIN + 'films', 'showMovies')
 MOVIE_HD = (URL_MAIN + '1080p-films', 'showMovies')
 MOVIE_VIEWS = (URL_MAIN + 'films-populaires', 'showMovies')
 MOVIE_GENRES = (True, 'showMovieGenres')
+MOVIE_QLT = (True, 'showQlt')
 
 SERIE_SERIES = (URL_MAIN + 'series-tv', 'showMovies')
 SERIE_GENRES = (True, 'showSerieGenres')
 
 ANIM_ANIMS = (URL_MAIN +'animes', 'showMovies')
+ANIM_GENRES = (True, 'showAnimeGenres')
+ANIM_THEMES = (True, 'showAnimeThemes')
 
 URL_SEARCH = ('', 'showMovies')
 FUNCTION_SEARCH = 'showMovies'
@@ -55,8 +58,8 @@ def load():
     oGui.addDir(SITE_IDENTIFIER, MOVIE_GENRES[1], 'Films (Genres)', 'films_genres.png', oOutputParameterHandler)
     
     oOutputParameterHandler = cOutputParameterHandler()
-    oOutputParameterHandler.addParameter('siteUrl', 'http://venom')
-    oGui.addDir(SITE_IDENTIFIER, 'showQlt', 'Films (Qualités)', 'films.png', oOutputParameterHandler)
+    oOutputParameterHandler.addParameter('siteUrl', MOVIE_QLT[0])
+    oGui.addDir(SITE_IDENTIFIER, MOVIE_QLT[1], 'Films (Qualités)', 'films.png', oOutputParameterHandler)
     
     oOutputParameterHandler = cOutputParameterHandler()
     oOutputParameterHandler.addParameter('siteUrl', SERIE_SERIES[0])
@@ -69,7 +72,15 @@ def load():
     oOutputParameterHandler = cOutputParameterHandler()
     oOutputParameterHandler.addParameter('siteUrl', ANIM_ANIMS[0])
     oGui.addDir(SITE_IDENTIFIER, ANIM_ANIMS[1], 'Animés', 'animes.png', oOutputParameterHandler)
-
+    
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', ANIM_GENRES[0])
+    oGui.addDir(SITE_IDENTIFIER, ANIM_GENRES[1], 'Animés (Genres)', 'animes_genres.png', oOutputParameterHandler)
+    
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', ANIM_THEMES[0])
+    oGui.addDir(SITE_IDENTIFIER, ANIM_THEMES[1], 'Animés (Thèmes)', 'animes_genres.png', oOutputParameterHandler)
+    
     oGui.setEndOfDirectory()
 
 def showSearch():
@@ -93,7 +104,7 @@ def showMovieGenres():
     liste.append( ['Comédie Dramatique',URL_MAIN + 'comedie-dramatique'] )
     liste.append( ['Documentaire',URL_MAIN + 'documentaire'] )
     liste.append( ['Drame',URL_MAIN + 'drame'] )
-    liste.append( ['Epouvante Horreur',URL_MAIN + 'epouvante-horreur'] )
+    liste.append( ['Épouvante Horreur',URL_MAIN + 'epouvante-horreur'] )
     liste.append( ['Familial',URL_MAIN + 'familial'] )
     liste.append( ['Fantastique',URL_MAIN + 'fantastique'] )
     liste.append( ['Histoire',URL_MAIN + 'histoire'] )
@@ -128,7 +139,7 @@ def showSerieGenres():
     liste.append( ['Comédie Dramatique',URL_MAIN + 'comedie-dramatique-series'] )
     liste.append( ['Documentaire',URL_MAIN + 'documentaire-series'] )
     liste.append( ['Drame',URL_MAIN + 'drame-series'] )
-    liste.append( ['Epouvante Horreur',URL_MAIN + 'epouvante-horreur-series'] )
+    liste.append( ['Épouvante Horreur',URL_MAIN + 'epouvante-horreur-series'] )
     liste.append( ['Familial',URL_MAIN + 'familial-series'] )
     liste.append( ['Fantastique',URL_MAIN + 'fantastique-series'] )
     liste.append( ['Histoire',URL_MAIN + 'histoire-series'] )
@@ -148,7 +159,95 @@ def showSerieGenres():
         oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'series_genres.png', oOutputParameterHandler)
 
     oGui.setEndOfDirectory()
+
+def showAnimeGenres():
+    oGui = cGui()
     
+    liste = []
+    liste.append( ['Action',URL_MAIN+ 'action-animes'] )    
+    liste.append( ['Animation',URL_MAIN + 'animation-animes'] )
+    liste.append( ['Aventure',URL_MAIN + 'aventure-animes'] )
+    liste.append( ['Biographie',URL_MAIN + 'biographie-animes'] )    
+    liste.append( ['Comédie',URL_MAIN + 'comedie-animes'] )
+    liste.append( ['Drame',URL_MAIN + 'drame-animes'] )
+    liste.append( ['Épouvante Horreur',URL_MAIN + 'epouvante-horreur-animes'] )
+    liste.append( ['Familial',URL_MAIN + 'familial-animes'] )
+    liste.append( ['Fantastique',URL_MAIN + 'fantastique-animes'] )
+    liste.append( ['Histoire',URL_MAIN + 'histoire-animes'] )
+    liste.append( ['Musical',URL_MAIN + 'musical-animes'] )
+    liste.append( ['Mystère',URL_MAIN + 'mystere-animes'] )
+    liste.append( ['Policier',URL_MAIN + 'policier-crime-animes'] )
+    liste.append( ['Romance',URL_MAIN + 'romance-animes'] )
+    liste.append( ['School Life',URL_MAIN + 'school-life-animes'] )
+    liste.append( ['Science Fiction',URL_MAIN + 'science-fiction-animes'] )
+    liste.append( ['Seinen',URL_MAIN + 'seinen-animes'] )
+    liste.append( ['Shôjo',URL_MAIN + 'shojo-animes'] )
+    liste.append( ['Shônen',URL_MAIN + 'shonen-animes'] )
+    liste.append( ['Slice of Life',URL_MAIN + 'slice-of-life-animes'] )
+    liste.append( ['Surnaturel',URL_MAIN + 'surnaturel-animes'] )
+    liste.append( ['Thriller',URL_MAIN + 'thriller-animes'] )
+    liste.append( ['Western',URL_MAIN + 'western-animes'] )
+    
+    for sTitle,sUrl in liste:
+        
+        oOutputParameterHandler = cOutputParameterHandler()
+        oOutputParameterHandler.addParameter('siteUrl', sUrl)
+        oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'animes_genres.png', oOutputParameterHandler)
+        
+    oGui.setEndOfDirectory()
+
+def showAnimeThemes():
+    oGui = cGui()
+    
+    liste = []
+    liste.append( ['Aliens',URL_MAIN+ 'aliens-animes-themes'] )    
+    liste.append( ['Amitié',URL_MAIN + 'amitie-animes-themes'] )
+    liste.append( ['Apprentissage',URL_MAIN + 'apprentissage-animes-themes'] )
+    liste.append( ['Arts Martiaux',URL_MAIN + 'arts-martiaux-animes-themes'] )    
+    liste.append( ['Assassinat',URL_MAIN + 'assassinat-animes-themes'] )
+    liste.append( ['Autre monde',URL_MAIN + 'autre-monde-animes-themes'] )
+    liste.append( ['Baseball',URL_MAIN + 'baseball-animes-themes'] )
+    liste.append( ['Combats',URL_MAIN + 'combats-animes-themes'] )
+    liste.append( ['Démons',URL_MAIN + 'demons-animes-themes'] )
+    liste.append( ['Détective',URL_MAIN + 'detective-animes-themes'] )
+    liste.append( ['École',URL_MAIN + 'ecole-animes-themes'] )
+    liste.append( ['Espace',URL_MAIN + 'espace-animes-themes'] )
+    liste.append( ['Football',URL_MAIN + 'football-animes-themes'] )
+    liste.append( ['Guerre',URL_MAIN + 'guerre-animes-themes'] )
+    liste.append( ['Harem',URL_MAIN + 'harem-animes-themes'] )
+    liste.append( ['Idols',URL_MAIN + 'idols-animes-themes'] )
+    liste.append( ['Mafia',URL_MAIN + 'mafia-animes-themes'] )
+    liste.append( ['Magie',URL_MAIN + 'magie-animes-themes'] )
+    liste.append( ['Mechas',URL_MAIN + 'mechas-animes-themes'] )
+    liste.append( ['Militaire',URL_MAIN + 'militaire-animes-themes'] )
+    liste.append( ['Mode',URL_MAIN + 'mode-animes-themes'] )
+    liste.append( ['Monstres',URL_MAIN + 'monstres-animes-themes'] )
+    liste.append( ['Musique',URL_MAIN + 'musique-animes-themes'] )
+    liste.append( ['Ninjas',URL_MAIN + 'ninjas-animes-themes'] )
+    liste.append( ['Nourriture',URL_MAIN + 'nourriture-animes-themes'] )
+    liste.append( ['One Piece',URL_MAIN + 'one-piece-animes-themes'] )
+    liste.append( ['Parodie',URL_MAIN + 'parodie-animes-themes'] )
+    liste.append( ['Politique',URL_MAIN + 'politique-animes-themes'] )
+    liste.append( ['Quotidien',URL_MAIN + 'quotidien-animes-themes'] )
+    liste.append( ['Robots',URL_MAIN + 'robots-animes-themes'] )
+    liste.append( ['Sorcière',URL_MAIN + 'sorciere-animes-themes'] )
+    liste.append( ['Sport',URL_MAIN + 'sport-animes-themes'] )
+    liste.append( ['Super Pouvoirs',URL_MAIN + 'super-pouvoirs-animes-themes'] )
+    liste.append( ['Super-héros',URL_MAIN + 'super-heros-animes-themes'] )
+    liste.append( ['Vampires',URL_MAIN + 'vampires-animes-themes'] )
+    liste.append( ['Vengeance',URL_MAIN + 'vengeance-animes-themes'] )
+    liste.append( ['Voyage temporel',URL_MAIN + 'voyage-temporel-animes-themes'] )
+    liste.append( ['Youkai',URL_MAIN + 'youkai-animes-themes'] )
+    liste.append( ['Zombies',URL_MAIN + 'zombies-animes-themes'] )
+    
+    for sTitle,sUrl in liste:
+        
+        oOutputParameterHandler = cOutputParameterHandler()
+        oOutputParameterHandler.addParameter('siteUrl', sUrl)
+        oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'animes_genres.png', oOutputParameterHandler)
+        
+    oGui.setEndOfDirectory()
+
 def showQlt():
     oGui = cGui()
 
