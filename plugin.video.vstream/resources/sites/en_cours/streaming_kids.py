6fe2bb4fdc92e904dd5344e1f539cd0e359ecd50
razorex <individuel@free.fr>
2017-04-20 23:55:19 +0200

    Update streaming_kids.py

--- plugin.video.vstream/resources/sites/en_cours/streaming_kids.py
+++ plugin.video.vstream/resources/sites/en_cours/streaming_kids.py
@@ -14,28 +14,27 @@ import unicodedata,htmlentitydefs
 
 
 SITE_IDENTIFIER = 'streaming_kids_com'
-SITE_NAME = 'Streaming-kids.com' 
+SITE_NAME = 'Streaming-kids' 
 SITE_DESC = 'Jeunesse Films' 
 
 URL_MAIN = 'http://streaming-kids.com/'
 
-URL_SEARCH = ('http://www.fullmoviz.org/?s=', 'showMovies')
+URL_SEARCH = (URL_MAIN + '?s=', 'showMovies')
 FUNCTION_SEARCH = 'showMovies'
 
-MOVIE_NEWS = ('http://streaming-kids.com/film-animation-en-streaming', 'showMovies')
+MOVIE_NEWS = (URL_MAIN + 'film-animation-en-streaming', 'showMovies')
 
 def load():
     oGui = cGui() 
-
+    
     oOutputParameterHandler = cOutputParameterHandler()
     oOutputParameterHandler.addParameter('siteUrl', 'http://venom/')
-    oGui.addDir(SITE_IDENTIFIER, 'showSearch', 'Recherche', 'search.png', oOutputParameterHandler) 
-   
+    oGui.addDir(SITE_IDENTIFIER, 'showSearch', 'Recherche', 'search.png', oOutputParameterHandler)
     
     oOutputParameterHandler = cOutputParameterHandler()
     oOutputParameterHandler.addParameter('siteUrl', MOVIE_NEWS[0])
-    oGui.addDir(SITE_IDENTIFIER, MOVIE_NEWS[1], 'Films Nouveautés', 'news.png', oOutputParameterHandler)
-	         
+    oGui.addDir(SITE_IDENTIFIER, MOVIE_NEWS[1], 'Films (Derniers ajouts)', 'films_news.png', oOutputParameterHandler)
+    
     oGui.setEndOfDirectory()
 
 def showSearch(): 
@@ -46,11 +45,11 @@ def showSearch():
         sUrl = URL_SEARCH[0] + sSearchText
         showMovies(sUrl)
         oGui.setEndOfDirectory()
-        return  
+        return
    
-def showGenre(): 
+def showGenres(): 
     oGui = cGui()
- 
+    
     liste = []
     liste.append( ['Action','http://full-streaming.org/action/'] )
     liste.append( ['Animation','http://full-streaming.org/animation/'] )
@@ -62,11 +61,11 @@ def showGenre():
     liste.append( ['Comedie Musicale','http://full-streaming.org/comedie-musicale/'] )
     liste.append( ['Documentaire','http://full-streaming.org/documentaire/'] )
     liste.append( ['Drame','http://full-streaming.org/drame/'] )
-    liste.append( ['Epouvante Horreur','http://full-streaming.org/epouvante-horreur/'] ) 
+    liste.append( ['Epouvante Horreur','http://full-streaming.org/epouvante-horreur/'] )
     liste.append( ['Erotique','http://full-streaming.org/erotique'] )
     liste.append( ['Espionnage','http://full-streaming.org/espionnage/'] )
     liste.append( ['Famille','http://full-streaming.org/famille/'] )
-    liste.append( ['Fantastique','http://full-streaming.org/fantastique/'] )  
+    liste.append( ['Fantastique','http://full-streaming.org/fantastique/'] )
     liste.append( ['Guerre','http://full-streaming.org/guerre/'] )
     liste.append( ['Historique','http://full-streaming.org/historique/'] )
     liste.append( ['Musical','http://full-streaming.org/musical/'] )
@@ -77,16 +76,16 @@ def showGenre():
     liste.append( ['Spectacle','http://full-streaming.org/spectacle/'] )
     liste.append( ['Thriller','http://full-streaming.org/thriller/'] )
     liste.append( ['Western','http://full-streaming.org/western/'] )
-    liste.append( ['Divers','http://full-streaming.org/divers/'] ) 
-                
+    liste.append( ['Divers','http://full-streaming.org/divers/'] )
+
     for sTitle,sUrl in liste:#boucle
         
         oOutputParameterHandler = cOutputParameterHandler()
         oOutputParameterHandler.addParameter('siteUrl', sUrl)
         oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'genres.png', oOutputParameterHandler)
-        
+
     oGui.setEndOfDirectory() 
-		
+
 def showMovies(sSearch = ''):
     oGui = cGui()
     if sSearch:
@@ -94,51 +93,50 @@ def showMovies(sSearch = ''):
     else:
         oInputParameterHandler = cInputParameterHandler()
         sUrl = oInputParameterHandler.getValue('siteUrl') # recupere l'url sortie en parametre
-   
+
     oRequestHandler = cRequestHandler(sUrl)
     sHtmlContent = oRequestHandler.request()
-    
+
     sPattern = '>([^<]+) .+?<a href="film-streaming/([^<]+)">([^<]+)<img src=".+?title="(.+?)"'
     #pour faire simple recherche ce bout de code dans le code source de l'url
     #- ([^<]+) je veut cette partie de code mais y a une suite
     #- .+? je ne veut pas cette partis et peux importe ceux qu'elle contient
     #- (.+?) je veut cette partis et c'est la fin
-    
+
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
     print aResult #Commenter ou supprimer cette ligne une foix fini
-    
+
     if (aResult[0] == True):
         total = len(aResult[1])
         dialog = cConfig().createDialog(SITE_NAME)
-        
+
         for aEntry in aResult[1]:
             cConfig().updateDialog(dialog, total)
-	
+
             sTitle = aEntry[1]
             oOutputParameterHandler = cOutputParameterHandler()
-            oOutputParameterHandler.addParameter('siteUrl', 'http://streaming-kids.com/film-streaming/'+aEntry[1])
+            oOutputParameterHandler.addParameter('siteUrl', URL_MAIN + 'film-streaming/' + aEntry[1])
             oOutputParameterHandler.addParameter('sMovieTitle', str(aEntry[1]))
             oOutputParameterHandler.addParameter('sThumbnail', str(aEntry[0])) 
 
             if '/series' in sUrl:
                 oGui.addTV(SITE_IDENTIFIER, 'seriesHosters', sTitle,'', aEntry[0], aEntry[1], oOutputParameterHandler)
-   
+
             else:
                 oGui.addMovie(SITE_IDENTIFIER, 'showHosters', sTitle, '', aEntry[0], aEntry[1], oOutputParameterHandler)
-            
+
         cConfig().finishDialog(dialog)
-           
+
         sNextPage = __checkForNextPage(sUrl)
         if (sNextPage != False):
             oOutputParameterHandler = cOutputParameterHandler()
             oOutputParameterHandler.addParameter('siteUrl', sNextPage)
             oGui.addDir(SITE_IDENTIFIER, 'showMovies', '[COLOR teal]Next >>>[/COLOR]', 'next.png', oOutputParameterHandler)
-        
-    if not sSearch:
-        oGui.setEndOfDirectory() 
 
-	
+    if not sSearch:
+        oGui.setEndOfDirectory()
+
 def __checkForNextPage(sUrl):
     if '/page/' not in sUrl:
         return sUrl + '/page/2'
@@ -147,9 +145,9 @@ def __checkForNextPage(sUrl):
     if (aResult[0] == True):
         cConfig().log(str(aResult[1][0][0]) + '/page/' + str(int (aResult[1][0][1])+1))
         return str(aResult[1][0][0]) + '/page/' + str(int (aResult[1][0][1])+1)
- 
+
     return False
-   
+
 def showHosters():
     oGui = cGui() 
     oInputParameterHandler = cInputParameterHandler()
@@ -159,7 +157,6 @@ def showHosters():
     
     oRequestHandler = cRequestHandler(sUrl) 
     sHtmlContent = oRequestHandler.request(); 
-    
 
     oParser = cParser()
     sPattern = '<iframe.+?src="([^<]+)"'
@@ -167,7 +164,7 @@ def showHosters():
    
     if (aResult[0] == True):
         for aEntry in aResult[1]:
-            
+
             sHosterUrl = str(aEntry)
             oHoster = cHosterGui().checkHoster(sHosterUrl) 
             if (oHoster != False):
@@ -176,7 +173,7 @@ def showHosters():
                 cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumbnail) 
 
     oGui.setEndOfDirectory()
-   
+
 def seriesHosters():
     oGui = cGui()
     oInputParameterHandler = cInputParameterHandler()
@@ -193,7 +190,7 @@ def seriesHosters():
     aResult = oParser.parse(sHtmlContent, sPattern)
     if (aResult[0] == True):
         for aEntry in aResult[1]:
-            
+
             sHosterUrl = str(aEntry[0])
             oHoster = cHosterGui().checkHoster(sHosterUrl)
             if (oHoster != False):
