5fc79ac40cab13525bd5891bec1df2153d9d71d6
Kodigoal <Kodigoal@users.noreply.github.com>
2017-04-24 21:51:03 +0200

    oops

--- plugin.video.vstream/resources/sites/replay_documentaire_com.py
+++ plugin.video.vstream/resources/sites/replay_documentaire_com.py
@@ -10,7 +10,7 @@ from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.lib.config import cConfig
 from resources.lib.util import cUtil
-import re, urllib2
+import re
 
 SITE_IDENTIFIER = 'replay_documentaire_com'
 SITE_NAME = 'replay-documentaire'
@@ -28,20 +28,6 @@ REPLAYTV_GENRES = (True, 'showGenres')
 
 URL_SEARCH = ('http://replay-streaming.com/?s=', 'showMovies')
 FUNCTION_SEARCH = 'showMovies'
-
-
-#bypass soucis ssl/tls SNI <kodi17
-def sslBypass(url):
-
-    UA = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
-    headers = { 'User-Agent' : UA }
-    link = 'https://ooops.rf.gd/url.php?url=' + url
-    request = urllib2.Request(link,None, headers)
-    reponse = urllib2.urlopen(request)
-    sHtmlContent = reponse.read()
-    reponse.close()
-    return sHtmlContent
-
 
 def load():
     oGui = cGui()
@@ -96,12 +82,9 @@ def showMovies(sSearch = ''):
     else:
         oInputParameterHandler = cInputParameterHandler()
         sUrl = oInputParameterHandler.getValue('siteUrl')
-   
-    try:
-       oRequestHandler = cRequestHandler(sUrl)
-       sHtmlContent = oRequestHandler.request()
-    except:
-       sHtmlContent = sslBypass(sUrl)
+    
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request() 
     
     sPattern = '<a class="img-holder" href="([^"]+)" title="([^"]+)" style="background-image: url\(([^\)]+)\);"><\/a>'
     
@@ -138,11 +121,11 @@ def __checkForNextPage(sHtmlContent):
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
     if (aResult[0] == True):
-        
         url = aResult[1][0]
         if url.startswith('//'):
            url = 'http:' + url
         return url
+
     return False
 
 def showHosters():
@@ -152,14 +135,10 @@ def showHosters():
     sMovieTitle = oInputParameterHandler.getValue('sMovieTitle')
     sThumbnail = oInputParameterHandler.getValue('sThumbnail')
     
-    try:
-       oRequestHandler = cRequestHandler(sUrl)
-       sHtmlContent = oRequestHandler.request();
-    except:
-           sHtmlContent = sslBypass(sUrl)
-
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request();
     #sHtmlContent = sHtmlContent.replace('<iframe src="//www.facebook.com/plugins/like.php','').replace('<iframe src="http://www.facebook.com/plugins/likebox.php','')
-           
+               
     sPattern = 'data-lazy-src="([^<]+)/.*?.mp4" scrolling="no"'
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
