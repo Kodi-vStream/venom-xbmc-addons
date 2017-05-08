de5655262e862ef5287ac81e81be7fda890e4fc2
johngf <john59650@live.fr>
2017-05-08 10:27:48 +0200

    Update replay_documentaire_com.py

--- plugin.video.vstream/resources/sites/replay_documentaire_com.py
+++ plugin.video.vstream/resources/sites/replay_documentaire_com.py
@@ -1,16 +1,12 @@
 #-*- coding: utf-8 -*-
 #zombi.
 from resources.lib.gui.hoster import cHosterGui
-from resources.lib.handler.hosterHandler import cHosterHandler
 from resources.lib.gui.gui import cGui
-from resources.lib.gui.guiElement import cGuiElement
 from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
-from resources.lib.config import cConfig
-from resources.lib.util import cUtil
-import re
+from resources.lib import util
 
 SITE_IDENTIFIER = 'replay_documentaire_com'
 SITE_NAME = 'replay-documentaire'
@@ -18,11 +14,11 @@ SITE_DESC = 'Revoir très facilement tous vos documentaires en replay streaming'
 
 URL_MAIN = 'http://www.replay-documentaire.com/'
 
-
-DOC_NEWS = ('http://www.replay-documentaire.com/cat%C3%A9gories/documentaire/', 'showMovies')
+DOC_NEWS = (URL_MAIN + 'cat%C3%A9gories/documentaire/', 'showMovies')
 DOC_DOCS = ('http://', 'load')
+DOC_GENRES = (True, 'showGenres')
 
-REPLAYTV_NEWS = ('http://www.replay-documentaire.com/', 'showMovies')
+REPLAYTV_NEWS = (URL_MAIN, 'showMovies')
 REPLAYTV_REPLAYTV = ('http://', 'load')
 REPLAYTV_GENRES = (True, 'showGenres')
 
@@ -41,40 +37,48 @@ def load():
     oGui.addDir(SITE_IDENTIFIER, DOC_NEWS[1], 'Documentaires', 'doc.png', oOutputParameterHandler)
     
     oOutputParameterHandler = cOutputParameterHandler()
-    oOutputParameterHandler.addParameter('siteUrl', 'http://frenchstream.org/les-plus-vues')
+    oOutputParameterHandler.addParameter('siteUrl', 'http://doc')
     oGui.addDir(SITE_IDENTIFIER, 'showGenres', 'Documentaires Genres', 'genres.png', oOutputParameterHandler)    
-            
+  
     oGui.setEndOfDirectory()
   
 def showSearch():
     oGui = cGui()
     sSearchText = oGui.showKeyBoard()
     if (sSearchText != False):
-            sUrl = 'http://replay-streaming.com/?s='+sSearchText  
-            showMovies(sUrl)
-            oGui.setEndOfDirectory()
-            return  
+        sUrl = 'http://replay-streaming.com/?s='+sSearchText  
+        showMovies(sUrl)
+        oGui.setEndOfDirectory()
+        return  
     
 def showGenres():
     oGui = cGui()
-    oInputParameterHandler = cInputParameterHandler()
-    sUrl = oInputParameterHandler.getValue('siteUrl')
- 
-    liste = []
-    liste.append( ["telefilm","http://replay-streaming.com/categorie/telefilm/"] )
-    liste.append( ["spectacle","http://replay-streaming.com/categorie/spectacle/"] )
-    liste.append( ["concert","http://replay-streaming.com/categorie/concert/"] )
-    liste.append( ["series","http://replay-streaming.com/categorie/series/"] )
-    liste.append( ["telefoot","http://replay-streaming.com/categorie/telefoot/"] )
-                
-    for sTitle,sUrl in liste:
-        
-        oOutputParameterHandler = cOutputParameterHandler()
-        oOutputParameterHandler.addParameter('siteUrl', sUrl)
-        oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'genres.png', oOutputParameterHandler)
-       
-    oGui.setEndOfDirectory() 
+    oParser = cParser()
+    oRequestHandler = cRequestHandler(URL_MAIN)
+    sHtmlContent = oRequestHandler.request()
 
+    oParser = cParser()
+    sPattern = 'value="(\d+)">(.+?)<\/option>'
+    aResult = oParser.parse(sHtmlContent, sPattern)
+    if (aResult[0] == True):
+        total = len(aResult[1])
+        dialog = util.createDialog(SITE_NAME)
+        for aEntry in aResult[1]:
+            util.updateDialog(dialog, total)
+            if dialog.iscanceled():
+                break
+                
+            sTitle = aEntry[1].replace('&prime;','')
+            sUrl = URL_MAIN +'catégories/'+ sTitle.replace(' ','-')
+
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl', sUrl)
+            oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle , 'replay.png', oOutputParameterHandler)
+            
+        util.finishDialog(dialog)
+        
+    oGui.setEndOfDirectory()
+    
 def showMovies(sSearch = ''):
     oGui = cGui()
     if sSearch:
@@ -85,38 +89,43 @@ def showMovies(sSearch = ''):
     
     oRequestHandler = cRequestHandler(sUrl)
     sHtmlContent = oRequestHandler.request() 
-    
-    sPattern = '<a class="img-holder" href="([^"]+)" title="([^"]+)" style="background-image: url\(([^\)]+)\);"><\/a>'
-    
+
+    sPattern = '<a class="img.+?" href="([^"]+)" title="([^"]+)" style="background-image: url([^"]+)"'
+
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
     if (aResult[0] == True):
         total = len(aResult[1])
-        dialog = cConfig().createDialog(SITE_NAME)
+        dialog = util.createDialog(SITE_NAME)
         for aEntry in aResult[1]:
-            cConfig().updateDialog(dialog, total)
+            util.updateDialog(dialog, total)
             if dialog.iscanceled():
                 break
-            
-            oOutputParameterHandler = cOutputParameterHandler()
-            oOutputParameterHandler.addParameter('siteUrl', str(aEntry[0]))
-            oOutputParameterHandler.addParameter('sMovieTitle', str(aEntry[1]))
-            oOutputParameterHandler.addParameter('sThumbnail', str(aEntry[2]))
-            oGui.addMisc(SITE_IDENTIFIER, 'showHosters', aEntry[1], 'doc.png', aEntry[2], "", oOutputParameterHandler)
+                
+            sThumb = aEntry[2].replace("(",'').replace(")",'').replace(';','').replace("'",'') #nettoyage
+            sTitle = aEntry[1].replace('en replay','').replace('&prime;','')#nettoyage
 
-        cConfig().finishDialog(dialog)
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl', aEntry[0])
+            oOutputParameterHandler.addParameter('sMovieTitle', sTitle)
+            oOutputParameterHandler.addParameter('sThumbnail', sThumb)
+            oGui.addMisc(SITE_IDENTIFIER, 'showHosters', sTitle, 'doc.png', sThumb, "", oOutputParameterHandler)
+
+        util.finishDialog(dialog)
             
         sNextPage = __checkForNextPage(sHtmlContent)
         if (sNextPage != False):
             oOutputParameterHandler = cOutputParameterHandler()
             oOutputParameterHandler.addParameter('siteUrl', sNextPage)
             oGui.addDir(SITE_IDENTIFIER, 'showMovies', '[COLOR teal]Next >>>[/COLOR]', 'next.png', oOutputParameterHandler)
-
+    else:
+        util.VSshowInfo('Vstream','Aucune vidéos',2)
+        return
+        
     if not sSearch:
         oGui.setEndOfDirectory()
 
 def __checkForNextPage(sHtmlContent):
-    #sPattern = 'id="tie-next-page"> <a href="([^<]+)">'
     sPattern = '<link rel="next" href="([^"]+)"/>'
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
@@ -136,17 +145,16 @@ def showHosters():
     sThumbnail = oInputParameterHandler.getValue('sThumbnail')
     
     oRequestHandler = cRequestHandler(sUrl)
-    sHtmlContent = oRequestHandler.request();
-    #sHtmlContent = sHtmlContent.replace('<iframe src="//www.facebook.com/plugins/like.php','').replace('<iframe src="http://www.facebook.com/plugins/likebox.php','')
-               
+    sHtmlContent = oRequestHandler.request()
+
     sPattern = 'data-lazy-src="([^<]+)/.*?.mp4" scrolling="no"'
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
     if (aResult[0] == True):
         total = len(aResult[1])
-        dialog = cConfig().createDialog(SITE_NAME)
+        dialog = util.createDialog(SITE_NAME)
         for aEntry in aResult[1]:
-            cConfig().updateDialog(dialog, total)
+            util.updateDialog(dialog, total)
             if dialog.iscanceled():
                 break
             
@@ -161,6 +169,6 @@ def showHosters():
                 oHoster.setFileName(sMovieTitle)
                 cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumbnail)
 
-        cConfig().finishDialog(dialog) 
-                
+        util.finishDialog(dialog) 
+   
     oGui.setEndOfDirectory()
