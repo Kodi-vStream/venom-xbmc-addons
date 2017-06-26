82c1ad4d315e0bbe44fc037d36f517364915e738
razorex <individuel@free.fr>
2017-06-26 15:49:12 +0200

    Update serie_streaminghd.py

--- plugin.video.vstream/resources/sites/serie_streaminghd.py
+++ plugin.video.vstream/resources/sites/serie_streaminghd.py
@@ -148,8 +148,7 @@ def __checkForNextPage(sHtmlContent):
     aResult = oParser.parse(sHtmlContent, sPattern)
     
     if (aResult[0] == True):
-        sUrl = aResult[1][0]
-        return sUrl 
+        return aResult[1][0]
 
     return False
 
