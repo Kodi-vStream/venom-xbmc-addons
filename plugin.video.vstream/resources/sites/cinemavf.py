c401611d5e85cfe9fc656863241ca68d94cafe67
razorex <individuel@free.fr>
2017-08-18 14:04:52 +0200

    Update cinemavf.py
    Genres par liste, écriture différentes d'un thèmes à l’autre et d'un genres à l’autre.

--- plugin.video.vstream/resources/sites/cinemavf.py
+++ plugin.video.vstream/resources/sites/cinemavf.py
@@ -25,15 +25,15 @@ FUNCTION_SEARCH = 'showMovies'
 
 MOVIE_NEWS = (URL_MAIN , 'showMovies')
 MOVIE_MOVIE = (URL_MAIN + 'film-en-streaming', 'showMovies')
-MOVIE_GENRES = (URL_MAIN + 'film-en-streaming', 'showGenres')
+MOVIE_GENRES = (True, 'showMovieGenres')
 
 SERIE_NEWS = (URL_MAIN + 'serie/', 'showMovies')
 SERIE_SERIES = (URL_MAIN + 'serie/', 'showMovies')
-SERIE_GENRES = (URL_MAIN + 'serie/', 'showGenres')
+SERIE_GENRES = (True, 'showSerieGenres')
 
 ANIM_NEWS = (URL_MAIN + 'mangas/', 'showMovies')
 ANIM_ANIMS = (URL_MAIN + 'mangas/', 'showMovies')
-ANIM_GENRES = (URL_MAIN + 'mangas/', 'showGenres')
+ANIM_GENRES = (True, 'showMangaGenres')
 
 def load():
     oGui = cGui()
@@ -82,41 +82,132 @@ def showSearch():
         oGui.setEndOfDirectory()
         return
 
-def showGenres():
+def showMovieGenres():
     oGui = cGui()
-    oInputParameterHandler = cInputParameterHandler()
-    sUrl = oInputParameterHandler.getValue('siteUrl')
-    
-    if 'film' in sUrl:
-        reqURL = MOVIE_GENRES[0]
-        sStart = '<h3 class="widget-title">Genres du films</h3>'
-        sEnd = '<footer id="footer">'
-    elif 'serie' in sUrl:
-        reqURL = SERIE_GENRES[0]
-        sStart = '<h3 class="widget-title">Catégories</h3>'
-        sEnd = '<footer id="footer">'
-    else:
-        reqURL = ANIM_GENRES[0]
-        sStart = '<h3 class="widget-title">Catégories</h3>'
-        sEnd = '<footer id="footer">'
-        
-    oParser = cParser()
-    
-    oRequestHandler = cRequestHandler(reqURL)
-    sHtmlContent = oRequestHandler.request()
 
-    sHtmlContent = oParser.abParse(sHtmlContent, sStart, sEnd)
+    liste = []
+    liste.append( ['Action',URL_MAIN + 'category/action'] )
+    liste.append( ['Animation',URL_MAIN + 'category/animation'] )
+    liste.append( ['Arts Martiaux',URL_MAIN + 'category/arts-martiaux'] )
+    liste.append( ['Aventure',URL_MAIN + 'category/aventure'] )
+    liste.append( ['Biopic',URL_MAIN + 'category/biopic'] )
+    liste.append( ['Classique',URL_MAIN + 'category/classique'] )
+    liste.append( ['Comédie',URL_MAIN + 'category/comedie'] )
+    liste.append( ['Comédie Dramatique',URL_MAIN + 'category/comedie-dramatique'] )
+    liste.append( ['Concert',URL_MAIN + 'category/concert'] )
+    liste.append( ['Divers',URL_MAIN + 'category/divers'] )
+    liste.append( ['Documentaire',URL_MAIN + 'category/documentaire-exclusive'] )
+    liste.append( ['Dramatique',URL_MAIN + 'category/dramatique'] )
+    liste.append( ['Drame',URL_MAIN + 'category/drame'] )
+    liste.append( ['Emissions TV',URL_MAIN + 'category/emissions-tv'] )
+    liste.append( ['Epouvante Horreur',URL_MAIN + 'category/epouvante-horreur'] )
+    liste.append( ['Erotique',URL_MAIN + 'category/erotique'] )
+    liste.append( ['Espionnage',URL_MAIN + 'category/espionnage'] )
+    liste.append( ['Expérimental',URL_MAIN + 'category/experimental'] )
+    liste.append( ['Famille',URL_MAIN + 'category/famille'] )
+    liste.append( ['Fantastique',URL_MAIN + 'category/fantastique'] )
+    liste.append( ['Guerre',URL_MAIN + 'category/guerre'] )
+    liste.append( ['Historique',URL_MAIN + 'category/historique'] )
+    liste.append( ['Horreur',URL_MAIN + 'category/horreur'] )
+    liste.append( ['Humour',URL_MAIN + 'category/humour'] )
+    liste.append( ['Judiciaire',URL_MAIN + 'category/judiciaire'] )
+    liste.append( ['Manga',URL_MAIN + 'category/manga'] )
+    liste.append( ['Musical',URL_MAIN + 'category/musical'] )
+    liste.append( ['Opéra',URL_MAIN + 'category/opera'] )
+    liste.append( ['Péplum',URL_MAIN + 'category/peplum'] )
+    liste.append( ['Policier',URL_MAIN + 'category/policier'] )
+    liste.append( ['Romance',URL_MAIN + 'category/romance'] )
+    liste.append( ['Science Fiction',URL_MAIN + 'category/science-fiction'] )
+    liste.append( ['Show',URL_MAIN + 'category/show'] )
+    liste.append( ['Spectacle',URL_MAIN + 'category/spectacle'] )
+    liste.append( ['Sport',URL_MAIN + 'category/sport'] )
+    liste.append( ['Téléréalité',URL_MAIN + 'category/telerealite'] )
+    liste.append( ['Thriller',URL_MAIN + 'category/thriller'] )
+    liste.append( ['Vieux films',URL_MAIN + 'category/vieux-films-2'] )
+    liste.append( ['Walt Disney',URL_MAIN + 'category/walt-disney'] )
+    liste.append( ['Western',URL_MAIN + 'category/western'] )
 
-    sPattern = '<li class="cat-item cat-item.+?"><a href="([^"]+)".+?>(.+?)</a>'
-    aResult = oParser.parse(sHtmlContent, sPattern)
-    if (aResult[0] == True):
-        for aEntry in aResult[1]:
-            sUrl = str(aEntry[0])
-            sTitle = str(aEntry[1])
-            
-            oOutputParameterHandler = cOutputParameterHandler()
-            oOutputParameterHandler.addParameter('siteUrl', sUrl)
-            oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'genres.png', oOutputParameterHandler)
+    for sTitle,sUrl in liste:
+
+        oOutputParameterHandler = cOutputParameterHandler()
+        oOutputParameterHandler.addParameter('siteUrl', sUrl)
+        oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'films_genres.png', oOutputParameterHandler)
+
+    oGui.setEndOfDirectory()
+
+def showSerieGenres():
+    oGui = cGui()
+
+    liste = []
+    liste.append( ['Action',URL_MAIN + 'serie/category/action'] )
+    liste.append( ['Animation',URL_MAIN + 'serie/category/animation'] )
+    liste.append( ['Aventure',URL_MAIN + 'serie/category/aventure'] )
+    liste.append( ['Comédie',URL_MAIN + 'serie/category/comedie'] )
+    liste.append( ['Comédie Dramatique',URL_MAIN + 'serie/category/comedie-dramatique'] )
+    liste.append( ['Documentaire',URL_MAIN + 'serie/category/documentaire'] )
+    liste.append( ['Drame',URL_MAIN + 'serie/category/drame'] )
+    liste.append( ['Emissions TV',URL_MAIN + 'serie/category/emissions-tv'] )
+    liste.append( ['Epouvante Horreur',URL_MAIN + 'serie/category/epouvante-horreur'] )
+    liste.append( ['Erotique',URL_MAIN + 'serie/category/erotique'] )
+    liste.append( ['Espionnage',URL_MAIN + 'serie/category/espionnage'] )
+    liste.append( ['Famille',URL_MAIN + 'serie/category/famille'] )
+    liste.append( ['Fantastique',URL_MAIN + 'serie/category/fantastique'] )
+    liste.append( ['Guerre',URL_MAIN + 'serie/category/guerre'] )
+    liste.append( ['Historique',URL_MAIN + 'serie/category/historique'] )
+    liste.append( ['Judiciaire',URL_MAIN + 'serie/category/judiciaire'] )
+    liste.append( ['Médical',URL_MAIN + 'serie/category/medical'] )
+    liste.append( ['Musical',URL_MAIN + 'serie/category/musical'] )
+    liste.append( ['Péplum',URL_MAIN + 'serie/category/peplum'] )
+    liste.append( ['Policier',URL_MAIN + 'serie/category/policier'] )
+    liste.append( ['Romance',URL_MAIN + 'serie/category/romance'] )
+    liste.append( ['Science Fiction',URL_MAIN + 'serie/category/science-fiction'] )
+    liste.append( ['Thriller',URL_MAIN + 'serie/category/thriller'] )
+    liste.append( ['Western',URL_MAIN + 'serie/category/western'] )
+
+    for sTitle,sUrl in liste:
+
+        oOutputParameterHandler = cOutputParameterHandler()
+        oOutputParameterHandler.addParameter('siteUrl', sUrl)
+        oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'series_genres.png', oOutputParameterHandler)
+
+    oGui.setEndOfDirectory()
+
+def showMangaGenres():
+    oGui = cGui()
+
+    liste = []
+    liste.append( ['Action',URL_MAIN + 'mangas/category/action'] )
+    liste.append( ['Amitié',URL_MAIN + 'mangas/category/amitie'] )
+    liste.append( ['Amour',URL_MAIN + 'mangas/category/amour-love'] )
+    liste.append( ['Animation',URL_MAIN + 'mangas/category/animation'] )
+    liste.append( ['Arts Martiaux',URL_MAIN + 'mangas/category/arts-martiaux'] )
+    liste.append( ['Aventure',URL_MAIN + 'mangas/category/aventure'] )
+    liste.append( ['Comédie',URL_MAIN + 'mangas/category/comedie'] )
+    liste.append( ['Comédie Dramatique',URL_MAIN + 'mangas/category/comedie-drama'] )
+    liste.append( ['Dramatique',URL_MAIN + 'mangas/category/dramatique'] )
+    liste.append( ['Drame',URL_MAIN + 'mangas/category/drame'] )
+    liste.append( ['Epouvante Horreur',URL_MAIN + 'mangas/category/epouvante-horreur'] )
+    liste.append( ['Erotique',URL_MAIN + 'mangas/category/erotique'] )
+    liste.append( ['Famille',URL_MAIN + 'mangas/category/famille'] )
+    liste.append( ['Fantastique',URL_MAIN + 'mangas/category/fantastique'] )
+    liste.append( ['Guerre',URL_MAIN + 'mangas/category/guerre'] )
+    liste.append( ['Historique',URL_MAIN + 'mangas/category/historique'] )
+    liste.append( ['Horreur',URL_MAIN + 'mangas/category/horreur'] )
+    liste.append( ['Humour',URL_MAIN + 'mangas/category/humour'] )
+    liste.append( ['Musical',URL_MAIN + 'mangas/category/musical'] )
+    liste.append( ['Non classé',URL_MAIN + 'mangas/category/non-classe'] )
+    liste.append( ['Policier',URL_MAIN + 'mangas/category/policier'] )
+    liste.append( ['Romance',URL_MAIN + 'mangas/category/romance'] )
+    liste.append( ['Science Fiction',URL_MAIN + 'mangas/category/science-fiction'] )
+    liste.append( ['Show',URL_MAIN + 'mangas/category/show'] )
+    liste.append( ['Sport',URL_MAIN + 'mangas/category/sport'] )
+    liste.append( ['Thriller',URL_MAIN + 'mangas/category/thriller'] )
+
+    for sTitle,sUrl in liste:
+
+        oOutputParameterHandler = cOutputParameterHandler()
+        oOutputParameterHandler.addParameter('siteUrl', sUrl)
+        oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'animes_genres.png', oOutputParameterHandler)
 
     oGui.setEndOfDirectory()
 
@@ -131,7 +222,7 @@ def showMovies(sSearch = ''):
     oRequestHandler = cRequestHandler(sUrl)
     sHtmlContent = oRequestHandler.request()
     
-    sPattern = '<div class="thumb">.+?<img src="([^<]+)" alt="(.+?)".+?>.+?<a href="(.+?)".+?'
+    sPattern = '<div class="thumb">.+?<img src="([^<]+)" alt="(.+?)".+?<a href="(.+?)"'
     
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
