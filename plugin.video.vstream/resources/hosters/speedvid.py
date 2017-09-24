eed8a44eede5b5d57b4b3b02caa4e2c5a4174708
TmpName <gihubtmp@yopmail.com>
2017-09-24 13:24:12 +0200

    Update speedvid.py
    Dsl de te gacher ton boulot, tres bon code, mais il va deconner a certain moment par exemple l'url chnage aleatoirement elle peut etre
    http://speedvd/blabla
    //spedvid/blabla
    /babla
    
    et suis pas tres fan pr bombarder le site jsunpack

--- plugin.video.vstream/resources/hosters/speedvid.py
+++ plugin.video.vstream/resources/hosters/speedvid.py
@@ -1,13 +1,13 @@
-from resources.lib.handler.requestHandler import cRequestHandler,MPencode
+from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
 from resources.lib.packer import cPacker
-# from resources.lib.util import VSlog
-#from resources.lib.config import cConfig
+
+from resources.lib.config import cConfig
 
 #from resources.lib.jsparser import JsParser
 
-import re,xbmcgui,urllib2,urllib
+import re,xbmcgui
 
 class cHoster(iHoster):
 
@@ -68,86 +68,114 @@ class cHoster(iHoster):
         return self.__getMediaLinkForGuest()
 
     def __getMediaLinkForGuest(self):
-        UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0'
-        headers = { 'User-Agent' : UA }
-        
+    
         oRequest = cRequestHandler(self.__sUrl)
         oRequest.addHeaderEntry('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0')
         sHtmlContent = oRequest.request()
-
+        
+        #fh = open('c:\\testok.txt', "r")
+        #sHtmlContent=fh.read()
+        #fh.close()
+        
         oParser = cParser()
         
         #Recherche de la vraie page
         Url = ''
         sPattern = '(eval\(function\(p,a,c,k,e(?:.|\s)+?\)\))<'
         aResult = re.findall(sPattern,sHtmlContent)
+        
+        #cConfig().log('nbre >>' + str(len(aResult)))
+        
         if (aResult):
             for packed in aResult:
- 
-                # cConfig().log('>>' + packed)
+                
+                #cConfig().log('>>' + packed)
                 
                 #Recherche du bon bloc
-                if not 'mp4' in packed and not 'FRAMEBORDER' in packed:
-
-                    fields = {'urlin':packed,'private':'on','referer':'http://www.ask.com/web?q=triplepack'}
-                    mpartdata = MPencode(fields)
-
-                    req = urllib2.Request('http://jsunpack.jeek.org/?',mpartdata[1],headers)
-                    req.add_header('Content-Type', mpartdata[0].replace(',',';'))
-                    req.add_header('Content-Length', len(mpartdata[1]))
-                    req.add_header('Connection','keep-alive')
-                    try:
-                        rep = urllib2.urlopen(req)
-                    except:
-                        return ''
-
-                    sHtml = rep.read()
-                    rep.close()
-                    break
-
-            code = re.search('location.href="(.+?)"',sHtml,re.DOTALL)
-            if code:
-                Url = 'http://www.speedvid.net/' + code.group(1)
-
-            
-            oRequest = cRequestHandler(Url)
-            oRequest.addHeaderEntry('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0')
-            oRequest.addHeaderEntry('Referer',self.__sUrl)
-            sHtmlContent = oRequest.request()
-
-            url=[]
-            qua=[]
-            api_call = ''
-        
-            sPattern = '(eval\(function\(p,a,c,k,e(?:.|\s)+?\)\))<'
-            aResult = oParser.parse(sHtmlContent, sPattern)
-            if (aResult[0] == True):
-                for packed in aResult[1]:
+                if not 'IFRAME' in packed:
+                                       
+                    #bidouille rapide
+                    #packed = packed.replace('|mp4','')
+                    #Pas besoin bidouile, faux lien
+                    if '|mp4' in packed:
+                        continue
+                                       
+                    #3 passages necessaires, avec nettoyage
                     sHtmlContent = cPacker().unpack(packed)
-                    sHtmlContent = sHtmlContent.replace('\\','')
-                    if "jwplayer('vplayer').setup" in sHtmlContent:
-                        sPattern2 = "{file:.([^']+.mp4)"
-                        aResult2 = oParser.parse(sHtmlContent, sPattern2)
-                        if (aResult2[0] == True):
-                            api_call = aResult2[1][0]
-                            break
-                            
-                    # # url.append(aResult2[1][0])
-                    # # if 'HD' in aResult2[1][0]:
-                        # # qua.append('HD')
-                    # # else:
-                        # # qua.append('SD')
+                    sHtmlContent = sHtmlContent.decode('string-escape')
+
+                    sHtmlContent = cPacker().unpack(sHtmlContent)
+                    sHtmlContent = sHtmlContent.decode('string-escape')
                     
-        # Si une seule url
-        # # if len(url) == 1:
-            # # api_call = url[0]
-        # si plus de une
-        # # elif len(url) > 1:
-        # Afichage du tableau
-            # # dialog2 = xbmcgui.Dialog()
-            # # ret = dialog2.select('Select Quality', qua)
-            # # if (ret > -1):
-                # # api_call = url[ret]
+                    sHtmlContent = cPacker().unpack(sHtmlContent)
+                    sHtmlContent = sHtmlContent.decode('string-escape')
+                    
+                    #recuperation de l'url
+                    Url = re.findall('href="(.+?)"',sHtmlContent)[0]
+                    if not 'speedvid' in Url:
+                        Url = 'http://www.speedvid.net/' + Url  
+                    if not 'http' in Url:
+                        if Url.startswith('//'):
+                            Url = 'http:' + Url
+                        else:
+                            Url = 'http://' + Url
+
+                    
+                    break
+                    
+        #cConfig().log('ok >> ' + Url)
+        
+        if not Url:
+            return False, False
+
+        oRequest = cRequestHandler(Url)
+        oRequest.addHeaderEntry('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0')
+        oRequest.addHeaderEntry('Referer',self.__sUrl)
+        sHtmlContent = oRequest.request()
+
+        #fh = open('c:\\test.txt', "w")
+        #fh.write(sHtmlContent)
+        #fh.close()
+                
+        #initialisation des tableaux
+        url=[]
+        qua=[]
+        api_call = ''
+        
+        sPattern = '(eval\(function\(p,a,c,k,e(?:.|\s)+?\)\))<'
+        aResult = oParser.parse(sHtmlContent, sPattern)
+        if (aResult[0] == True):
+            for packed in aResult[1]:
+                
+                #cConfig().log(packed)
+
+                #if 'while' not in packed:
+                #    continue
+                
+                sHtmlContent = cPacker().unpack(packed)
+                sHtmlContent = sHtmlContent.replace('\\','')
+                
+                #cConfig().log(sHtmlContent)
+                    
+                sPattern2 = "{file:.([^']+.mp4)"
+                aResult2 = oParser.parse(sHtmlContent, sPattern2)
+                if (aResult2[0] == True):
+                    url.append(aResult2[1][0])
+                    if 'HD' in aResult2[1][0]:
+                        qua.append('HD')
+                    else:
+                        qua.append('SD')
+                    
+        #Si une seule url
+        if len(url) == 1:
+            api_call = url[0]
+        #si plus de une
+        elif len(url) > 1:
+        #Afichage du tableau
+            dialog2 = xbmcgui.Dialog()
+            ret = dialog2.select('Select Quality', qua)
+            if (ret > -1):
+                api_call = url[ret]
 
         if (api_call):
             api_call = api_call + '|User-Agent=Mozilla/5.0 (Windows NT 6.1; WOW64; rv:55.0) Gecko/20100101 Firefox/55.0'
