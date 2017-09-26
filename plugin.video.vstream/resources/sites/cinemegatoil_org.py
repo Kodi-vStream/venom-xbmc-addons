1c0569fc9afde9911da13ed3716b912ef55341f2
chataigne73 <chataigne73@gmail.com>
2017-09-26 10:03:52 +0200

    Add files via upload

---
+++ plugin.video.vstream/resources/sites/cinemegatoil_org.py
@@ -0,0 +1,241 @@
+#-*- coding: utf-8 -*-
+#Venom.
+from resources.lib.gui.hoster import cHosterGui
+from resources.lib.gui.gui import cGui
+from resources.lib.handler.inputParameterHandler import cInputParameterHandler
+from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.config import cConfig
+from resources.lib.parser import cParser
+from resources.lib.util import cUtil
+import re,urllib
+
+UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
+
+SITE_IDENTIFIER = 'cinemegatoil_org'
+SITE_NAME = 'Cinemegatoil'
+SITE_DESC = 'Films - Films HD'
+
+URL_MAIN = 'https://www.cinemegatoil.org/'
+
+MOVIE_NEWS = (URL_MAIN , 'showMovies')
+MOVIE_MOVIE = (URL_MAIN , 'showMovies')
+MOVIE_GENRES = (True, 'showGenres')
+MOVIE_ANNEES = (True, 'showYears')
+
+URL_SEARCH = (URL_MAIN + '?do=search&mode=advanced&subaction=search&titleonly=3&story=', 'showMovies')
+
+def load():
+    oGui = cGui()
+    
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', 'http://venom/')
+    oGui.addDir(SITE_IDENTIFIER, 'showSearch', 'Recherche', 'search.png', oOutputParameterHandler)
+    
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', MOVIE_NEWS[0])
+    oGui.addDir(SITE_IDENTIFIER, MOVIE_NEWS[1], 'Films (Derniers ajouts)', 'films_news.png', oOutputParameterHandler)
+    
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', MOVIE_GENRES[0])
+    oGui.addDir(SITE_IDENTIFIER, MOVIE_GENRES[1], 'Films (Genres)', 'films_genres.png', oOutputParameterHandler)
+    
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', MOVIE_ANNEES[0])
+    oGui.addDir(SITE_IDENTIFIER, MOVIE_ANNEES[1], 'Films (Par Années)', 'films_annees.png', oOutputParameterHandler)
+	
+    oGui.setEndOfDirectory()
+
+def showSearch():
+    oGui = cGui()
+
+    sSearchText = oGui.showKeyBoard()
+    if (sSearchText != False):
+            sUrl = sSearchText
+            showMovies(sUrl)
+            oGui.setEndOfDirectory()
+            return
+
+def showGenres():
+    oGui = cGui()
+    
+    liste = []
+    liste.append( ['Action/Aventure',URL_MAIN + 'action'] )
+    liste.append( ['Animation',URL_MAIN + 'animation/'] )
+    liste.append( ['Aventure',URL_MAIN + 'aventure/'] )
+    liste.append( ['Comédie',URL_MAIN + 'comedie/'] )
+    liste.append( ['Crime',URL_MAIN + 'crime/'] )
+    liste.append( ['Documentaire',URL_MAIN + 'documentaire/'] )
+    liste.append( ['Drame',URL_MAIN + 'drame/'] )
+    liste.append( ['Etranger',URL_MAIN + 'etranger/'] )
+    liste.append( ['Famille',URL_MAIN + 'familial/'] )
+    liste.append( ['Fantastique',URL_MAIN + 'fantastique/'] )
+    liste.append( ['Guerre',URL_MAIN + 'guerre/'] )
+    liste.append( ['Histoire',URL_MAIN + 'histoire/'] )
+    liste.append( ['Horreur',URL_MAIN + 'horreurr/'] )
+    liste.append( ['Musique',URL_MAIN + 'musique/'] )
+    liste.append( ['Mystère',URL_MAIN + 'mystere/'] )
+    liste.append( ['Romance',URL_MAIN + 'romance/'] )
+    liste.append( ['Science-fiction',URL_MAIN + 'science-fiction/'] )
+    liste.append( ['Téléfilm',URL_MAIN + 'telefilm/'] )
+    liste.append( ['Thriller',URL_MAIN + 'thriller/'] )
+    liste.append( ['Western',URL_MAIN + 'western/'] )
+    
+    for sTitle,sUrl in liste:
+       
+        oOutputParameterHandler = cOutputParameterHandler()
+        oOutputParameterHandler.addParameter('siteUrl', sUrl)
+        oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'films_genres.png', oOutputParameterHandler)
+        
+    oGui.setEndOfDirectory()
+
+def showYears():
+    oGui = cGui()
+    
+    sStart = '<a href="#">Année</a>'
+    sEnd = '<a href="#">Pays</a>'
+	
+    oParser = cParser()
+    
+    oRequestHandler = cRequestHandler(URL_MAIN)
+    sHtmlContent = oRequestHandler.request()
+	
+    sHtmlContent = oParser.abParse(sHtmlContent, sStart, sEnd)
+	
+    sPattern = '<a href="([^"]+)">(.+?)</a>'
+    aResult = oParser.parse(sHtmlContent, sPattern)
+    if (aResult[0] == True):
+        for aEntry in aResult[1]:
+            sUrl = URL_MAIN[:-1] + aEntry[0]
+            #print sUrl
+            sTitle = aEntry[1]
+            
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl', sUrl)
+            oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'annees.png', oOutputParameterHandler)
+			
+    oGui.setEndOfDirectory()
+
+def showMovies(sSearch = ''):
+    oGui = cGui()
+    if sSearch:
+        if URL_SEARCH[0] in sSearch:
+            sUrl = sSearch
+        else:
+            sUrl = URL_SEARCH[0] + sSearch
+    else:
+        oInputParameterHandler = cInputParameterHandler()
+        sUrl = oInputParameterHandler.getValue('siteUrl')
+    #print ('sUrl=', sUrl)
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request()
+    #print ('sHtmlContent=', sHtmlContent)
+    oParser = cParser()
+    
+    sPattern = '<div class="short_content"> *<a href="([^"]+)"> *<img src="([^"]+)" alt="" class="shortstory-img".+?<div class="short_header"> *([^<]+)                </div>.+?<div class="qulabel">([^<]+)</div>'
+    
+    aResult = oParser.parse(sHtmlContent, sPattern)
+    #print ('aResult=', aResult)
+    if (aResult[0] == False):
+        oGui.addText(SITE_IDENTIFIER)
+
+    if (aResult[0] == True):
+        total = len(aResult[1])
+        dialog = cConfig().createDialog(SITE_NAME)
+
+        for aEntry in aResult[1]:
+            cConfig().updateDialog(dialog, total)
+            if dialog.iscanceled():
+                break
+                
+            sName = aEntry[2]
+            sTitle = sName + ' [' + aEntry[3] + ']'
+            sUrl2 = aEntry[0]
+            sThumb = aEntry[1]
+            
+            
+            if sThumb.startswith('//'):
+                sThumb = 'http:' + sThumb
+
+            oOutputParameterHandler = cOutputParameterHandler()
+            oOutputParameterHandler.addParameter('siteUrl', sUrl2)
+            oOutputParameterHandler.addParameter('sMovieTitle', sName)
+            oOutputParameterHandler.addParameter('sThumb', sThumb)
+            sDisplayTitle = cUtil().DecoTitle(sTitle)
+            
+            oGui.addMovie(SITE_IDENTIFIER, 'showHosters', sDisplayTitle, 'films.png', sThumb, '', oOutputParameterHandler)
+            
+        cConfig().finishDialog(dialog)
+           
+        if not sSearch:
+            sNextPage = __checkForNextPage(sHtmlContent)
+            if (sNextPage != False):
+                oOutputParameterHandler = cOutputParameterHandler()
+                oOutputParameterHandler.addParameter('siteUrl', sNextPage)
+                oGui.addNext(SITE_IDENTIFIER, 'showMovies', '[COLOR teal]Next >>>[/COLOR]', oOutputParameterHandler)
+
+    if not sSearch:
+        oGui.setEndOfDirectory()
+
+def __checkForNextPage(sHtmlContent):
+    sPattern = '<b class="next"><a href="(.+?)"'
+    oParser = cParser()
+    aResult = oParser.parse(sHtmlContent, sPattern)
+	
+    if (aResult[0] == True):
+        return  aResult[1][0]
+
+    return False
+
+def showHosters():
+    oGui = cGui()
+    
+    oInputParameterHandler = cInputParameterHandler()
+    sUrl = oInputParameterHandler.getValue('siteUrl')
+    sMovieTitle = oInputParameterHandler.getValue('sMovieTitle')
+    sThumb = oInputParameterHandler.getValue('sThumb')
+
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request()
+    
+    #Vire les bandes annonces
+    sHtmlContent = sHtmlContent.replace('src="https://www.youtube.com/', '')
+
+    sPattern = '<iframe.+?src="(.+?)"'
+    
+    oParser = cParser()
+    aResult = oParser.parse(sHtmlContent, sPattern)
+    #print aResult
+
+    if (aResult[0] == True):
+        for aEntry in aResult[1]:
+            
+            if '//goo.gl' in aEntry:
+                import urllib2
+                try:
+                    class NoRedirection(urllib2.HTTPErrorProcessor):
+                        def http_response(self, request, response):
+                            return response
+
+                    url8 = str(aEntry).replace('https','http')
+
+                    opener = urllib2.build_opener(NoRedirection)
+                    opener.addheaders.append (('User-Agent', UA))
+                    opener.addheaders.append (('Connection', 'keep-alive'))
+
+                    HttpReponse = opener.open(url8)
+                    sHosterUrl = HttpReponse.headers['Location']
+                    sHosterUrl = sHosterUrl.replace('https','http')
+                except:
+                    pass
+            else:
+                sHosterUrl = str(aEntry)
+            #print sHosterUrl    
+            oHoster = cHosterGui().checkHoster(sHosterUrl)
+            if (oHoster != False):
+                sDisplayTitle = cUtil().DecoTitle(sMovieTitle)
+                oHoster.setDisplayName(sDisplayTitle)
+                oHoster.setFileName(sMovieTitle)
+                cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumb)
+
+    oGui.setEndOfDirectory()
