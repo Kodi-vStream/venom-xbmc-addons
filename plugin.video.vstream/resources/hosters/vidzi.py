0f562fc5e9f3964e360bc4a9bcee0285f0a4c3f6
johngf <john59650@live.fr>
2017-10-17 15:56:06 +0200

    Update vidzi.py

--- plugin.video.vstream/resources/hosters/vidzi.py
+++ plugin.video.vstream/resources/hosters/vidzi.py
@@ -1,10 +1,10 @@
+#-*- coding: utf-8 -*-
+#https://vidzi.tv/xxx.html
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
-from resources.lib.gui.gui import cGui
-from resources.lib.util import cUtil
 from resources.hosters.hoster import iHoster
 from resources.lib.packer import cPacker
-import xbmcgui, re, time
+import re
 
 class cHoster(iHoster):
 
@@ -30,37 +30,13 @@ class cHoster(iHoster):
     def isDownloadable(self):
         return True
 
-    def isJDownloaderable(self):
-        return True
-
-    def getPattern(self):
-        return ''
-        
-    def __getIdFromUrl(self):
-        sPattern = "ref=([^<]+)"
-        oParser = cParser()
-        aResult = oParser.parse(self.__sUrl, sPattern)
-        if (aResult[0] == True):
-            return aResult[1][0]
-
-        return ''
-        
-    def __modifyUrl(self, sUrl):
-        if (sUrl.startswith('http://')):
-            oRequestHandler = cRequestHandler(sUrl)
-            oRequestHandler.request()
-            sRealUrl = oRequestHandler.getRealUrl()
-            self.__sUrl = sRealUrl
-            return self.__getIdFromUrl()
-
-        return sUrl;
-
     def setUrl(self, sUrl):       
         self.__sUrl = sUrl.replace('http://vidzi.tv/', '')
+        self.__sUrl = sUrl.replace('https://vidzi.tv/', '')
         self.__sUrl = self.__sUrl.replace('embed-', '')
-        self.__sUrl=re.sub(r'\-.*\.html',r'',self.__sUrl)
+        self.__sUrl= re.sub(r'\-.*\.html',r'',self.__sUrl)
         self.__sUrl = self.__sUrl.replace('.html', '')
-        self.__sUrl = 'http://vidzi.tv/' + str(self.__sUrl)+'.html'
+        self.__sUrl = 'https://vidzi.tv/' + str(self.__sUrl)+'.html'
 
     def checkUrl(self, sUrl):
         return True
@@ -72,27 +48,30 @@ class cHoster(iHoster):
         return self.__getMediaLinkForGuest()
 
     def __getMediaLinkForGuest(self):
-    
+        api_call = ''
+        
         oRequest = cRequestHandler(self.__sUrl)
         sHtmlContent = oRequest.request()
-        
-        #Dexieme test Dean Edwards Packer
         oParser = cParser()
-        sPattern = "<\/a><\/noscript><\/div>.+?(\s*eval\s*\(\s*function(?:.|\s)+?)<\/script>"
-        aResult = oParser.parse(sHtmlContent, sPattern)
         
+        #lien direct
+        sPattern = ',{file: *"([^"]+)"}\]'
+        aResult = oParser.parse(sHtmlContent, sPattern)
         if (aResult[0] == True):
+            api_call = aResult[1][0]
+            
+        #2 test Dean Edwards Packer
+        else:
+            sPattern = "<\/a><\/noscript><\/div>.+?(\s*eval\s*\(\s*function(?:.|\s)+?)<\/script>"
+            aResult = oParser.parse(sHtmlContent, sPattern)
+            if (aResult[0] == True):
                 sUnpacked = cPacker().unpack(aResult[1][0])
-                
                 sPattern =  'file:"([^"]+\.mp4)'
                 aResult = oParser.parse(sUnpacked, sPattern)
-
                 if (aResult[0] == True):
                     api_call = aResult[1][0]
               
-                return True, api_call
-        else:
-            cGui().showInfo(self.__sDisplayName, 'Fichier introuvable' , 5)
-            return False, False
-        
+        if (api_call):
+            return True, api_call
+            
         return False, False
