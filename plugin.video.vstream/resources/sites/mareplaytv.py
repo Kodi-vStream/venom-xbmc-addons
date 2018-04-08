3478f3280124861d5af0f21e8189ded4ad75a792
LordVenom <lordvenom57@gmail.com>
2018-04-08 14:45:13 +0200

    Off maretv

--- plugin.video.vstream/resources/sites/mareplaytv.py
+++ plugin.video.vstream/resources/sites/mareplaytv.py
@@ -1,5 +1,7 @@
 #-*- coding: utf-8 -*-
 #Venom.
+#rien ne fonctionne, video pas héberger par le site.
+return False
 from resources.lib.gui.hoster import cHosterGui
 from resources.lib.gui.gui import cGui
 from resources.lib.handler.inputParameterHandler import cInputParameterHandler
