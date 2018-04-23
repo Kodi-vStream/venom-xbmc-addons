9a5cc17a03b884f5b28fcecf119deaddf09fcc49
jordigarnacho <jordigarnacho@aol.com>
2018-04-23 22:33:47 +0200

    Add files via upload

--- plugin.video.vstream/resources/sites/mamcin.py
+++ plugin.video.vstream/resources/sites/mamcin.py
@@ -3,141 +3,121 @@
 # jordigarnacho
 #
 #
-from resources.lib.gui.hoster import cHosterGui #systeme de recherche pour l'hote
-from resources.lib.handler.hosterHandler import cHosterHandler #systeme de recherche pour l'hote
-from resources.lib.gui.gui import cGui #systeme d'affichage pour xbmc
-from resources.lib.gui.guiElement import cGuiElement #systeme d'affichage pour xbmc
-from resources.lib.handler.inputParameterHandler import cInputParameterHandler #entree des parametres
-from resources.lib.handler.outputParameterHandler import cOutputParameterHandler #sortie des parametres
-from resources.lib.handler.requestHandler import cRequestHandler #requete url
-from resources.lib.config import cConfig #config
-from resources.lib.parser import cParser #recherche de code
-
-#from resources.lib.util import cUtil #outils pouvant etre utiles
+from resources.lib.gui.hoster import cHosterGui
+from resources.lib.handler.hosterHandler import cHosterHandler
+from resources.lib.gui.gui import cGui
+from resources.lib.gui.guiElement import cGuiElement
+from resources.lib.handler.inputParameterHandler import cInputParameterHandler
+from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.config import cConfig
+from resources.lib.parser import cParser
 
 import xbmc
 
-#Si vous créez une source et la deposez dans le dossier "sites" elle sera directement visible sous xbmc
+SITE_IDENTIFIER = 'mamcin'
+SITE_NAME = 'Mamcin'
+SITE_DESC = 'plus belle la vie, streaming, séries, récent'
 
-SITE_IDENTIFIER = 'mamcin' #identifant (nom de votre fichier) remplacez les espaces et les . par _ AUCUN CARACTERE SPECIAL
-SITE_NAME = 'Mamcin' #nom que xbmc affiche
-SITE_DESC = 'streaming hd, Films/séries, récent' #description courte de votre source
+URL_MAIN = 'https://www.mamcin.com/'
 
-URL_MAIN = 'https://www.mamcin.com/' #url de votre source
+FUNCTION_SEARCH = 'showMovies'
 
-SERIE_SERIES = (True, 'showGenres')
-SERIE_NEWS = (URL_MAIN + 'non-classe/', 'showMovies')
+SERIE_NEWS = (URL_MAIN, 'showMovies')
+SERIE_SERIES = (URL_MAIN, 'showMovies')
+SERIE_GENRES = (True, 'showSerieGenres')
 
-def load(): #fonction chargee automatiquement par l'addon l'index de votre navigation.
-    oGui = cGui() #ouvre l'affichage
+#loader function
+def load():
+    oGui = cGui()
 
     oOutputParameterHandler = cOutputParameterHandler()
     oOutputParameterHandler.addParameter('siteUrl', SERIE_NEWS[0])
-    oGui.addDir(SITE_IDENTIFIER, SERIE_NEWS[1], 'PBLV (Derniers ajouts)', 'news.png', oOutputParameterHandler)
-
-    oGui.setEndOfDirectory() #ferme l'affichage
-
-def showSearch(): #fonction de recherche
-    oGui = cGui()
-
-    sSearchText = oGui.showKeyBoard() #appelle le clavier xbmc
-    if (sSearchText != False):
-        sUrl = URL_SEARCH[0] + sSearchText #modifie l'url de recherche
-        showMovies(sUrl) #appelle la fonction qui pourra lire la page de resultats
-        oGui.setEndOfDirectory()
-        return
-
-
-def showGenres(): #affiche les genres
-    oGui = cGui()
-
-    #juste a entrer les categories et les liens qui vont bien
-    liste = []
-    liste.append( ['News', URL_MAIN + 'non-classe/'] )
-
-    for sTitle,sUrl in liste: #boucle
-
-        oOutputParameterHandler = cOutputParameterHandler()
-        oOutputParameterHandler.addParameter('siteUrl', sUrl) #sortie de l'url en parametre
-        oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'genres.png', oOutputParameterHandler)
-        #ajouter un dossier vers la fonction showMovies avec le titre de chaque categorie.
+    oGui.addDir(SITE_IDENTIFIER, SERIE_NEWS[1], 'Plus Belle La Vie (Derniers ajouts)', 'news.png', oOutputParameterHandler)
 
     oGui.setEndOfDirectory()
 
+#search function
+def showSearch():
+    oGui = cGui()
+
+    sSearchText = oGui.showKeyBoard()
+    if (sSearchText != False):
+        sUrl = URL_SEARCH[0] + sSearchText
+        showMovies(sUrl)
+        oGui.setEndOfDirectory()
+        return
+
+#genre definition
+def showGenres():
+    oGui = cGui()
+
+    liste = []
+    liste.append( ['News', URL_MAIN + 'non-classe/'] )
+
+    for sTitle,sUrl in liste:
+
+        oOutputParameterHandler = cOutputParameterHandler()
+        oOutputParameterHandler.addParameter('siteUrl', sUrl)
+        oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'genres.png', oOutputParameterHandler)
+
+    oGui.setEndOfDirectory()
+
+#function to extract episodes
 def showMovies(sSearch = ''):
-    oGui = cGui() #ouvre l'affichage
-    if sSearch: #si une url et envoyer directement grace a la fonction showSearch
+    oGui = cGui()
+    if sSearch:
       sUrl = sSearch
     else:
         oInputParameterHandler = cInputParameterHandler()
-        sUrl = oInputParameterHandler.getValue('siteUrl') #recupere l'url sortie en parametre
+        sUrl = oInputParameterHandler.getValue('siteUrl')
 
-    oRequestHandler = cRequestHandler(sUrl) #envoye une requete a l'url
-    sHtmlContent = oRequestHandler.request() #requete aussi
-
-    sHtmlContent = sHtmlContent.replace('<span class="likeThis">', '').replace('</span>', '')
-    #la fonction replace est pratique pour supprimer un code du resultat
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request()
 
     sPattern = '<div class="featured-image"><a href="(.+?)" title="(.+?)"><img width=".+?" height=".+?" src="(.+?)"'
-    #pour faire simple recherche ce bout de code dans le code source de l'url
-    #- "([^"]+)" je veux cette partie de code qui se trouve entre guillemets mais pas de guillemets dans la chaine
-    #- .+? je ne veux pas cette partie et peux importe ceux qu'elle contient
-    #- >(.+?)< je veux cette partie de code qui se trouve entre < et > mais il peut y avoir n'inporte quoi entre les 2.
-    #- (https*://[^"]) je veux l'adresse qui commence par https ou http jusqu'au prochain guillemet.
-    #
-    #Pour tester vos Regex, vous pouvez utiliser le site https://regex101.com/ en mettant dans les modifiers "gmis"
 
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
-    #le plus simple et de faire un  cConfig().log(str(aResult))
-    #dans le fichier log d'xbmc vous pourrez voir un array de ce que recupere le script
-    #et modifier sPattern si besoin
-    cConfig().log(str(aResult)) #Commenter ou supprimer cette ligne une fois fini
 
-    #affiche une information si aucun resulat
     if (aResult[0] == False):
-		oGui.addText(SITE_IDENTIFIER)
+        oGui.addText(SITE_IDENTIFIER)
 
     if (aResult[0] == True):
         total = len(aResult[1])
-        #dialog barre de progression
+
         dialog = cConfig().createDialog(SITE_NAME)
 
         for aEntry in aResult[1]:
-            cConfig().updateDialog(dialog, total) #dialog update
+            cConfig().updateDialog(dialog, total)
             if dialog.iscanceled():
                 break
 
-            #L'array affiche vos info dans l'orde de sPattern en commencant a 0, attention dans ce cas la on recupere 6 information
-            #Mais selon votre regex il ne peut y en avoir que 2 ou 3.
-            sUrl    = str(aEntry[0])
-            sTitle  = (' %s ') % (str(aEntry[1]))
-            sThumbnail = str(aEntry[2])
+            #first post filter
+            if (str(aEntry[2]) != "https://www.mamcin.com/wp-content/uploads/2017/10/plus-belle-la-vie-episode-suivant-en-avance.jpg"):
+                sUrl    = str(aEntry[0])
+                sTitle  = (' %s ') % (str(aEntry[1]))
+                sThumbnail = str(aEntry[2])
+ 
+                oOutputParameterHandler = cOutputParameterHandler()
+                oOutputParameterHandler.addParameter('siteUrl', sUrl)
+                oOutputParameterHandler.addParameter('sMovieTitle', sTitle)
+                oOutputParameterHandler.addParameter('sThumbnail', sThumbnail)
+                oGui.addMovie(SITE_IDENTIFIER, 'showHosters', sTitle, '', sThumbnail,'', oOutputParameterHandler)	
 
-            oOutputParameterHandler = cOutputParameterHandler()
-            oOutputParameterHandler.addParameter('siteUrl', sUrl2) #sortie de l'url
-            oOutputParameterHandler.addParameter('sMovieTitle', sTitle) #sortie du titre
-            oOutputParameterHandler.addParameter('sThumbnail', sThumb) #sortie du poster
-			
-            oGui.addMovie(SITE_IDENTIFIER, 'showHosters', sTitle, '', sThumbnail,'', oOutputParameterHandler)			
+        cConfig().finishDialog(dialog)
 
-            #il existe aussi addMisc(identifiant, function, titre, icon, poster, description, sortie parametre)
-            #la difference et pour les metadonner serie, films ou sans
-
-        cConfig().finishDialog(dialog) #fin du dialog
-
-        sNextPage = __checkForNextPage(sHtmlContent) #cherche la page suivante
+        sNextPage = __checkForNextPage(sHtmlContent)
         if (sNextPage != False):
             oOutputParameterHandler = cOutputParameterHandler()
             oOutputParameterHandler.addParameter('siteUrl', sNextPage)
             oGui.addNext(SITE_IDENTIFIER, 'showMovies', '[COLOR teal]Next >>>[/COLOR]', oOutputParameterHandler)
-            #Ajoute une entree pour le lien Next | pas de addMisc pas de poster et de description inutile donc
 
     if not sSearch:
-        oGui.setEndOfDirectory() #ferme l'affichage
+        oGui.setEndOfDirectory()
 
-
-def __checkForNextPage(sHtmlContent): #cherche la page suivante
+#search the next page
+def __checkForNextPage(sHtmlContent):
     oParser = cParser()
     sPattern = '<ul class="default-wp-page clearfix"><li class="previous"><a href="(.+?)" >'
     aResult = oParser.parse(sHtmlContent, sPattern)
@@ -147,34 +127,41 @@ def __checkForNextPage(sHtmlContent): #cherche la page suivante
 
     return False
 
+#search hosts
+def showHosters():
+    oGui = cGui()
+    oInputParameterHandler = cInputParameterHandler()
+    sUrl = oInputParameterHandler.getValue('siteUrl')
+    sMovieTitle = oInputParameterHandler.getValue('sMovieTitle')
+    sThumbnail = oInputParameterHandler.getValue('sThumbnail')
 
-def showHosters(): #recherche et affiche les hotes
-    oGui = cGui() #ouvre l'affichage
-    oInputParameterHandler = cInputParameterHandler() #apelle l'entree de parametre
-    sUrl = oInputParameterHandler.getValue('siteUrl') #apelle siteUrl
-    sMovieTitle = oInputParameterHandler.getValue('sMovieTitle') #appelle le titre
-    sThumbnail = oInputParameterHandler.getValue('sThumbnail') #appelle le poster
-
-    oRequestHandler = cRequestHandler(sUrl) #requete sur l'url
-    sHtmlContent = oRequestHandler.request(); #requete sur l'url
+    oRequestHandler = cRequestHandler(sUrl)
+    sHtmlContent = oRequestHandler.request();
 
     oParser = cParser()
-    sPattern = '<iframe.+?src="(.+?)"'
-    #ici nous cherchons toute les sources iframe
 
+    #add dailymotion sources
+    sPattern = '<iframe.+?src="(.+?)?logo=0&info=0"'
     aResult = oParser.parse(sHtmlContent, sPattern)
-    #pensez a faire un xbmc.log(str(aResult)) pour verifier
-
-    #si un lien ne s'affiche pas peux etre que l'hote n'est pas supporte par l'addon
     if (aResult[0] == True):
         for aEntry in aResult[1]:
-
-            sHosterUrl = str(aEntry)
-            oHoster = cHosterGui().checkHoster(sHosterUrl) #recherche l'hote dans l'addon
+            sHosterUrl = str("http:"+aEntry)
+            oHoster = cHosterGui().checkHoster(sHosterUrl)
             if (oHoster != False):
-                oHoster.setDisplayName(sMovieTitle) #nom affiche
-                oHoster.setFileName(sMovieTitle) #idem
+                oHoster.setDisplayName(sMovieTitle)
+                oHoster.setFileName(sMovieTitle)
                 cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumbnail)
-                #affiche le lien (oGui, oHoster, url du lien, poster)
+
+    #add sendvid sources
+    sPattern = '<source src="(.+?)" type=\'video/mp4\' />'
+    aResult = oParser.parse(sHtmlContent, sPattern)
+    if (aResult[0] == True):
+        for aEntry in aResult[1]:
+            sHosterUrl = str(aEntry)
+            oHoster = cHosterGui().checkHoster(sHosterUrl)
+            if (oHoster != False):
+                oHoster.setDisplayName(sMovieTitle)
+                oHoster.setFileName(sMovieTitle)
+                cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumbnail)                
 
     oGui.setEndOfDirectory() #fin
