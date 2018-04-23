73c1b572d69409d1fe97c3f1e19626ba7ee7d86f
LordVenom <lordvenom57@gmail.com>
2018-04-23 06:57:37 +0200

    runscript

--- plugin.video.vstream/resources/lib/runscript.py
+++ plugin.video.vstream/resources/lib/runscript.py
@@ -9,11 +9,14 @@ sLibrary = xbmc.translatePath(vstream.getAddonInfo("path")).decode("utf-8")
 sys.path.append (sLibrary)
 
 #remove sys.path lib/
-sys.path.remove(sys.path[0])
+for indice, valeur in enumerate(sys.path):
+    if 'plugin.video.vstream/resources/lib' in valeur:
+        sys.path.remove(sys.path[indice])
 
-from resources.lib.util import VStranslatePath
+
 from resources.lib.config import cConfig
 #from util import VStranslatePath
+#from resources.lib.util import VStranslatePath
 
 try:
     from sqlite3 import dbapi2 as sqlite
@@ -48,7 +51,7 @@ class cClear:
                 oRequest =  urllib2.Request(sUrl)
                 oResponse = urllib2.urlopen(oRequest)
                 sContent = oResponse.read()
-                from about import cAbout
+                from resources.lib.about import cAbout
                 cAbout().TextBoxes('vStream Changelog', sContent)
             except:
                 cConfig().error("%s,%s" % (cConfig().getlanguage(30205), sUrl))
@@ -60,7 +63,7 @@ class cClear:
                 oRequest =  urllib2.Request(sUrl)
                 oResponse = urllib2.urlopen(oRequest)
                 sContent = oResponse.read()
-                from about import cAbout
+                from resources.lib.about import cAbout
                 cAbout().TextBoxes('vStream Soutient', sContent)
             except:
                 cConfig().error("%s,%s" % (cConfig().getlanguage(30205), sUrl))
@@ -69,13 +72,9 @@ class cClear:
         elif (env == 'addon'):
             dialog = xbmcgui.Dialog()
             if dialog.yesno('vStream', 'Êtes-vous sûr ?','','','Non', 'Oui'):
-
-                #cached_fav = cConfig().getFileFav()
-                #cached_DB = cConfig().getFileDB()
                 cached_Cache = cConfig().getFileCache()
-                #self.ClearDir2(VStranslatePath(cached_fav),True)
-                #self.ClearDir2(VStranslatePath(cached_DB),True)
-                self.ClearDir2(VStranslatePath(cached_Cache),True)
+                cached_Cache = xbmc.translatePath(cached_Cache).decode("utf-8")
+                self.ClearDir2(cached_Cache,True)
                 xbmc.executebuiltin("XBMC.Notification(Clear Addon Cache,Successful,5000,"")")
             return
 
@@ -116,7 +115,8 @@ class cClear:
         elif (env == 'xbmc'):
             dialog = xbmcgui.Dialog()
             if dialog.yesno('vStream', 'Êtes-vous sûr ?','','','Non', 'Oui'):
-                self.ClearDir(VStranslatePath('special://temp/'),True)
+                temp = xbmc.translatePath('special://temp/').decode("utf-8")
+                self.ClearDir(temp,True)
                 xbmc.executebuiltin("XBMC.Notification(Clear XBMC Cache,Successful,5000,"")")
             return
 
@@ -124,7 +124,7 @@ class cClear:
             dialog = xbmcgui.Dialog()
             if dialog.yesno('vStream', 'Êtes-vous sûr ?','','','Non', 'Oui'):
                 xbmc.executebuiltin("XBMC.Notification(Clear .fi Files ,Successful,2000,"")")
-                path = VStranslatePath('special://temp/')
+                path = xbmc.translatePath('special://temp/').decode("utf-8")
                 filenames = next(os.walk(path))[2]
                 for i in filenames:
                     if ".fi" in i:
@@ -134,7 +134,7 @@ class cClear:
         elif (env == 'uplog'):
             dialog = xbmcgui.Dialog()
             if dialog.yesno('vStream', 'Êtes-vous sûr ?','','','Non', 'Oui'):
-                path = VStranslatePath('special://logpath/')
+                path = xbmc.translatePath('special://logpath/').decode("utf-8")
                 UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
                 headers = { 'User-Agent' : UA }
                 filenames = next(os.walk(path))[2]
@@ -257,8 +257,8 @@ class cClear:
             dialog = xbmcgui.Dialog()
             if dialog.yesno('vStream', 'Êtes-vous sûr ? Ceci effacera toutes les thumbnails ','','','Non', 'Oui'):
                 xbmc.executebuiltin("XBMC.Notification(Clear Thumbnails ,Successful,2000,"")")
-                path = VStranslatePath('special://userdata/Thumbnails/')
-                path2 = VStranslatePath('special://userdata/Database/')
+                path = xbmc.translatePath('special://userdata/Thumbnails/').decode("utf-8")
+                path2 = xbmc.translatePath('special://userdata/Database/').decode("utf-8")
                 for i in os.listdir(path):
                     folders = os.path.join(path, i).encode('utf-8')
                     if os.path.isdir(folders):
