6b00dea8d9c34a8d7328f2f768925091fad3f0b2
johngf <john59650@live.fr>
2018-04-26 17:26:15 +0200

    cloud cartel

---
+++ plugin.video.vstream/resources/hosters/cloudcartel.py
@@ -0,0 +1,63 @@
+#-*- coding: utf-8 -*-
+#https://cloudcartel.net/embed/video/xxx
+from resources.lib.handler.requestHandler import cRequestHandler 
+from resources.lib.parser import cParser
+from resources.hosters.hoster import iHoster
+
+sPattern1 = '"url":"([^"]+)",'
+ 
+class cHoster(iHoster):
+
+    def __init__(self):
+        self.__sDisplayName = 'Cloudcartel'
+        self.__sFileName = self.__sDisplayName
+        self.__sHD = ''
+
+    def getDisplayName(self):
+        return  self.__sDisplayName
+
+    def setDisplayName(self, sDisplayName):
+        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]'+self.__sDisplayName+'[/COLOR]'
+
+    def setFileName(self, sFileName):
+        self.__sFileName = sFileName
+        
+    def getFileName(self):
+        return self.__sFileName
+
+    def getPluginIdentifier(self):
+        return 'cloudcartel'
+        
+    def setHD(self, sHD):
+        self.__sHD = ''
+        
+    def getHD(self):
+        return self.__sHD
+
+    def isDownloadable(self):
+        return True
+
+    def setUrl(self, sUrl):
+        self.__sUrl = str(sUrl)
+        self.__sUrl = self.__sUrl.replace('cloudcartel.net/embed/video/','cloudcartel.vnadigital.com/download/link/')
+        #suivre
+        
+    def getMediaLink(self):
+        return self.__getMediaLinkForGuest()
+
+    def __getMediaLinkForGuest(self):
+        api_call = ''
+        oParser = cParser()
+
+        oRequest = cRequestHandler(self.__sUrl)
+        sHtmlContent = oRequest.request()
+
+        aResult = oParser.parse(sHtmlContent, sPattern1)
+        if (aResult[0] == True):
+            api_call = aResult[1][0]
+            
+
+        if (api_call):
+            return True, api_call
+            
+        return False, False
