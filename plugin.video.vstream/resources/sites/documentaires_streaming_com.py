dec21ae19e3fba691d0fec5e90063be1fac03b49
razorex <individuel@free.fr>
2018-05-14 22:54:49 +0200

    Update documentaires_streaming_com.py

--- plugin.video.vstream/resources/sites/documentaires_streaming_com.py
+++ plugin.video.vstream/resources/sites/documentaires_streaming_com.py
@@ -18,11 +18,9 @@ DOC_NEWS = (URL_MAIN+'category/documentaire/', 'showMovies')
 DOC_DOCS = ('http://', 'load')
 DOC_GENRES = (True, 'showDocuGenres')
 
-
 REPLAYTV_NEWS = (URL_MAIN+'category/replay_tv/', 'showMovies')
 REPLAYTV_REPLAYTV = ('http://', 'load')
 REPLAYTV_GENRES = (True, 'showReplayGenres')
-
 
 REPORTAGE_NEWS = (URL_MAIN+'category/reportages/', 'showMovies')
 
@@ -70,17 +68,17 @@ def showDocuGenres():
     oGui = cGui()
 
     liste = []
-    liste.append( ['Histoires',URL_MAIN + 'category/histoires/'] )
-    liste.append( ['Nature, Animaux',URL_MAIN + 'category/nature_animaux/'] )
-    liste.append( ['Paranormal',URL_MAIN + 'category/paranormal/'] )
-    liste.append( ['Politique, Société',URL_MAIN + 'category/politique-societe/'] )
-    liste.append( ['Science',URL_MAIN + 'category/science/'] )
-    liste.append( ['Société',URL_MAIN + 'documentaire/societe/'] )
-    liste.append( ['Sport',URL_MAIN + 'category/sport/'] )
-    liste.append( ['Voiture',URL_MAIN + 'category/voiture/'] )
-    liste.append( ['Voyage',URL_MAIN + 'category/voyage-2/'] )
+    liste.append( ['Histoires', URL_MAIN + 'category/histoires/'] )
+    liste.append( ['Nature, Animaux', URL_MAIN + 'category/nature_animaux/'] )
+    liste.append( ['Paranormal', URL_MAIN + 'category/paranormal/'] )
+    liste.append( ['Politique, Société', URL_MAIN + 'category/politique-societe/'] )
+    liste.append( ['Science', URL_MAIN + 'category/science/'] )
+    liste.append( ['Société', URL_MAIN + 'documentaire/societe/'] )
+    liste.append( ['Sport', URL_MAIN + 'category/sport/'] )
+    liste.append( ['Voiture', URL_MAIN + 'category/voiture/'] )
+    liste.append( ['Voyage', URL_MAIN + 'category/voyage-2/'] )
 
-    for sTitle,sUrl in liste:
+    for sTitle, sUrl in liste:
 
         oOutputParameterHandler = cOutputParameterHandler()
         oOutputParameterHandler.addParameter('siteUrl', sUrl)
@@ -92,25 +90,24 @@ def showReplayGenres():
     oGui = cGui()
 
     liste = []
-    liste.append( ['Arte',URL_MAIN + 'category/replay_tv/arte-replay_tv/'] )
-    liste.append( ['France 2',URL_MAIN + 'category/replay_tv/france_2/'] )
-    liste.append( ['France 3',URL_MAIN + 'category/replay_tv/france_3/'] )
-    liste.append( ['France 5',URL_MAIN + 'category/replay_tv/france_5/'] )
-    liste.append( ['M6',URL_MAIN + 'category/replay_tv/m6/'] )
-    liste.append( ['6Ter',URL_MAIN + 'category/replay_tv/6ter/'] )
-    liste.append( ['D8',URL_MAIN + 'category/replay_tv/d8/'] )
-    liste.append( ['France O',URL_MAIN + 'category/replay_tv/franceo/'] )
-    liste.append( ['ICI',URL_MAIN + 'category/replay_tv/ici/'] )
-    liste.append( ['N23',URL_MAIN + 'category/replay_tv/n23/'] )
-    liste.append( ['Nrj12',URL_MAIN + 'category/replay_tv/nrj12/'] )
-    liste.append( ['Nt1',URL_MAIN + 'category/replay_tv/nt1/'] )
-    liste.append( ['Rmc',URL_MAIN + 'category/replay_tv/rmc/'] )
-    liste.append( ['Tf1',URL_MAIN + 'category/replay_tv/tf1/'] )
-    liste.append( ['Tmc',URL_MAIN + 'category/replay_tv/tmc/'] )
-    liste.append( ['W9',URL_MAIN + 'category/replay_tv/w9/'] )
+    liste.append( ['Arte', URL_MAIN + 'category/replay_tv/arte-replay_tv/'] )
+    liste.append( ['France 2', URL_MAIN + 'category/replay_tv/france_2/'] )
+    liste.append( ['France 3', URL_MAIN + 'category/replay_tv/france_3/'] )
+    liste.append( ['France 5', URL_MAIN + 'category/replay_tv/france_5/'] )
+    liste.append( ['M6', URL_MAIN + 'category/replay_tv/m6/'] )
+    liste.append( ['6Ter', URL_MAIN + 'category/replay_tv/6ter/'] )
+    liste.append( ['D8', URL_MAIN + 'category/replay_tv/d8/'] )
+    liste.append( ['France O', URL_MAIN + 'category/replay_tv/franceo/'] )
+    liste.append( ['ICI', URL_MAIN + 'category/replay_tv/ici/'] )
+    liste.append( ['N23', URL_MAIN + 'category/replay_tv/n23/'] )
+    liste.append( ['Nrj12', URL_MAIN + 'category/replay_tv/nrj12/'] )
+    liste.append( ['Nt1', URL_MAIN + 'category/replay_tv/nt1/'] )
+    liste.append( ['Rmc', URL_MAIN + 'category/replay_tv/rmc/'] )
+    liste.append( ['Tf1', URL_MAIN + 'category/replay_tv/tf1/'] )
+    liste.append( ['Tmc', URL_MAIN + 'category/replay_tv/tmc/'] )
+    liste.append( ['W9', URL_MAIN + 'category/replay_tv/w9/'] )
 
-
-    for sTitle,sUrl in liste:
+    for sTitle, sUrl in liste:
 
         oOutputParameterHandler = cOutputParameterHandler()
         oOutputParameterHandler.addParameter('siteUrl', sUrl)
@@ -127,8 +124,7 @@ def showMovies(sSearch = ''):
         sUrl = oInputParameterHandler.getValue('siteUrl')
 
     oRequestHandler = cRequestHandler(sUrl)
-
-    sHtmlContent = oRequestHandler.request();
+    sHtmlContent = oRequestHandler.request()
     sHtmlContent = sHtmlContent.replace('&#039;', '\'').replace('&#8217;', '\'')
 
     sPattern = '<div class="item-thumbnail">.*?<a href="([^"]+)".*?<img src="([^"]+)" alt="([^"]+)"(?:.+?<div class="item-content hidden"><p>([^<]+)</p>|)'
@@ -137,18 +133,26 @@ def showMovies(sSearch = ''):
     aResult = oParser.parse(sHtmlContent, sPattern)
 
     if (aResult[0] == False):
-		oGui.addText(SITE_IDENTIFIER)
+        oGui.addText(SITE_IDENTIFIER)
 
     if (aResult[0] == True):
+        total = len(aResult[1])
+        dialog = cConfig().createDialog(SITE_NAME)
         for aEntry in aResult[1]:
-            #sTitle = aEntry[2].replace('&laquo;','<<').replace('&raquo;','>>').replace('&nbsp;','')
-            sCom = aEntry[3].replace('[&hellip;]','...')
+            cConfig().updateDialog(dialog, total)
+            if dialog.iscanceled():
+                break
+
+            sTitle = aEntry[2]#.replace('&laquo;', '<<').replace('&raquo;', '>>').replace('&nbsp;', '')
+            sUrl = aEntry[0]
+            sThumb = aEntry[1]
+            sDesc = aEntry[3].replace('[&hellip;]', '...')
 
             oOutputParameterHandler = cOutputParameterHandler()
-            oOutputParameterHandler.addParameter('siteUrl', aEntry[0])
-            #oOutputParameterHandler.addParameter('sMovieTitle', sTitle)
-            oOutputParameterHandler.addParameter('sThumbnail', aEntry[1])
-            oGui.addMisc(SITE_IDENTIFIER, 'showHosters', aEntry[2], 'doc.png', aEntry[1], sCom, oOutputParameterHandler)
+            oOutputParameterHandler.addParameter('siteUrl', sUrl)
+            oOutputParameterHandler.addParameter('sMovieTitle', sTitle)
+            oOutputParameterHandler.addParameter('sThumb', sThumb)
+            oGui.addMisc(SITE_IDENTIFIER, 'showHosters', sTitle, 'doc.png', sThumb, sDesc, oOutputParameterHandler)
 
         sNextPage = __checkForNextPage(sHtmlContent)
         if (sNextPage != False):
@@ -173,24 +177,33 @@ def showHosters():
     oGui = cGui()
     oInputParameterHandler = cInputParameterHandler()
     sUrl = oInputParameterHandler.getValue('siteUrl')
-    sMovieTitle = oInputParameterHandler.getValue('title')
-    sThumbnail = oInputParameterHandler.getValue('sThumbnail')
+    sMovieTitle = oInputParameterHandler.getValue('sMovieTitle')
+    sThumb = oInputParameterHandler.getValue('sThumb')
 
     oRequestHandler = cRequestHandler(sUrl)
-    sHtmlContent = oRequestHandler.request();
+    sHtmlContent = oRequestHandler.request()
     sHtmlContent = sHtmlContent.replace('<iframe src="//www.facebook.com/', '')
 
     sPattern = '<IFRAME SRC="(.+?)".+?</IFRAME>'
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
 
+    if (aResult[0] == False):
+        oGui.addText(SITE_IDENTIFIER)
+
     if (aResult[0] == True):
+        total = len(aResult[1])
+        dialog = cConfig().createDialog(SITE_NAME)
         for aEntry in aResult[1]:
+            cConfig().updateDialog(dialog, total)
+            if dialog.iscanceled():
+                break
+
             sHosterUrl = str(aEntry)
             oHoster = cHosterGui().checkHoster(sHosterUrl)
             if (oHoster != False):
                 oHoster.setDisplayName(sMovieTitle)
                 oHoster.setFileName(sMovieTitle)
-                cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumbnail)
+                cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumb)
 
     oGui.setEndOfDirectory()
