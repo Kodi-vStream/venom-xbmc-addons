315e674b0a319974ebe73a8f9112a7831650fc9d
LordVenom <lordvenom57@gmail.com>
2018-06-19 00:40:47 +0200

    comaddon class gui xbmc

--- plugin.video.vstream/resources/lib/comaddon.py
+++ plugin.video.vstream/resources/lib/comaddon.py
@@ -1,10 +1,12 @@
 #-*- coding: utf-8 -*-
 # https://github.com/Kodi-vStream/venom-xbmc-addons
 
-import xbmcaddon
+import xbmcaddon, xbmcgui, xbmc
 
-""" utiliser 
+"""
 from resources.lib.comaddon import addon
+ou
+from resources.lib.comaddon import *
 
 addons = addon() en haut de page.
 
@@ -25,7 +27,59 @@ class addon(xbmcaddon.Addon):
 
     def __init__(self, id='plugin.video.vstream'):
         xbmcaddon.__init__(id)
+        print 'initttttttttt xbmcaddon'
         pass
 
     def lang(self, lang):
         return self.getLocalizedString(lang)
+
+    def setting(self, name, value=False):
+    #adons = addon()
+    #use addons.setting('name') pour getsetting
+    #use addons.setting('name', 'value) pour setsetting
+        if value:
+            return self.setSetting(name, value)
+        else:
+            return self.getSetting(name)
+
+
+"""
+from resources.lib.comaddon import dialog
+ou
+from resources.lib.comaddon import *
+
+ne peux pas utiliser les autres fonction que dialog 
+dialogs = dialog()
+dialogs.info('test')
+"""
+
+class dialog(xbmcgui.Dialog):
+
+    def __init__(self):
+        xbmcgui.__init__('')
+        print 'initttttttttt xbmcgui'
+        pass
+
+    def info(self, desc, title = 'vStream', iseconds=0,sound = False):
+        if (iseconds == 0):
+            iseconds = 1000
+        else:
+            iseconds = iseconds * 1000
+        
+        if addon().setting('Block_Noti_sound') == 'true':
+            sound = True
+
+        return self.notification(str(title), str(desc),xbmcgui.NOTIFICATION_INFO,iseconds,sound)
+
+    def error(self, e):
+        self.notification('Vstream','Erreur: '+str(e),xbmcgui.NOTIFICATION_ERROR,2000)
+        log('Erreur: ' + str(e))
+
+
+"""
+from resources.lib.comaddon import *
+log('testtttttttttttt')
+"""
+#xbmc des fonctions pas des class
+def log(e):
+    return xbmc.log('\t[PLUGIN] Vstream: '+str(e), xbmc.LOGDEBUG)
