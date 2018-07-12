8384457f262f959bdbe50af3da13e92c496e2b99
LordVenom <lordvenom57@gmail.com>
2018-07-12 16:47:46 +0200

    site de retour

--- plugin.video.vstream/resources/sites/voirfilms_org.py
+++ plugin.video.vstream/resources/sites/voirfilms_org.py
@@ -1,7 +1,6 @@
 #-*- coding: utf-8 -*-
 #Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
 #Venom.
-return False
 from resources.lib.gui.hoster import cHosterGui
 from resources.lib.gui.gui import cGui
 from resources.lib.handler.inputParameterHandler import cInputParameterHandler
