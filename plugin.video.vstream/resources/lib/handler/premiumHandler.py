445dc477e0f261de5bedfb99ce6b881d8121686d
johngf <john59650@live.fr>
2018-07-14 06:03:57 +0200

    Update premiumHandler.py

--- plugin.video.vstream/resources/lib/handler/premiumHandler.py
+++ plugin.video.vstream/resources/lib/handler/premiumHandler.py
@@ -183,6 +183,8 @@ class cPremiumHandler:
             #print aResult
             if (aResult[0] == True):
                 for cook in aResult[1]:
+                    if 'deleted' in cook[1]:
+                        continue
                     cookies = cookies + cook[0] + '=' + cook[1]+ ';'
 
         #save cookie
