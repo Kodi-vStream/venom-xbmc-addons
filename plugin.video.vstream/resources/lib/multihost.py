20abe86fc820b4cf79cd530cb650798f4416ac7c
razorex <individuel@free.fr>
2018-08-30 00:27:26 +0200

    Update multihost.py
    Traitement de l'url pour les liens jheberg

--- plugin.video.vstream/resources/lib/multihost.py
+++ plugin.video.vstream/resources/lib/multihost.py
@@ -2,7 +2,7 @@
 # https://github.com/Kodi-vStream/venom-xbmc-addons
 #Venom.
 from resources.lib.handler.requestHandler import cRequestHandler
-import re,urllib
+import re, urllib
 UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0',
 #modif cloudflare
 def GetHtml(url, postdata = None):
@@ -13,12 +13,12 @@ def GetHtml(url, postdata = None):
     oRequest.addHeaderEntry('User-Agent', UA)
     
     if postdata != None:
-        oRequest.addHeaderEntry('X-Requested-With','XMLHttpRequest')
-        oRequest.addHeaderEntry('Content-Type','application/x-www-form-urlencoded; charset=UTF-8')
-        oRequest.addHeaderEntry('Referer','http://www.jheberg.net/redirect/xxxxxx/yyyyyy/')
+        oRequest.addHeaderEntry('X-Requested-With', 'XMLHttpRequest')
+        oRequest.addHeaderEntry('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+        oRequest.addHeaderEntry('Referer', 'http://www.jheberg.net/redirect/xxxxxx/yyyyyy/')
         
     elif 'www.jheberg.net' in url:
-        oRequest.addHeaderEntry('Referer', url.replace('http://www.jheberg.net/mirrors','http://www.jheberg.net/captcha').replace('\r',''))
+        oRequest.addHeaderEntry('Referer', url.replace('http://www.jheberg.net/mirrors', 'http://www.jheberg.net/captcha').replace('\r', ''))
     
     oRequest.addParametersLine(postdata)
 
@@ -31,14 +31,14 @@ class cMultiup:
         self.id = ''
         self.list = []
         
-    def GetUrls(self,url):
+    def GetUrls(self, url):
 
-        NewUrl = url.replace('http://www.multiup.org/fr/download','http://www.multiup.eu/fr/mirror').replace('http://www.multiup.eu/fr/download','http://www.multiup.eu/fr/mirror').replace('http://www.multiup.org/download', 'http://www.multiup.eu/fr/mirror')
+        NewUrl = url.replace('http://www.multiup.org/fr/download', 'http://www.multiup.eu/fr/mirror').replace('http://www.multiup.eu/fr/download', 'http://www.multiup.eu/fr/mirror').replace('http://www.multiup.org/download', 'http://www.multiup.eu/fr/mirror')
         
         sHtmlContent = GetHtml(NewUrl)
 
         sPattern = 'nameHost="([^"]+)"\s+link="([^"]+)"\s+validity="([a-z]+)"'
-        r = re.findall(sPattern,sHtmlContent,re.DOTALL)
+        r = re.findall(sPattern,sHtmlContent, re.DOTALL)
 
         if not r:
             return False
@@ -55,14 +55,14 @@ class cJheberg:
         self.id = ''
         self.list = []
         
-    def GetUrls(self,url):
+    def GetUrls(self, url):
 
-        NewUrl = url.replace('http://www.jheberg.net/captcha','http://www.jheberg.net/mirrors')
+        NewUrl = url.replace('http://jheberg.net', 'http://www.jheberg.net').replace('captcha', 'mirrors')
         
         sHtmlContent = GetHtml(NewUrl)
         
         sPattern = '<a class="hoster-thumbnail" data-hoster="([^"]+)" href="([^"]+)".+?<div class="status success-status">\s+<p>([^><]+)<'
-        r = re.findall(sPattern,sHtmlContent,re.DOTALL)
+        r = re.findall(sPattern, sHtmlContent, re.DOTALL)
         if not r:
             return False
 
@@ -70,11 +70,11 @@ class cJheberg:
             if not 'indisponible' in item[2]:
                 hoster = item[0]
                 slug = url.split('/')[-2]
-                data = { 'slug' : str(slug) , 'hoster' : str(hoster) }
+                data = { 'slug' : str(slug), 'hoster' : str(hoster) }
                 postdata = urllib.urlencode(data)
                 urllink = 'http://www.jheberg.net/get/link/'
                 
-                sHtmlContent = GetHtml(urllink,postdata)
+                sHtmlContent = GetHtml(urllink, postdata)
                 
                 r = re.search('{"url": "([^"]+)"}', sHtmlContent)
                 if r:              
