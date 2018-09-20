2d2349bc187837e8fc2af12c4accbc18041fffa1
razorex <individuel@free.fr>
2018-09-20 21:48:29 +0200

    Update megawatch.py

--- plugin.video.vstream/resources/hosters/megawatch.py
+++ plugin.video.vstream/resources/hosters/megawatch.py
@@ -1,6 +1,6 @@
 #-*- coding: utf-8 -*-
 #Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
-from resources.lib.handler.requestHandler import cRequestHandler 
+from resources.lib.handler.requestHandler import cRequestHandler
 from resources.hosters.hoster import iHoster
 from resources.lib.parser import cParser
 from resources.lib.packer import cPacker
@@ -17,20 +17,20 @@ class cHoster(iHoster):
         return  self.__sDisplayName
 
     def setDisplayName(self, sDisplayName):
-        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]'+self.__sDisplayName+'[/COLOR]'
+        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]' + self.__sDisplayName + '[/COLOR]'
 
     def setFileName(self, sFileName):
         self.__sFileName = sFileName
-        
+
     def getFileName(self):
         return self.__sFileName
 
     def getPluginIdentifier(self):
         return 'megawatch'
-        
+
     def setHD(self, sHD):
         self.__sHD = ''
-        
+
     def getHD(self):
         return self.__sHD
 
@@ -42,7 +42,7 @@ class cHoster(iHoster):
 
     def getPattern(self):
         return ''
-    
+
     def __getIdFromUrl(self, sUrl):
         return ''
 
@@ -54,32 +54,32 @@ class cHoster(iHoster):
 
     def __getUrl(self, media_id):
         return
-    
+
     def getMediaLink(self):
         return self.__getMediaLinkForGuest()
 
     def __getMediaLinkForGuest(self):
         api_call = False
-    
+
         sUrl = self.__sUrl
-        
+
         oRequest = cRequestHandler(sUrl)
         sHtmlContent = oRequest.request()
         if 'File was deleted' in sHtmlContent:
-            return False,False
-            
+            return False, False
+
         oParser = cParser()
         sPattern = '(eval\(function\(p,a,c,k,e(?:.|\s)+?\))<\/script>'
-        aResult = oParser.parse(sHtmlContent,sPattern)
+        aResult = oParser.parse(sHtmlContent, sPattern)
         if (aResult[0] == True):
             sHtmlContent = cPacker().unpack(aResult[1][0])
 
-        sPattern = '{file:"(http.+?mp4)"}' 
+        sPattern = '{file:"(http.+?mp4)"}'
         aResult = oParser.parse(sHtmlContent,sPattern)
         if (aResult[0] == True):
             api_call = aResult[1][0] #pas de choix qualité trouvé pour le moment
 
         if (api_call):
             return True, api_call
-            
+
         return False, False
