f9a351e8b60e569814509279dda4b3fd1705bd09
razorex <individuel@free.fr>
2018-09-20 20:13:19 +0200

    Update cloudcartel.py

--- plugin.video.vstream/resources/hosters/cloudcartel.py
+++ plugin.video.vstream/resources/hosters/cloudcartel.py
@@ -1,15 +1,15 @@
 #-*- coding: utf-8 -*-
 #https://cloudcartel.net/embed/video/xxx
-from resources.lib.handler.requestHandler import cRequestHandler 
+from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
 
 sPattern1 = '"url":"([^"]+)",'
- 
+
 class cHoster(iHoster):
 
     def __init__(self):
-        self.__sDisplayName = 'Cloudcartel'
+        self.__sDisplayName = 'CloudCartel'
         self.__sFileName = self.__sDisplayName
         self.__sHD = ''
 
@@ -17,20 +17,20 @@ class cHoster(iHoster):
         return  self.__sDisplayName
 
     def setDisplayName(self, sDisplayName):
-        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]'+self.__sDisplayName+'[/COLOR]'
+        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]' + self.__sDisplayName + '[/COLOR]'
 
     def setFileName(self, sFileName):
         self.__sFileName = sFileName
-        
+
     def getFileName(self):
         return self.__sFileName
 
     def getPluginIdentifier(self):
         return 'cloudcartel'
-        
+
     def setHD(self, sHD):
         self.__sHD = ''
-        
+
     def getHD(self):
         return self.__sHD
 
@@ -39,9 +39,9 @@ class cHoster(iHoster):
 
     def setUrl(self, sUrl):
         self.__sUrl = str(sUrl)
-        self.__sUrl = self.__sUrl.replace('cloudcartel.net/embed/video/','cloudcartel.vnadigital.com/download/link/')
+        self.__sUrl = self.__sUrl.replace('cloudcartel.net/embed/video/', 'cloudcartel.vnadigital.com/download/link/')
         #suivre
-        
+
     def getMediaLink(self):
         return self.__getMediaLinkForGuest()
 
@@ -55,9 +55,9 @@ class cHoster(iHoster):
         aResult = oParser.parse(sHtmlContent, sPattern1)
         if (aResult[0] == True):
             api_call = aResult[1][0]
-            
+
 
         if (api_call):
             return True, api_call
-            
+
         return False, False
