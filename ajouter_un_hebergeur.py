a95ab877123ac7f56e12013307fe6fb049bded0c
razorex <individuel@free.fr>
2018-09-21 09:36:04 +0200

    Update ajouter_un_hebergeur.py

--- ajouter_un_hebergeur.py
+++ ajouter_un_hebergeur.py
@@ -37,7 +37,7 @@ class cHoster(iHoster):
     #facultatif mais a laisser pour compatibilitee
     def setFileName(self, sFileName):
         self.__sFileName = sFileName
-        
+
     #facultatif mais a laisser pour compatibilitee
     def getFileName(self):
         return self.__sFileName
@@ -45,11 +45,11 @@ class cHoster(iHoster):
     def getPluginIdentifier(self):
         #Nom du fichier exact sans .py
         return 'ajouter_un_hebergeur'
-        
+
     #facultatif mais a laisser pour compatibilitee
     def setHD(self, sHD):
         self.__sHD = ''
-        
+
     #facultatif mais a laisser pour compatibilitee
     def getHD(self):
         return self.__sHD
@@ -65,7 +65,7 @@ class cHoster(iHoster):
     #facultatif mais a laisser pour compatibilitee
     def getPattern(self):
         return ''
-    
+
     #facultatif mais a laisser pour compatibilitee
     def __getIdFromUrl(self, sUrl):
         sPattern = "id=([^<]+)"
@@ -88,7 +88,7 @@ class cHoster(iHoster):
     #facultatif mais a laisser pour compatibilitee
     def __getUrl(self, media_id):
         return
-    
+
     #Fonction appelle par Vstream pour avoir le lien decode
     def getMediaLink(self):
         return self.__getMediaLinkForGuest()
@@ -98,25 +98,25 @@ class cHoster(iHoster):
         api_call = False
 
         oRequest = cRequestHandler(self.__sUrl)
-        #oRequest.addHeaderEntry('Referer','http://www.google.fr/') #Rajoute un header
+        #oRequest.addHeaderEntry('Referer', 'http://www.google.fr/') #Rajoute un header
         sHtmlContent = oRequest.request()
 
         oParser = cParser()
         sPattern =  'file: *"([^<>"]+?mp4)"'
         aResult = oParser.parse(sHtmlContent, sPattern)
-        
-        
+
+
         if (aResult[0]):
             api_call = aResult[1][0]
-            
+
         if (api_call):
             #Rajout d'un header ?
             #api_call = api_call + '|User-Agent=Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0'
             return True, api_call
-        
+
         return False, False
 
-        
+
 #Attention : Pour fonctionner le nouvel hebergeur doit etre rajoute dans le corps de Vstream, fichier Hosters.py.
 #----------------------------------------------------------------------------------------------------------------
 #
@@ -124,18 +124,18 @@ class cHoster(iHoster):
 #--------------------------------------
 #
 #            from resources.lib.comaddon import dialog
-#           
+#
 #            url=[]
 #            qua=[]
 #            api_call = False
-#            
+#
 #            for aEntry in aResult[1]:
 #                url.append(aEntry[0])
 #                qua.append(aEntry[1])
 #
-#            #Afichage du tableau
+#            #Affichage du tableau
 #            api_call = dialog().VSselectqual(qua, url)
-#                
+#
 #             if (api_call):
 #                  return True, api_call
 
