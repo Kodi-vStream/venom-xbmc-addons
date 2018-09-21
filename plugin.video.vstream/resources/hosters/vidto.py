7d69d30e75f906dd6573b8c44fa8e1fdab7211e1
razorex <individuel@free.fr>
2018-09-21 08:55:32 +0200

    Update vidto.py

--- plugin.video.vstream/resources/hosters/vidto.py
+++ plugin.video.vstream/resources/hosters/vidto.py
@@ -2,12 +2,12 @@ from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
 from resources.lib.packer import cPacker
-import re,time,xbmcgui
+import re, time, xbmcgui
 
 class cHoster(iHoster):
 
     def __init__(self):
-        self.__sDisplayName = 'Vidto'
+        self.__sDisplayName = 'VidTo'
         self.__sFileName = self.__sDisplayName
 
     def getDisplayName(self):
@@ -40,7 +40,7 @@ class cHoster(iHoster):
     def setUrl(self, sUrl):
         self.__sUrl = sUrl.replace('http://vidto.me/', '')
         self.__sUrl = self.__sUrl.replace('embed-', '')
-        self.__sUrl= re.sub(r'\-.*\.html','',self.__sUrl)
+        self.__sUrl= re.sub(r'\-.*\.html', '', self.__sUrl)
         self.__sUrl = 'http://vidto.me/' + str(self.__sUrl)
 
     def checkUrl(self, sUrl):
@@ -69,8 +69,8 @@ class cHoster(iHoster):
 
             oRequest.addParameters('referer', self.__sUrl)
             sHtmlContent = oRequest.request()
-            sHtmlContent = sHtmlContent.replace('file:""','')
-            
+            sHtmlContent = sHtmlContent.replace('file:""', '')
+
             sPattern = '(eval\(function\(p,a,c,k,e(?:.|\s)+?\))<\/script>'
             aResult = oParser.parse(sHtmlContent, sPattern)
             if (aResult[0] == True):
@@ -88,9 +88,9 @@ class cHoster(iHoster):
                 for i in aResult[1]:
                     url.append(str(i[0]))
                     qua.append(str(i[1]))
-      
+
                 if len(url) == 1:
-                    return True,url[0]
+                    return True, url[0]
 
                 elif len(url) > 1:
                     return True, url[0] #240p de nos jours serieux dialog choix inutile max vue 360p pour le moment
