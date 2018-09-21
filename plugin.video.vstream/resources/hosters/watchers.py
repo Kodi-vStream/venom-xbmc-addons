aaa41768b4e0d35736e46494dcf11db713f54913
razorex <individuel@free.fr>
2018-09-21 09:19:05 +0200

    Update watchers.py

--- plugin.video.vstream/resources/hosters/watchers.py
+++ plugin.video.vstream/resources/hosters/watchers.py
@@ -2,7 +2,7 @@
 #Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.hosters.hoster import iHoster
-from resources.lib.parser import cParser 
+from resources.lib.parser import cParser
 from resources.lib.packer import cPacker
 from resources.lib.comaddon import dialog
 
@@ -20,20 +20,20 @@ class cHoster(iHoster):
         return  self.__sDisplayName
 
     def setDisplayName(self, sDisplayName):
-        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]'+self.__sDisplayName+'[/COLOR]'
+        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]' + self.__sDisplayName + '[/COLOR]'
 
     def setFileName(self, sFileName):
         self.__sFileName = sFileName
-        
+
     def getFileName(self):
         return self.__sFileName
 
     def getPluginIdentifier(self):
         return 'watchers'
-        
+
     def setHD(self, sHD):
         self.__sHD = ''
-        
+
     def getHD(self):
         return self.__sHD
 
@@ -45,7 +45,7 @@ class cHoster(iHoster):
 
     def getPattern(self):
         return ''
-    
+
     def __getIdFromUrl(self, sUrl):
         return ''
 
@@ -57,56 +57,56 @@ class cHoster(iHoster):
 
     def __getUrl(self, media_id):
         return
-    
+
     def getMediaLink(self):
         return self.__getMediaLinkForGuest()
 
     def __getMediaLinkForGuest(self):
         api_call = False
-    
+
         sUrl = self.__sUrl
-        
+
         oRequest = cRequestHandler(sUrl)
         sHtmlContent = oRequest.request()
 
         oParser = cParser()
         sPattern = '(eval\(function\(p,a,c,k,e(?:.|\s)+?\))<\/script>'
-        aResult = oParser.parse(sHtmlContent,sPattern)
+        aResult = oParser.parse(sHtmlContent, sPattern)
         if (aResult[0] == True):
             sHtmlContent = cPacker().unpack(aResult[1][0])
-            
+
         sPattern =  '{file:"(http.+?m3u8)"}' #sPattern = '{file:"([^"]+)",label:"(\d+)"}'
-        aResult = oParser.parse(sHtmlContent,sPattern)
+        aResult = oParser.parse(sHtmlContent, sPattern)
         if (aResult[0] == True):
             m3url = aResult[1][0]
             oRequest = cRequestHandler(m3url)
-            oRequest.addHeaderEntry('User-Agent',UA)
-            oRequest.addHeaderEntry('Referer','http://watchers.to/player7/jwplayer.flash.swf')
+            oRequest.addHeaderEntry('User-Agent', UA)
+            oRequest.addHeaderEntry('Referer', 'http://watchers.to/player7/jwplayer.flash.swf')
             sHtmlContent = oRequest.request()
 
         #fh = open('c:\\test.txt', "w")
         #fh.write(sHtmlContent)
         #fh.close()
-   
-        sPattern =  ',RESOLUTION=(.+?),.+?(http.+?m3u8)' 
-        aResult = oParser.parse(sHtmlContent,sPattern)
+
+        sPattern =  ',RESOLUTION=(.+?),.+?(http.+?m3u8)'
+        aResult = oParser.parse(sHtmlContent, sPattern)
         if (aResult[0] == True):
             #initialisation des tableaux
             url=[]
             qua=[]
-            #Replissage des tableaux
+            #Remplissage des tableaux
             for i in aResult[1]:
                 url.append(str(i[1]))
-                qua.append(str(i[0])) 
-            
+                qua.append(str(i[0]))
+
             #tableau
             api_call = dialog().VSselectqual(qua, url)
-                    
-            # ne fonctionne pas a partir des fichiers mp4 (video de 3 minutes) meme sur firefox ???      
-            api_call = api_call + '|User-Agent='+ UA
+
+            # ne fonctionne pas a partir des fichiers mp4 (video de 3 minutes) meme sur firefox ???
+            api_call = api_call + '|User-Agent=' + UA
             api_call = api_call + '&Referer=http://watchers.to/player7/jwplayer.flash.swf'
-            
+
         if (api_call):
             return True, api_call
-            
+
         return False, False
