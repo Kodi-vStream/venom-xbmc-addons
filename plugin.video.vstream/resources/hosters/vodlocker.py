2607c189b3ec102d9dc058fb85346ebf137c91bc
razorex <individuel@free.fr>
2018-09-21 09:14:53 +0200

    Update vodlocker.py

--- plugin.video.vstream/resources/hosters/vodlocker.py
+++ plugin.video.vstream/resources/hosters/vodlocker.py
@@ -15,7 +15,7 @@ class cHoster(iHoster):
         return  self.__sDisplayName
 
     def setDisplayName(self, sDisplayName):
-        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]'+self.__sDisplayName+'[/COLOR] [COLOR khaki]'+self.__sHD+'[/COLOR]'
+        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]' + self.__sDisplayName + '[/COLOR] [COLOR khaki]' + self.__sHD + '[/COLOR]'
 
     def setFileName(self, sFileName):
         self.__sFileName = sFileName
@@ -40,20 +40,20 @@ class cHoster(iHoster):
 
     def getPattern(self):
         return ''
-        
+
     def __getIdFromUrl(self, sUrl):
         oParser = cParser()
-        
+
         sPattern = 'http://((?:www.)?vodlocker.com)/(?:embed-)?([0-9a-zA-Z]+)(?:-\d+x\d+.html)?'
         aResult = oParser.parse(sUrl, sPattern)
         if (aResult[0] == True):
             return aResult[1][0][1]
-            
+
         sPattern = 'http://vodlocker.com/([^<]+)'
         aResult = oParser.parse(sUrl, sPattern)
         if (aResult[0] == True):
             return aResult[1][0]
-            
+
         return ''
 
     def setUrl(self, sUrl):
@@ -74,9 +74,9 @@ class cHoster(iHoster):
 
         oRequest = cRequestHandler(sUrl)
         sHtmlContent = oRequest.request()
-        
+
         oParser = cParser()
-        
+
         # merci de laisser cette partie, elle me servira plus tard
         # sPattern = '<Form method="POST" action='
         # aResult = oParser.parse(sHtmlContent, sPattern)
@@ -84,7 +84,7 @@ class cHoster(iHoster):
 
         # if (aResult[0] == True):
             # url = self.__sUrl
-        
+
             # op = oParser.parse(sHtmlContent, 'name="op" value="(.+?)">')
             # id = oParser.parse(sHtmlContent, 'name="id" value="(.+?)">')
             # fname = oParser.parse(sHtmlContent, 'name="fname" value="(.+?)">')
@@ -92,31 +92,28 @@ class cHoster(iHoster):
             # #ameliorer op pattern, pour le moment je force a download1
             # query_args = { 'op' : 'download1' , 'usr_login' : '' , 'id' : str(id[1][0]), 'fname' : str(fname[1][0]) , 'referer' :'', 'hash' : str(hash[1][0]), 'imhuman' : 'Proceed to video'}
             # data = urllib.urlencode(query_args)
-      
+
             # headers = {'User-Agent' : 'Mozilla 5.10', 'Referer' : str(url)}
-            
+
             # request = urllib2.Request(url,data,headers)
-          
-            # try: 
+
+            # try:
                 # reponse = urllib2.urlopen(request)
             # except URLError, e:
                 # print e.read()
                 # print e.reason
-          
+
             # html = reponse.read()
             # sHtmlContent = html
-        
+
         sPattern = 'file: "([^"]+)"'
-        
-        sHtmlContent=sHtmlContent.replace('|','/')
+
+        sHtmlContent=sHtmlContent.replace('|', '/')
         aResult = oParser.parse(sHtmlContent, sPattern)
 
-        
+
         if (aResult[0] == True):
             api_call = aResult[1][0]
             return True, api_call
-            
+
         return False, False
-        
-       
-        
