ae7b4412d6636c19b6411329906e71a18e8af4ca
razorex <individuel@free.fr>
2018-09-21 08:36:04 +0200

    Update vidbull.py

--- plugin.video.vstream/resources/hosters/vidbull.py
+++ plugin.video.vstream/resources/hosters/vidbull.py
@@ -25,7 +25,7 @@ class cHoster(iHoster):
         return  self.__sDisplayName
 
     def setDisplayName(self, sDisplayName):
-        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]'+self.__sDisplayName+'[/COLOR]'
+        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]' + self.__sDisplayName + '[/COLOR]'
 
     def setFileName(self, sFileName):
         self.__sFileName = sFileName
@@ -44,16 +44,16 @@ class cHoster(iHoster):
 
     def getPattern(self):
         return ''
-        
+
     def __getIdFromUrl(self):
         return ''
-        
+
     def __modifyUrl(self, sUrl):
         return ''
 
     def setUrl(self, sUrl):
         self.__sUrl = sUrl
-        
+
     def checkUrl(self, sUrl):
         return True
 
@@ -64,48 +64,48 @@ class cHoster(iHoster):
         return self.__getMediaLinkForGuest()
 
     def __getMediaLinkForGuest(self):
-        
+
         url_stream = ''
 
         oRequest = cRequestHandler(self.__sUrl)
         sHtmlContent = oRequest.request()
-        
+
         oParser = cParser()
-        
+
         sPattern =  "<script type='text\/javascript'>(eval\(function\(p,a,c,k,e,d.+?)<\/script>"
         aResult = oParser.parse(sHtmlContent, sPattern)
-        
+
         if (aResult[0] == True):
             for i in aResult[1]:
                 sHtmlContent = cPacker().unpack(i)
                 #xbmc.log(sHtmlContent)
-                
+
                 #Premiere methode avec <embed>
                 if '<embed' in sHtmlContent:
                     pass
-                
+
                 #deuxieme methode, lien code aes
                 else:
                     EncodedLink = re.search('file:"([^"]+)"', sHtmlContent, re.DOTALL)
 
                     if (EncodedLink):
-                        
+
                         Key = "a949376e37b369" + "f17bc7d3c7a04c5721"
-                        x = GKDecrypter(128,128)
+                        x = GKDecrypter(128, 128)
                         sUrl = x.decrypt(EncodedLink.group(1), Key.decode("hex"), "ECB").split('\0')[0]
 
                         #Si utilise pyaes
                         #import resources.lib.pyaes as pyaes
-                        #decryptor = pyaes.new(Key.decode("hex"), pyaes.MODE_ECB, IV='')
+                        #decryptor = pyaes.new(Key.decode("hex"), pyaes.MODE_ECB, IV = '')
                         #sUrl = decryptor.decrypt(lt.decode("hex")).replace('\x00', '')
-                        
+
                         #xbmc.log('>> ' + sUrl)
-                        
+
                         url_stream = sUrl
 
-        if (url_stream): 
+        if (url_stream):
             return True, url_stream
         else:
             return False, False
-        
+
         return False, False
