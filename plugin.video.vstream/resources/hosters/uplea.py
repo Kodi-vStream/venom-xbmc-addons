65c656bf824640fdfe189da0d520e3af0d9e6d71
razorex <individuel@free.fr>
2018-09-21 08:00:48 +0200

    Update uplea.py

--- plugin.video.vstream/resources/hosters/uplea.py
+++ plugin.video.vstream/resources/hosters/uplea.py
@@ -17,7 +17,7 @@ class cHoster(iHoster):
         return  self.__sDisplayName
 
     def setDisplayName(self, sDisplayName):
-        self.__sDisplayName = sDisplayName + ' [COLOR violet]'+self.__sDisplayName+'[/COLOR]'
+        self.__sDisplayName = sDisplayName + ' [COLOR violet]' + self.__sDisplayName + '[/COLOR]'
 
     def setFileName(self, sFileName):
         self.__sFileName = sFileName
@@ -36,7 +36,7 @@ class cHoster(iHoster):
 
     def getPattern(self):
         return ''
-        
+
     def __getIdFromUrl(self,url):
         sPattern = 'http:\/\/uplea\.com\/dl\/([0-9a-zA-Z]+)'
         oParser = cParser()
@@ -44,10 +44,10 @@ class cHoster(iHoster):
         if (aResult[0] == True):
             return aResult[1][0]
         return ''
-        
+
     def __modifyUrl(self, sUrl):
         return ''
-        
+
     def __getKey(self):
         return ''
 
@@ -68,33 +68,33 @@ class cHoster(iHoster):
         return self.__getMediaLinkForGuest()
 
     def __getMediaLinkForGuest(self):
-        
+
         #http:///dl/12345XXYEEEEREERERE
-        
+
         UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0'
         headers = {'User-Agent': UA ,
-                   'Host' : 'uplea.com',
-                   #'Referer' : self.__sUrl ,
+                   'Host': 'uplea.com',
+                   #'Referer': self.__sUrl ,
                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                   'Accept-Language' : 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3'
+                   'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3'
                    #'Content-Type': 'application/x-www-form-urlencoded'
                    }
-        
-        req = urllib2.Request(self.__sUrl,None,headers)
+
+        req = urllib2.Request(self.__sUrl, None, headers)
         response = urllib2.urlopen(req)
         sHtmlContent = response.read()
         head = response.headers
         response.close()
-        
+
         oParser = cParser()
-        
+
         #get step
         urlstep = ''
         sPattern =  '<a href="(\/step\/[^<>"]+)">'
         aResult = oParser.parse(sHtmlContent, sPattern)
         if (aResult[0] == True):
             urlstep = aResult[1][0]
-            
+
         #get cookie
         cookies = ''
         if 'Set-Cookie' in head:
@@ -103,13 +103,13 @@ class cHoster(iHoster):
             aResult = oParser.parse(str(cookies), sPattern)
             if (aResult[0] == True):
                 cookies = str(aResult[1][0][0]) + str(aResult[1][0][1])
-        
+
         url = 'http://uplea.com' + urlstep
-        
+
         headers['Cookie'] = cookies
         headers['Referer'] = self.__sUrl
-        
-        req = urllib2.Request(url,None,headers)
+
+        req = urllib2.Request(url, None, headers)
         response = urllib2.urlopen(req)
         sHtmlContent = response.read()
         head = response.headers
@@ -118,23 +118,23 @@ class cHoster(iHoster):
         #fh = open('c:\\test.txt', "w")
         #fh.write(sHtmlContent)
         #fh.close()
-        
+
         #waiting time
         waitingtime = 20
         sPattern =  "ulCounter\({'timer':([0-9]+)}\);"
         aResult = oParser.parse(sHtmlContent, sPattern)
         if (aResult[0] == True):
             waitingtime = int(aResult[1][0]) + 2
-        
+
         sPattern =  '<a class="button-download" href="([^<>"]+?)">'
         aResult = oParser.parse(sHtmlContent, sPattern)
-        
+
         if (aResult[0] == True):
-            dialog.VSinfo('Waiting time' , self.__sDisplayName, waitingtime)
+            dialog.VSinfo('Waiting time', self.__sDisplayName, waitingtime)
             xbmc.sleep(waitingtime*1000)
-            
+
             #print aResult[1][0]
-            
+
             return True, aResult[1][0] + '|User-Agent=' + UA# + '&Referer=' + self.__sUrl
-        
+
         return False, False
