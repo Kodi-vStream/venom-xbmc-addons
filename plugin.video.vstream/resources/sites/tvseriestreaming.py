29f2d4f6e8a1f47b06f462958893cc89501eea7f
razorex <individuel@free.fr>
2018-10-12 00:30:59 +0200

    url du site
    Nouvelle url.

--- plugin.video.vstream/resources/sites/tvseriestreaming.py
+++ plugin.video.vstream/resources/sites/tvseriestreaming.py
@@ -17,7 +17,7 @@ SITE_IDENTIFIER = 'tvseriestreaming'
 SITE_NAME = 'Tv_seriestreaming'
 SITE_DESC = 'Séries & Animés en Streaming'
 
-URL_MAIN = 'https://serie.seriestreaming.site/'
+URL_MAIN = 'https://fr.seriestreaming.site/'
 
 SERIE_SERIES = ('http://', 'load')
 SERIE_NEWS = (URL_MAIN + 'nouveaux-episodes', 'showMovies')
