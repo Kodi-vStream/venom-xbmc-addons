601a0862a1082d867fdea8879ea278528355db35
Arias800 <azertypoil630@gmail.com>
2018-10-19 17:47:02 +0200

    Create pdj.py

---
+++ plugin.video.vstream/resources/hosters/pdj.py
@@ -0,0 +1,90 @@
+#-*- coding: utf-8 -*-
+#Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
+#Votre pseudo
+from resources.lib.handler.requestHandler import cRequestHandler #requete url
+from resources.lib.parser import cParser #recherche de code
+from resources.hosters.hoster import iHoster
+
+import re, urllib2, urllib
+
+class cHoster(iHoster):
+
+    def __init__(self):
+        #Nom a afficher dans Vstream
+        self.__sDisplayName = 'Promo DJ'
+        self.__sFileName = self.__sDisplayName
+        self.__sHD = ''
+
+    def getDisplayName(self):
+        return  self.__sDisplayName
+
+    def setDisplayName(self, sDisplayName):
+        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]' + self.__sDisplayName + '[/COLOR]'
+
+    def setFileName(self, sFileName):
+        self.__sFileName = sFileName
+
+    def getFileName(self):
+        return self.__sFileName
+
+    def getPluginIdentifier(self):
+        #Nom du fichier exact sans .py
+        return 'pdj'
+
+    def setHD(self, sHD):
+        self.__sHD = ''
+
+    def getHD(self):
+        return self.__sHD
+
+    def isDownloadable(self):
+        return True
+
+    def isJDownloaderable(self):
+        return True
+
+    def getPattern(self):
+        return ''
+
+    def __getIdFromUrl(self, sUrl):
+        sPattern = "id=([^<]+)"
+        oParser = cParser()
+        aResult = oParser.parse(sUrl, sPattern)
+        if (aResult[0] == True):
+            return aResult[1][0]
+
+        return ''
+
+    def setUrl(self, sUrl):
+        self.__sUrl = str(sUrl)
+        #self.__sUrl = self.__sUrl.replace('https://', 'http://')
+
+    def checkUrl(self, sUrl):
+        return True
+
+    def __getUrl(self, media_id):
+        return
+
+    def getMediaLink(self):
+        return self.__getMediaLinkForGuest()
+
+    def __getMediaLinkForGuest(self):
+        api_call = False
+
+        oRequest = cRequestHandler(self.__sUrl)
+        sHtmlContent = oRequest.request()
+
+        oParser = cParser()
+        sPattern =  '<span class="download">.+?href="(.+?)" ambatitle="Download podcast">'
+        aResult = oParser.parse(sHtmlContent, sPattern)
+
+
+        if (aResult[0]):
+            api_call = aResult[1][0]
+
+        if (api_call):
+            #Rajout d'un header ?
+            #api_call = api_call + '|User-Agent=Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0'
+            return True, api_call
+
+        return False, False
