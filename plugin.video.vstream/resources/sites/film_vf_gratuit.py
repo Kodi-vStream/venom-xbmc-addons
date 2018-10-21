3751499030718b3420dd2534d12c69cf1e58dd97
TmpName <gihubtmp@yopmail.com>
2018-10-21 10:46:20 +0000

    Update film_vf_gratuit.py

--- plugin.video.vstream/resources/sites/film_vf_gratuit.py
+++ plugin.video.vstream/resources/sites/film_vf_gratuit.py
@@ -119,7 +119,7 @@ def showMovies(sSearch = ''):
     oGui = cGui()
     oParser = cParser()
     if sSearch:
-      sUrl = sSearch
+      sUrl = sSearch.replace(' ','+')
     else:
         oInputParameterHandler = cInputParameterHandler()
         sUrl = oInputParameterHandler.getValue('siteUrl')
