00c7e96908ee497fdc36bf826dda5196e3cc1372
LordVenom <lordvenom57@gmail.com>
2018-11-15 08:08:22 +0100

    more replace espace

--- plugin.video.vstream/resources/sites/fullstream.py
+++ plugin.video.vstream/resources/sites/fullstream.py
@@ -224,7 +224,7 @@ def showMovieYears():
 def showMovies(sSearch = ''):
     oGui = cGui()
     if sSearch:
-      sUrl = sSearch
+      sUrl = sSearch.replace(' ', '+')
     else:
         oInputParameterHandler = cInputParameterHandler()
         sUrl = oInputParameterHandler.getValue('siteUrl')
