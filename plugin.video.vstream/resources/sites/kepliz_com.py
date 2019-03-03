975812f644b485c318f47692687148f59b3cfd57
magicseb <jeremiah252@hotmail.com>
2019-03-03 23:30:53 +0100

    Fixing Search, now multiple results

--- plugin.video.vstream/resources/sites/kepliz_com.py
+++ plugin.video.vstream/resources/sites/kepliz_com.py
@@ -111,7 +111,7 @@ def showMovies(sSearch = ''):
         #limite de caractere sinon bug de la recherche
         sSearch = sSearch[:20]
         sUrl = URL_MAIN + 'index.php?ordering=&searchphrase=all&option=com_search&searchword=' + sSearch.replace(' ','+')
-        sPattern = 'class="contentpaneopen".+?href="\/[0-9a-zA-Z]+\/(.+?)".+?>(.+?)</a>' #wobno
+        sPattern = 'class="small".+?href="\/[0-9a-zA-Z]+\/(.+?)".+?>(.+?)</a>' #wobno
     else :
         oInputParameterHandler = cInputParameterHandler()
         sUrl = oInputParameterHandler.getValue('siteUrl')
