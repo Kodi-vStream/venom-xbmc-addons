ff3afa732706d500b1679fcca0332519206d191f
Arias800 <azertypoil630@gmail.com>
2019-03-10 16:50:06 +0100

    Reste jheberg a fix car les liens retourne 400

--- plugin.video.vstream/resources/lib/multihost.py
+++ plugin.video.vstream/resources/lib/multihost.py
@@ -3,41 +3,53 @@
 #Venom.
 from resources.lib.handler.requestHandler import cRequestHandler
 import re, urllib
+from resources.lib.comaddon import VSlog
 UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0',
 #modif cloudflare
 def GetHtml(url, postdata = None):
 
-    sHtmlContent = ''
-    oRequest = cRequestHandler(url)
-    oRequest.setRequestType(1)
-    oRequest.addHeaderEntry('User-Agent', UA)
-    
-    if postdata != None:
-        oRequest.addHeaderEntry('X-Requested-With', 'XMLHttpRequest')
-        oRequest.addHeaderEntry('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
-        oRequest.addHeaderEntry('Referer', 'http://www.jheberg.net/redirect/xxxxxx/yyyyyy/')
-        
-    elif 'www.jheberg.net' in url:
-        oRequest.addHeaderEntry('Referer', url.replace('http://www.jheberg.net/mirrors', 'http://www.jheberg.net/captcha').replace('\r', ''))
-    
-    oRequest.addParametersLine(postdata)
+    if 'download.jheberg.net/redirect' in url:
+        oRequest = cRequestHandler(url)
+        sHtmlContent = oRequest.request()
+        url = oRequest.getRealUrl()
+        return url
+    else:
+        sHtmlContent = ''
+        oRequest = cRequestHandler(url)
+        oRequest.setRequestType(1)
+        oRequest.addHeaderEntry('User-Agent', UA)
 
-    sHtmlContent = oRequest.request()
+        if postdata != None:
+            oRequest.addHeaderEntry('X-Requested-With', 'XMLHttpRequest')
+            oRequest.addHeaderEntry('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
+            oRequest.addHeaderEntry('Referer', 'https://download.jheberg.net/redirect/xxxxxx/yyyyyy/')
 
-    return sHtmlContent
+        elif 'download.jheberg.net' in url:
+            oRequest.addHeaderEntry('Host','download.jheberg.net')
+            oRequest.addHeaderEntry('Referer', url)
+
+        oRequest.addParametersLine(postdata)
+
+        sHtmlContent = oRequest.request()
+
+        return sHtmlContent
 
 class cMultiup:
     def __init__(self):
         self.id = ''
         self.list = []
-        
+
     def GetUrls(self, url):
+        sHtmlContent = GetHtml(url)
+        sPattern = '<form action="(.+?)" method="post">'
+        result = re.findall(sPattern,sHtmlContent)
+        url = 'https://multiup.org'+''.join(result[0])
 
         NewUrl = url.replace('http://www.multiup.org/fr/download', 'http://www.multiup.eu/fr/mirror').replace('http://www.multiup.eu/fr/download', 'http://www.multiup.eu/fr/mirror').replace('http://www.multiup.org/download', 'http://www.multiup.eu/fr/mirror')
-        
+
         sHtmlContent = GetHtml(NewUrl)
 
-        sPattern = 'nameHost="([^"]+)"\s+link="([^"]+)"\s+validity="([a-z]+)"'
+        sPattern = 'nameHost="([^"]+)".+?link="([^"]+)".+?class="([^"]+)"'
         r = re.findall(sPattern,sHtmlContent, re.DOTALL)
 
         if not r:
@@ -45,40 +57,32 @@ class cMultiup:
 
         for item in r:
 
-            if item[2] == 'valid' and not 'download-fast' in item[1]:
+            if 'bounce-to-right' in str(item[2]) and not 'download-fast' in item[1]:
                 self.list.append(item[1])
-            
+
         return self.list
 
 class cJheberg:
     def __init__(self):
         self.id = ''
         self.list = []
-        
-    def GetUrls(self, url):
 
-        NewUrl = url.replace('http://jheberg.net', 'http://www.jheberg.net').replace('captcha', 'mirrors')
-        
+    def GetUrls(self, url):
+        idFile = url.rsplit('/', 1)[-1]
+        NewUrl = 'https://api.jheberg.net/file/'+idFile
         sHtmlContent = GetHtml(NewUrl)
-        
-        sPattern = '<a class="hoster-thumbnail" data-hoster="([^"]+)" href="([^"]+)".+?<div class="status success-status">\s+<p>([^><]+)<'
+
+        sPattern = '"hosterId":([^"]+),"hosterName":"([^"]+)",".+?status":"([^"]+)"'
         r = re.findall(sPattern, sHtmlContent, re.DOTALL)
         if not r:
             return False
 
         for item in r:
-            if not 'indisponible' in item[2]:
-                hoster = item[0]
-                slug = url.split('/')[-2]
-                data = { 'slug' : str(slug), 'hoster' : str(hoster) }
-                postdata = urllib.urlencode(data)
-                urllink = 'http://www.jheberg.net/get/link/'
-                
-                sHtmlContent = GetHtml(urllink, postdata)
-                
-                r = re.search('{"url": "([^"]+)"}', sHtmlContent)
-                if r:              
-                    self.list.append(r.group(1))
-            
+            if not 'ERROR' in item[2]:
+                urllink = 'https://download.jheberg.net/redirect/'+idFile+'-'+item[0]
+                url = GetHtml(urllink)
+
+                self.list.append(url)
+
         return self.list
-        
+
