57179bf73e09701de0a450472b0588ef0c615031
TmpName <gihubtmp@yopmail.com>
2019-05-04 12:16:38 +0000

    Create verystream.py

---
+++ plugin.video.vstream/resources/hosters/verystream.py
@@ -0,0 +1,76 @@
+#coding: utf-8
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.parser import cParser
+from resources.hosters.hoster import iHoster
+import xbmcgui, re
+
+from resources.lib.comaddon import VSlog
+
+class cHoster(iHoster):
+
+    def __init__(self):
+        self.__sDisplayName = 'VeryStream'
+        self.__sFileName = self.__sDisplayName
+
+    def getDisplayName(self):
+        return  self.__sDisplayName
+
+    def setDisplayName(self, sDisplayName):
+        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]' + self.__sDisplayName + '[/COLOR]'
+
+    def setFileName(self, sFileName):
+        self.__sFileName = sFileName
+
+    def getFileName(self):
+        return self.__sFileName
+
+    def getPluginIdentifier(self):
+        return 'verystream'
+
+    def isDownloadable(self):
+        return True
+
+    def isJDownloaderable(self):
+        return True
+
+    def getPattern(self):
+        return ''
+
+    def __getIdFromUrl(self):
+        return ''
+
+    def __modifyUrl(self, sUrl):
+        return ''
+
+    def setUrl(self, sUrl):
+        self.__sUrl = sUrl
+
+    def checkUrl(self, sUrl):
+        return True
+
+    def getUrl(self):
+        return self.__sUrl
+
+    def getMediaLink(self):
+        return self.__getMediaLinkForGuest()
+
+    def __getMediaLinkForGuest(self):
+
+        oRequest = cRequestHandler(self.__sUrl)
+        sHtmlContent = oRequest.request()
+
+        api_call = ''
+
+        oParser = cParser()
+
+        sPattern =  'id="videolink">([^<>]+)<\/p>'
+        aResult = re.findall(sPattern, sHtmlContent)
+        
+        if (aResult):
+            
+            api_call = 'https://verystream.com/gettoken/' + aResult[0] + '?mime=true'
+
+        if (api_call):
+            return True, api_call
+
+        return False, False
