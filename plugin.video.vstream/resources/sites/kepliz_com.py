031d1c65a30edf11cb8c8235925c05aa294f10d9
sizanic <47307622+sizanic@users.noreply.github.com>
2019-07-07 17:08:44 +0200

    Kepliz pointe sur Wonior
    Pirvox ne réponds plus
    -> On bascule sur un clone

--- plugin.video.vstream/resources/sites/kepliz_com.py
+++ plugin.video.vstream/resources/sites/kepliz_com.py
@@ -15,8 +15,7 @@ import unicodedata
 SITE_IDENTIFIER = 'kepliz_com'
 SITE_NAME = 'Kepliz'
 SITE_DESC = 'Films en streaming'
-URL_HOST = 'http://www.pirvox.com/'
-#URL_HOST = 'http://www.ozporo.com/'
+URL_HOST = 'http://www.wonior.com/'
 URL_MAIN = 'URL_MAIN'
 
 #pour l'addon
@@ -111,7 +110,7 @@ def showMovies(sSearch = ''):
         #limite de caractere sinon bug de la recherche
         sSearch = sSearch[:20]
         sUrl = URL_MAIN + 'index.php?ordering=&searchphrase=all&option=com_search&searchword=' + sSearch.replace(' ', '+')
-        sPattern = '<div style="font-size:20px">.+?href="\/[0-9a-zA-Z]+\/(.+?)".+?>(.+?)</a>'
+        sPattern = '<h4><a href="\/[0-9a-zA-Z]+\/(.+?)"  >(.+?)<'
     else:
         oInputParameterHandler = cInputParameterHandler()
         sUrl = oInputParameterHandler.getValue('siteUrl')
@@ -123,7 +122,7 @@ def showMovies(sSearch = ''):
     data = response.read()
     response.close()
     sMainUrl = ''
-    aResult = oParser.parse(data, 'window\.location\.href="([0-9a-zA-Z]+)";')
+    aResult = oParser.parse(data, '<a href="([0-9a-zA-Z]+)"')
 
     if aResult[0]:
         #memorisation pour la suite
