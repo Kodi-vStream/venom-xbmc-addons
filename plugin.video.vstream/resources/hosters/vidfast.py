5700ce191e258cb3b45bafb09ba50f3ce95cf6bf
johngf <john59650@live.fr>
2019-12-01 14:57:16 +0100

    Create vidfast.py

---
+++ plugin.video.vstream/resources/hosters/vidfast.py
@@ -0,0 +1,64 @@
+#coding: utf-8
+#Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.parser import cParser
+from resources.hosters.hoster import iHoster
+
+class cHoster(iHoster):
+
+    def __init__(self):
+        self.__sDisplayName = 'Vidfast'
+        self.__sFileName = self.__sDisplayName
+        self.__sHD = ''
+
+    def getDisplayName(self):
+        return  self.__sDisplayName
+
+    def setDisplayName(self, sDisplayName):
+        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]' + self.__sDisplayName + '[/COLOR]'
+
+    def setFileName(self, sFileName):
+        self.__sFileName = sFileName
+
+    def getFileName(self):
+        return self.__sFileName
+
+    def getPluginIdentifier(self):
+        return 'vidfast'
+
+    def setHD(self, sHD):
+        self.__sHD = ''
+
+    def getHD(self):
+        return self.__sHD
+
+    def isDownloadable(self):
+        return True
+
+    def setUrl(self, sUrl):
+        self.__sUrl = str(sUrl)
+
+    def getUrl(self):
+        return self.__sUrl
+
+    def getMediaLink(self):
+        return self.__getMediaLinkForGuest()
+
+    def __getMediaLinkForGuest(self):
+ 
+        api_call = ''
+        
+        oParser = cParser()
+        oRequest = cRequestHandler(self.__sUrl)
+        sHtmlContent = oRequest.request()
+
+        sPattern = '{file:"([^"]+)"}'
+        aResult = oParser.parse(sHtmlContent, sPattern)
+        if (aResult[0] == True):
+            api_call = aResult[1][0].replace(',','').replace('master.m3u8','index-v1-a1.m3u8')
+
+
+        if (api_call):
+            return True, api_call
+
+        return False, False
