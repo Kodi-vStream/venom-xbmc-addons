88807c580774e0929291a46a10f5610579c568ec
johngf <john59650@live.fr>
2019-12-11 12:44:27 +0100

    Update onlystream.py

--- plugin.video.vstream/resources/hosters/onlystream.py
+++ plugin.video.vstream/resources/hosters/onlystream.py
@@ -1,12 +1,10 @@
 #-*- coding: utf-8 -*-
 #Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
-#
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
-
 from resources.lib.packer import cPacker
-from resources.lib.comaddon import VSlog,dialog
+from resources.lib.comaddon import dialog
 
 class cHoster(iHoster):
 
@@ -39,15 +37,6 @@ class cHoster(iHoster):
     def isDownloadable(self):
         return True
 
-    def isJDownloaderable(self):
-        return True
-
-    def getPattern(self):
-        return ''
-
-    def __getIdFromUrl(self, sUrl):
-        return ''
-
     def setUrl(self, sUrl):
         self.__sUrl = str(sUrl)
 
@@ -67,36 +56,32 @@ class cHoster(iHoster):
         oRequest = cRequestHandler(self.__sUrl)
         sHtmlContent = oRequest.request()
 
-        sPattern = '(\s*eval\s*\(\s*function\(p,a,c,k,e(?:.|\s)+?)<\/script>'
-        aResult = oParser.parse(sHtmlContent, sPattern)
-
-        #Attention sous titre present aussi
-
-        if (aResult[0] == True):
-            sHtmlContent = cPacker().unpack(aResult[1][0])
-            
-            
-
         sPattern =  '(?:file|src): *"([^"]+)"[^{}<>]+?(?:, *label: *"([^"]+)")*}'
         aResult = oParser.parse(sHtmlContent, sPattern)
-        
+
         if (aResult[0] == True):
+            api_call = aResult[1][0][0]
 
-            #initialisation des tableaux
-            url=[]
-            qua=[]
+        else:
+            sPattern = '(\s*eval\s*\(\s*function\(p,a,c,k,e(?:.|\s)+?)<\/script>'
+            aResult = oParser.parse(sHtmlContent, sPattern)
+            if (aResult[0] == True):
+                sHtmlContent = cPacker().unpack(aResult[1][0])
 
-            #Remplissage des tableaux
-            for i in aResult[1]:
-                url.append(str(i[0]))
-                if len(i) > 1:
-                    q = str(i[1])
-                else:
-                    q = "Inconnu"
-                qua.append(q)
+                sPattern =  '(?:file|src): *"([^"]+)"[^{}<>]+?(?:, *label: *"([^"]+)")*}'
+                aResult = oParser.parse(sHtmlContent, sPattern)
+                if (aResult[0] == True):
+                    url=[]
+                    qua=[]
+                    for i in aResult[1]:
+                        url.append(str(i[0]))
+                        if len(i) > 1:
+                            q = str(i[1])
+                        else:
+                            q = "Inconnu"
+                            qua.append(q)
 
-            #Affichage du tableau
-            api_call = dialog().VSselectqual(qua, url)
+                    api_call = dialog().VSselectqual(qua, url)
 
         if (api_call):
             return True, api_call
