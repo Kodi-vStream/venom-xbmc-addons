35beb3522c6fc1a6b34255293a7ad848c8cd43bb
Arias800 <azertypoil630@gmail.com>
2020-03-01 12:54:40 +0100

    Update iptv_four_sat.py

--- plugin.video.vstream/resources/sites/iptv_four_sat.py
+++ plugin.video.vstream/resources/sites/iptv_four_sat.py
@@ -16,11 +16,21 @@ SITE_NAME = 'Iptv4Sat'
 SITE_DESC = 'Regarder la télévision'
 
 URL_MAIN = 'https://www.iptv4sat.com/'
-IPTV_WOLRDWiDE = URL_MAIN + 'category/worldwide-iptv/'
-LISTE_GRATUIT = URL_MAIN + 'category/free-list/'
-SPORT_LISTE = URL_MAIN + 'category/free-list/sports-m3u/'
-IPTV_ARABE = URL_MAIN + 'category/free-list/iptv-arabic/'
-IPTV_FRENCH = URL_MAIN + 'dl-iptv-french/'
+IPTV_WOLRDWiDE = URL_MAIN + 'category/worldwide-m3u-free/'
+SPORT_LISTE = URL_MAIN + 'category/m3u-sports-iptv/'
+SMART_IPTV = URL_MAIN + 'category/smart-iptv-free/'
+
+IPTV_ARABE = URL_MAIN + 'category/iptv-m3u-arabic/'
+IPTV_AMERICAIN = URL_MAIN + 'category/america-m3u-list/'
+IPTV_BELGE = URL_MAIN + 'category/belgique-iptv/'
+IPTV_CANADA = URL_MAIN + 'category/canada-iptv-m3u/'
+IPTV_FRENCH = URL_MAIN + 'category/france-m3u-iptv/'
+IPTV_ROUMANIE = URL_MAIN + 'category/iptv-romania/'
+IPTV_PAYSBAS = URL_MAIN + 'category/netherland-iptv/'
+IPTV_POLOGNE = URL_MAIN + 'poland-iptv/'
+IPTV_PORTUGAl = URL_MAIN + 'category/portugal-iptv-m3u/'
+IPTV_TURC = URL_MAIN + 'turkey-list-iptv/'
+IPTV_AUTRE = URL_MAIN + 'category/other-list/'
 
 def load():
     oGui = cGui()
@@ -34,74 +44,67 @@ def load():
     oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Derniere liste mondial', 'tv.png', oOutputParameterHandler)
 
     oOutputParameterHandler = cOutputParameterHandler()
-    oOutputParameterHandler.addParameter('siteUrl', LISTE_GRATUIT)
-    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste Gratuite', 'tv.png', oOutputParameterHandler)
-
-    oOutputParameterHandler = cOutputParameterHandler()
     oOutputParameterHandler.addParameter('siteUrl', SPORT_LISTE)
     oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste Sport', 'tv.png', oOutputParameterHandler)
 
     oOutputParameterHandler = cOutputParameterHandler()
-    oOutputParameterHandler.addParameter('siteUrl', IPTV_ARABE)
-    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste Chaine Arabe', 'tv.png', oOutputParameterHandler)
+    oOutputParameterHandler.addParameter('siteUrl', SMART_IPTV)
+    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Smart Iptv', 'tv.png', oOutputParameterHandler)
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', 'http://venom/')
+    oGui.addDir(SITE_IDENTIFIER, 'listePerContry', 'Liste par Pays', 'tv.png', oOutputParameterHandler)
+
+    oGui.setEndOfDirectory()
     
+def listePerContry():
+    oGui = cGui()
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', IPTV_ARABE)
+    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste chaines Arabe', 'tv.png', oOutputParameterHandler)
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', IPTV_AMERICAIN)
+    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste chaines Amerique', 'tv.png', oOutputParameterHandler)
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', IPTV_BELGE)
+    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste chaines Belgique', 'tv.png', oOutputParameterHandler)
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', IPTV_CANADA)
+    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste chaines Canada', 'tv.png', oOutputParameterHandler)
+
     oOutputParameterHandler = cOutputParameterHandler()
     oOutputParameterHandler.addParameter('siteUrl', IPTV_FRENCH)
-    oGui.addDir(SITE_IDENTIFIER, 'showDailyListFr', 'Derniere Liste France', 'tv.png', oOutputParameterHandler)
+    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste chaines France', 'tv.png', oOutputParameterHandler)
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', IPTV_ROUMANIE)
+    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste chaines Roumanie', 'tv.png', oOutputParameterHandler)
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', IPTV_PAYSBAS)
+    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste chaines Pays-bas', 'tv.png', oOutputParameterHandler)
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', IPTV_POLOGNE)
+    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste chaines Pologne', 'tv.png', oOutputParameterHandler)
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', IPTV_PORTUGAl)
+    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste chaines Portugal', 'tv.png', oOutputParameterHandler)
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', IPTV_TURC)
+    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste chaines Turc', 'tv.png', oOutputParameterHandler)
+
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', IPTV_TURC)
+    oGui.addDir(SITE_IDENTIFIER, 'showDailyList', 'Liste chaines des autres pays', 'tv.png', oOutputParameterHandler)
 
     oGui.setEndOfDirectory()
-    
-
-def showDailyListFr():
-    oGui = cGui()
-    oInputParameterHandler = cInputParameterHandler()
-    sUrl = oInputParameterHandler.getValue('siteUrl')
-    
-    #oGui.addText(SITE_IDENTIFIER, sUrl)
-
-    oParser = cParser()
-    sHtmlContent = getHtml(sUrl)
-    sPattern = '"https://www.iptv4sat.com/download-attachment/([^"]+)".+?class="attachment-caption">([^<]+)<'
-    
-    aResult = oParser.parse(sHtmlContent, sPattern)
-
-    if (aResult[0] == True):
-        total = len(aResult[1])
-        progress_ = progress().VScreate(SITE_NAME)
-
-        for aEntry in aResult[1]:
-            progress_.VSupdate(progress_, total)
-            if progress_.iscanceled():
-                break
-
-            sTitle = aEntry[1]
-            sUrl2 = 'https://www.iptv4sat.com/download-attachment/' + aEntry[0]
-
-            oOutputParameterHandler = cOutputParameterHandler()
-            oOutputParameterHandler.addParameter('siteUrl', sUrl2)
-            oOutputParameterHandler.addParameter('sMovieTitle', sTitle)
-
-            oGui.addDir(SITE_IDENTIFIER, 'showWeb', sTitle, 'listes.png', oOutputParameterHandler)
-            #oGui.addText(SITE_IDENTIFIER, sUrl2)
-            
-        progress_.VSclose(progress_)
-    # if (aResult[0] == True):
-
-                # sTitle = 'Liste M3u French'
-                # sUrl2 = 'https://www.iptv4sat.com/download-attachment/' + aResult[1][0]
-
-                # oOutputParameterHandler = cOutputParameterHandler()
-                # oOutputParameterHandler.addParameter('siteUrl', sUrl2)
-                # oOutputParameterHandler.addParameter('sMovieTitle', sTitle)
-
-                # oGui.addDir(SITE_IDENTIFIER, 'showWeb', sTitle, 'listes.png', oOutputParameterHandler)
-                # oGui.addText(SITE_IDENTIFIER, sUrl2)
-        
-    # else:
-        # oGui.addText(SITE_IDENTIFIER, 'pas trouvé')
-        
-    oGui.setEndOfDirectory()
-    
 
 def showDailyList():
     oGui = cGui()
@@ -163,62 +166,11 @@ def showAllPlaylist():#On recupere les differentes playlist si il y en a
 
     sHtmlContent = getHtml(sUrl)
 
-    sUrl2 = getDownloadLink(sHtmlContent)
+    url = re.search('<a href="([^"]+)".+class="da-download-link da-download-attachment', sHtmlContent).group(1)
 
-    for url in sUrl2:
+    oOutputParameterHandler = cOutputParameterHandler()
+    oOutputParameterHandler.addParameter('siteUrl', url)
 
-        if 'dl' in url:
-            VSlog('Redirect')
-            url = getRealLink(url)
-
-        oOutputParameterHandler = cOutputParameterHandler()
-        oOutputParameterHandler.addParameter('siteUrl', url)
-
-        oGui.addDir(SITE_IDENTIFIER, 'showWeb', sTitle, '', oOutputParameterHandler)
+    oGui.addDir(SITE_IDENTIFIER, 'showWeb', sTitle, '', oOutputParameterHandler)
 
     oGui.setEndOfDirectory()
-    
-def showAllPlaylist2():#On recupere les differentes playlist si il y en a
-    oGui = cGui()
-
-    oInputParameterHandler = cInputParameterHandler()
-    sUrl = oInputParameterHandler.getValue('siteUrl')
-    sTitle = oInputParameterHandler.getValue('sMovieTitle')
-
-    sHtmlContent = getHtml(sUrl)
-
-    sUrl2 = getDownloadLink(sHtmlContent)
-
-    for url in sUrl2:
-
-        if 'download' in url:
-            VSlog('Redirect')
-            url = getRealLink(url)
-            oGui.addText(SITE_IDENTIFIER, url)
-        else:
-            oGui.addText(SITE_IDENTIFIER, 'rien')
-        
-        oOutputParameterHandler = cOutputParameterHandler()
-        oOutputParameterHandler.addParameter('siteUrl', sUrl)
-
-        oGui.addDir(SITE_IDENTIFIER, 'showWeb', sTitle, '', oOutputParameterHandler)
-
-
-    oGui.setEndOfDirectory()
-
-def getRealLink(sUrl):
-    sHtmlContent = getHtml(sUrl)
-    return re.search('<a href="([^"]+)" class="attachment-link"', sHtmlContent).group(1)
-
-def getDownloadLink(sHtmlContent):
-    sUrl2 = []
-    urlDirect = re.search('<a href=".+?download(.+?)"', sHtmlContent)
-    if urlDirect:
-        urlDirect = URL_MAIN + 'download' + urlDirect.group(1)
-        sUrl2.append(urlDirect)
-
-    urlRedirect = re.search('<a href=".+?dl([^"]+)"', sHtmlContent)
-    if urlRedirect:
-        urlRedirect = URL_MAIN + 'dl' + urlRedirect.group(1)
-        sUrl2.append(urlRedirect)
-    return sUrl2
