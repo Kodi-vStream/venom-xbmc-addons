fe9fc08484f0421fa9847b8016d3c699458b445d
razorex <individuel@free.fr>
2020-03-18 11:40:40 +0100

    Update about.py
    Nettoyage

--- plugin.video.vstream/resources/lib/about.py
+++ plugin.video.vstream/resources/lib/about.py
@@ -2,7 +2,7 @@
 # https://github.com/Kodi-vStream/venom-xbmc-addons
 #Venom.
 
-#sLibrary = xbmc.translatePath("special://home/addons/plugin.video.vstream").decode("utf-8")
+#sLibrary = xbmc.translatePath('special://home/addons/plugin.video.vstream').decode('utf-8')
 #sys.path.append (sLibrary)
 
 from resources.lib.comaddon import addon, progress, dialog, xbmcgui, window, VSlog, xbmc
@@ -51,7 +51,7 @@ class cAbout:
         addons = addon()
         service_time = addons.getSetting('service_time')
         service_version = addons.getSetting('service_version')
-        #service_version = ""
+        #service_version = ''
 
         #Si pas d'heure indique = premiere install
         if not (service_time):
@@ -62,20 +62,19 @@ class cAbout:
 
         if not (service_version):
             #version de l'addon
-            addons.setSetting('service_version', str(addons.getAddonInfo("version")))
-            service_version = addons.getAddonInfo("version")
+            addons.setSetting('service_version', str(addons.getAddonInfo('version')))
+            service_version = addons.getAddonInfo('version')
 
         #si addon = 0.7.0 et service_version 0.6.35 pas de mise ajour.
-        if (addons.getAddonInfo("version") > service_version):
-            addons.setSetting('service_version', str(addons.getAddonInfo("version")))
-            service_version = addons.getAddonInfo("version")
-
+        if (addons.getAddonInfo('version') > service_version):
+            addons.setSetting('service_version', str(addons.getAddonInfo('version')))
+            service_version = addons.getAddonInfo('version')
 
         if (service_time):
             #delay mise a jour
-            time_sleep = datetime.timedelta(hours=72)
+            time_sleep = datetime.timedelta(hours = 72)
             time_now = datetime.datetime.now()
-            time_service = self.__strptime(service_time, "%Y-%m-%d %H:%M:%S.%f")
+            time_service = self.__strptime(service_time, '%Y-%m-%d %H:%M:%S.%f')
             #pour test
             #if (time_sleep):
             if (time_now - time_service > time_sleep):
@@ -92,62 +91,11 @@ class cAbout:
                     addons.setSetting('service_futur', str(result['tag_name']))
                     addons.setSetting('home_update', str('true'))
                     addons.setSetting('service_time', str(datetime.datetime.now()))
-                    dialog().VSinfo("Mise à jour disponible")
+                    dialog().VSinfo('Mise à jour disponible')
                 else:
                     addons.setSetting('service_time', str(datetime.datetime.now()))
                     addons.setSetting('home_update', str('false'))
-                    VSlog('Prochaine vérification de MAJ le: ' + str(time_sleep + time_service) )
-        return
-
-    def getUpdate_old(self):
-        addons = addon()
-        service_time = addons.getSetting('service_time')
-
-        #Si pas d'heure indique = premiere install
-        if not (service_time):
-            #On memorise la date d'aujourdhui
-            addons.setSetting('service_time', str(datetime.datetime.now()))
-            #Mais on force la maj avec une date a la con
-            service_time = '2000-09-23 10:59:50.877000'
-
-        if (service_time):
-            #delay mise a jour
-            time_sleep = datetime.timedelta(hours=72)
-            time_now = datetime.datetime.now()
-            time_service = self.__strptime(service_time, "%Y-%m-%d %H:%M:%S.%f")
-            #pour test
-            #if (time_sleep):
-            if (time_now - time_service > time_sleep):
-                #test les fichier pour mise a jour
-                #self.checkupdate()
-                result = self.resultGit()
-                sDown = 0
-
-                if result:
-                    for i in result:
-                        try:
-                            rootpath = self.getRootPath(i['path'])
-
-                            if self.checksize(rootpath,i['size']):
-                                sDown = sDown+1
-                                break #Si on en trouve un, pas besoin de tester les autres.
-
-                        except:
-                            VSlog('Erreur: vérification MAJ' )
-                            return
-
-                    if (sDown != 0):
-                        addons.setSetting('home_update', str('true'))
-                        addons.setSetting('service_time', str(datetime.datetime.now()))
-                        dialog().VSinfo("Mise à jour disponible")
-                    else:
-                        #cConfig().showInfo('vStream', 'Fichiers à jour')
-                        addons.setSetting('service_time', str(datetime.datetime.now()))
-                        addons.setSetting('home_update', str('false'))
-
-            else:
-                VSlog('Prochaine verification de MAJ le: ' + str(time_sleep + time_service) )
-                #Pas besoin de memoriser la date, a cause du cache kodi > pas fiable.
+                    VSlog('Prochaine vérification de MAJ le: ' + str(time_sleep + time_service))
         return
 
     #bug python
@@ -158,22 +106,11 @@ class cAbout:
             date = datetime.datetime(*(time.strptime(date, format)[0:6]))
         return date
 
-
-    def getRootPath_old(self, folder):
-        sMath = cConfig().getAddonPath().replace('plugin.video.vstream', '').decode("utf-8")
-
-        sFolder = os.path.join(sMath, folder)
-
-        # xbox hack
-        sFolder = sFolder.replace('\\', '/')
-        return sFolder
-
     def getRootPath(self, folder):
 
-        path = "special://home/addons"
+        path = 'special://home/addons'
         path = "/".join([path, folder])
         return path
-
 
     def resultGit(self):
         try:
@@ -245,7 +182,7 @@ class cAbout:
 
                         try:
                             self.__download(i['raw_url'], rootpath)
-                            #site += "Add: [B]%s[/B] | Del: [B]%s[/B] | [COLOR green]%s[/COLOR][CR]" % (i['additions'], i['deletions'], i['filename'].encode("utf-8"))
+                            #site += 'Add: [B]%s[/B] | Del: [B]%s[/B] | [COLOR green]%s[/COLOR][CR]' % (i['additions'], i['deletions'], i['filename'].encode('utf-8'))
                             add += i['additions']
                             dell += i['deletions']
                             sdown = sdown + 1
@@ -258,30 +195,30 @@ class cAbout:
                 progress_.VSclose(progress_)
 
                 #donner fichier
-                sContent = "Ajouter (%s) | Supprimer (%s) | Changement (%s) [CR]Fichiers mise à jour %s / %s" %  (add, dell, schange, sdown, total)
-                listitem = xbmcgui.ListItem(label = "vStream", label2 = sContent)
-                icon = "special://home/addons/plugin.video.vstream/resources/art/update.png"
+                sContent = 'Ajouter (%s) | Supprimer (%s) | Changement (%s) [CR]Fichiers mise à jour %s / %s' %  (add, dell, schange, sdown, total)
+                listitem = xbmcgui.ListItem(label = 'vStream', label2 = sContent)
+                icon = 'special://home/addons/plugin.video.vstream/resources/art/update.png'
                 listitem.setArt({'icon': icon, 'thumb': icon})
                 listitems.append(listitem)
 
                 #boucle commit
                 for i in result['commits']:
                     try:
-                        #text += "[B]%s[/B]: %s[CR]" % (i['commit']['author']['name'], i['commit']['message'].encode("utf-8"))
+                        #text += '[B]%s[/B]: %s[CR]' % (i['commit']['author']['name'], i['commit']['message'].encode('utf-8'))
                         icon = i['author']['avatar_url']
                         login = i['author']['login']
                         desc = i['commit']['message'].encode("utf-8")
                         listitem = xbmcgui.ListItem(label = login, label2 = desc)
                         listitem.setArt({'icon' : icon, 'thumb' : icon})
                     except:
-                        #text += "[B]%s[/B]: nop[CR]" % (i['commit']['author']['name'])
+                        #text += '[B]%s[/B]: nop[CR]' % (i['commit']['author']['name'])
                         listitem = xbmcgui.ListItem(label = 'None', label2 = 'none')
                         pass
                     listitems.append(listitem)
 
-                #sContent = "Changement (%s) | Fichiers mise à jour %s / %s [CR]" %  (schange, sdown, total)
-                #sContent += "%s" %  (text.encode("utf-8"))
-                #sContent += "%s" %  (site)
+                #sContent = 'Changement (%s) | Fichiers mise à jour %s / %s [CR]' %  (schange, sdown, total)
+                #sContent += '%s' %  (text.encode('utf-8'))
+                #sContent += '%s' %  (site)
 
                 addons.setSetting('service_time', str(datetime.datetime.now()))
                 addons.setSetting('service_version', str(service_futur))
@@ -306,16 +243,16 @@ class cAbout:
 
     def TextBoxes(self, anounce):
         # activate the text viewer window
-        xbmc.executebuiltin( "ActivateWindow(%d)" % ( 10147, ) )
+        xbmc.executebuiltin('ActivateWindow(%d)' % (10147))
         # get window
         win = window(10147)
         #win.show()
         # give window time to initialize
         xbmc.sleep(100)
         # set heading
-        win.getControl(1).setLabel("vStream mise à jour")
+        win.getControl(1).setLabel('vStream mise à jour')
         win.getControl(5).setText(anounce)
-        while xbmc.getCondVisibility("Window.IsActive(10147)"):
+        while xbmc.getCondVisibility('Window.IsActive(10147)'):
             xbmc.sleep(100)
         ret = dialog().VSok('Mise à jour terminée')
         return
@@ -325,7 +262,7 @@ class cAbout:
         class XMLDialog(xbmcgui.WindowXMLDialog):
 
             def __init__(self, *args, **kwargs):
-                xbmcgui.WindowXMLDialog.__init__( self )
+                xbmcgui.WindowXMLDialog.__init__(self)
                 pass
 
             def onInit(self):
@@ -345,11 +282,11 @@ class cAbout:
             def onFocus(self, controlId):
                 self.controlId = controlId
 
-            def _close_dialog( self ):
+            def _close_dialog(self):
                 self.close()
 
-        path = "special://home/addons/plugin.video.vstream"
-        wd = XMLDialog('DialogSelect.xml', path, "Default")
+        path = 'special://home/addons/plugin.video.vstream'
+        wd = XMLDialog('DialogSelect.xml', path, 'Default')
         wd.doModal()
         del wd
         return
