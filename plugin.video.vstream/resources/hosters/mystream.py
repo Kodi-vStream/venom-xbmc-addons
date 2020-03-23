e2c2cb8830694ff22a543cf35d8ea3fc3eb885f2
razorex <individuel@free.fr>
2020-03-23 22:04:08 +0100

    Update mystream.py

--- plugin.video.vstream/resources/hosters/mystream.py
+++ plugin.video.vstream/resources/hosters/mystream.py
@@ -2,12 +2,8 @@
 # https://github.com/Kodi-vStream/venom-xbmc-addons
 
 from resources.lib.handler.requestHandler import cRequestHandler
-from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
-#from resources.lib.aadecode import AADecoder
-#from resources.lib.jjdecode import JJDecoder
 import re
-#import base64
 UA = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'
 
 class cHoster(iHoster):
@@ -47,24 +43,18 @@ class cHoster(iHoster):
         return self.__getMediaLinkForGuest()
 
     def __getMediaLinkForGuest(self):
-        
+
         url = self.__sUrl
-        
-        #url = self.__sUrl.replace('embed.mystream.to','mstream.cloud')
-        #url = 'https://mstream.cloud/gfa35ebu1nt1'
 
         oRequest = cRequestHandler(url)
         oRequest.addHeaderEntry('User-Agent', UA)
         oRequest.addHeaderEntry('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
         sHtmlContent = oRequest.request()
 
-
-        oParser = cParser()
-        
         api_call = False
 
         sPattern =  '([$]=.+?\(\)\)\(\);)'
-        aResult = re.findall(sPattern,sHtmlContent,re.DOTALL)
+        aResult = re.findall(sPattern, sHtmlContent, re.DOTALL)
         if aResult:
 
             for i in aResult:
@@ -75,25 +65,23 @@ class cHoster(iHoster):
                     if r:
                         api_call = r.group(1)
 
-
-
         if (api_call):
             return True, api_call + '|User-Agent=' + UA + '&Referer=' + self.__sUrl + '&Origin=https://embed.mystream.to'
-            
+
         return False, False
-        
+
 def temp_decode(data):
     startpos = data.find('"\\""+') + 5
     endpos = data.find('"\\"")())()')
 
     first_group = data[startpos:endpos]
 
-    l = re.search("(\(!\[\]\+\"\"\)\[.+?\]\+)",first_group,re.DOTALL)
+    l = re.search("(\(!\[\]\+\"\"\)\[.+?\]\+)", first_group, re.DOTALL)
     if l:
-        first_group = first_group.replace(l.group(1), 'l').replace('$.__+','t').replace('$._+','u').replace('$._$+','o')
+        first_group = first_group.replace(l.group(1), 'l').replace('$.__+', 't').replace('$._+', 'u').replace('$._$+', 'o')
 
         tmplist = []
-        js = re.search('(\$={.+?});',data,re.DOTALL)
+        js = re.search('(\$={.+?});', data, re.DOTALL)
         if js:
             js_group = js.group(1)[3:][:-1]
 
@@ -102,23 +90,23 @@ def temp_decode(data):
             i = -1
 
             for x in second_group:
-                a,b = x.split(':')
+                a, b = x.split(':')
 
                 if b == '++$':
                     i += 1
-                    tmplist.append(("{}{}{}".format('$.',a,'+'),i))
+                    tmplist.append(("{}{}{}".format('$.', a, '+'), i))
 
                 elif b == '(![]+"")[$]':
-                    tmplist.append(("{}{}{}".format('$.',a,'+'),'false'[i]))
+                    tmplist.append(("{}{}{}".format('$.', a, '+'), 'false'[i]))
 
                 elif b == '({}+"")[$]':
-                    tmplist.append(("{}{}{}".format('$.',a,'+'),'[object Object]'[i]))
+                    tmplist.append(("{}{}{}".format('$.', a, '+'), '[object Object]'[i]))
 
                 elif b == '($[$]+"")[$]':
                     tmplist.append(("{}{}{}".format('$.',a,'+'),'undefined'[i]))
 
                 elif b == '(!""+"")[$]':
-                    tmplist.append(("{}{}{}".format('$.',a,'+'),'true'[i]))
+                    tmplist.append(("{}{}{}".format('$.', a, '+'), 'true'[i]))
 
 
             tmplist = sorted(tmplist, key=lambda x: x[1])
@@ -126,12 +114,12 @@ def temp_decode(data):
             for x in tmplist:
                 first_group = first_group.replace(x[0], str(x[1]))
 
-            first_group = first_group.replace(r'\\"' , '\\').replace("\"\\\\\\\\\"", "\\\\").replace('\\"','\\').replace('"','').replace("+", "")
+            first_group = first_group.replace(r'\\"' , '\\').replace("\"\\\\\\\\\"", "\\\\").replace('\\"', '\\').replace('"', '').replace("+", "")
 
 
 
     try:
-        final_data = unicode(first_group, encoding='unicode-escape')
+        final_data = unicode(first_group, encoding = 'unicode-escape')
         return final_data
     except:
         return False
