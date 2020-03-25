da49e3218a112f03340993ea47cbe5b42b16c57a
TmpName <gihubtmp@yopmail.com>
2020-03-25 14:31:25 +0000

    Create sendvid.py

---
+++ plugin.video.vstream/resources/hosters/sendvid.py
@@ -0,0 +1,74 @@
+#-*- coding: utf-8 -*-
+# https://github.com/Kodi-vStream/venom-xbmc-addons
+#french-stream /18117-la-frontire-verte-saison-1.html
+#liens FVS io
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.parser import cParser
+from resources.hosters.hoster import iHoster
+from resources.lib.comaddon import dialog, VSlog
+import urllib
+import json
+
+
+UA = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'
+
+class cHoster(iHoster):
+
+    def __init__(self):
+        self.__sDisplayName = 'SendVid'
+        self.__sFileName = self.__sDisplayName
+
+    def getDisplayName(self):
+        return  self.__sDisplayName
+
+    def setDisplayName(self, sDisplayName):
+        self.__sDisplayName = sDisplayName + ' [COLOR skyblue]' + self.__sDisplayName + '[/COLOR]'
+
+    def setFileName(self, sFileName):
+        self.__sFileName = sFileName
+
+    def getFileName(self):
+        return self.__sFileName
+
+    def getPluginIdentifier(self):
+        return 'sendvid'
+
+    def isDownloadable(self):
+        return True
+
+    def setUrl(self, sUrl):
+        self.__sUrl = str(sUrl)
+
+    def checkUrl(self, sUrl):
+        return True
+
+    def getUrl(self):
+        return self.__sUrl
+
+    def getMediaLink(self):
+        return self.__getMediaLinkForGuest()
+
+    def __getMediaLinkForGuest(self):
+        
+        api_call = ''
+
+        oRequest = cRequestHandler(self.__sUrl)
+        sHtmlContent = oRequest.request()
+        
+        #fh = open('c:\\test.txt', "w")
+        #fh.write(sHtmlContent)
+        #fh.close()
+        
+        oParser = cParser()
+        sPattern =  'video_source *= *"([^"]+)"'
+        aResult = oParser.parse(sHtmlContent, sPattern)
+        
+        VSlog(aResult)
+        
+        if (aResult[0] == True):
+            api_call = aResult[1][0]
+            
+        if (api_call):
+            return True, api_call
+
+        return False, False
