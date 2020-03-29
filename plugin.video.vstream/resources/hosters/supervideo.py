7a6997f93b433fe0d92bb2e22079b0425887fa5e
razorex <individuel@free.fr>
2020-03-29 20:26:39 +0200

    Update supervideo.py
    Nettoyage et correction des indentations

--- plugin.video.vstream/resources/hosters/supervideo.py
+++ plugin.video.vstream/resources/hosters/supervideo.py
@@ -1,34 +1,18 @@
 #-*- coding: utf-8 -*-
 #Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
-#Votre pseudo
+#
 from resources.lib.handler.requestHandler import cRequestHandler #requete url
 from resources.lib.parser import cParser #recherche de code
 from resources.hosters.hoster import iHoster
 from resources.lib.comaddon import dialog
 from resources.lib.packer import cPacker
-#from resources.lib.util import cUtil #Autres fonctions utiles
-#et comaddon, exemple
-#from resources.lib.comaddon import addon, dialog, VSlog, xbmcgui, xbmc
-
-#AAdecoder
-#from resources.lib.aadecode import AADecoder
-#Cpaker decoder
-#from resources.lib.packer import cPacker
-#Jdecoder
-#from resources.lib.jjdecode import JJDecoder
-#Si premium
-#from resources.lib.handler.premiumHandler import cPremiumHandler
-
-#Ne garder que celles qui vous servent
-import re, urllib2, urllib
 
 class cHoster(iHoster):
 
     def __init__(self):
-        #Nom a afficher dans Vstream
         self.__sDisplayName = 'SuperVideo'
         self.__sFileName = self.__sDisplayName
-        
+
 
     def getDisplayName(self):
         return  self.__sDisplayName
@@ -67,44 +51,37 @@ class cHoster(iHoster):
         return True
 
     def getUrl(self, media_id):
-        return 
+        return
 
     def getMediaLink(self):
         return self.__getMediaLinkForGuest()
 
     def __getMediaLinkForGuest(self):
-        
-       api_call = False 
-        
-       
-       oRequest = cRequestHandler(self.__sUrl)
-       sHtmlContent = oRequest.request()
 
-       oParser = cParser() 
+        api_call = False
 
-       
-       sPattern = "(\s*eval\s*\(\s*function(?:.|\s)+?)<\/script>"
-       aResult = oParser.parse(sHtmlContent, sPattern)
+        oRequest = cRequestHandler(self.__sUrl)
+        sHtmlContent = oRequest.request()
+        sPattern = "(\s*eval\s*\(\s*function(?:.|\s)+?)<\/script>"
+        oParser = cParser()
+        aResult = oParser.parse(sHtmlContent, sPattern)
 
-       if (aResult[0] == True):
+        if (aResult[0] == True):
             sHtmlContent = cPacker().unpack(aResult[1][0])
-
-
             sPattern = 'file:"([^<>"]+?\.mp4).+?label:"([^"]+)"'
             aResult = oParser.parse(sHtmlContent, sPattern)
-       if (aResult[0] == True):     
-           url=[]
-           qua=[]
-            
 
-           for i in aResult[1]:
-               url.append(str(i[0]))
-               qua.append(str(i[1]))
+        if (aResult[0] == True):
+            url=[]
+            qua=[]
+            for i in aResult[1]:
+                url.append(str(i[0]))
+                qua.append(str(i[1]))
 
             #Affichage du tableau
-           api_call = dialog().VSselectqual(qua, url)
+            api_call = dialog().VSselectqual(qua, url)
 
-       if (api_call):
-           return True, api_call
+        if (api_call):
+            return True, api_call
 
-       return False, False
+        return False, False
