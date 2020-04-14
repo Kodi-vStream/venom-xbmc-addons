362138ce355f7547497c07e187f22623c59f1ac7
razorex <individuel@free.fr>
2020-04-14 22:26:37 +0200

    Update setup.py
    indent de 2 au lieu de 4

--- plugin.video.vstream/setup.py
+++ plugin.video.vstream/setup.py
@@ -2,22 +2,22 @@
 from setuptools import setup, find_packages
 
 setup (
-  name = 'vStream',
-  version = '0.1',
-  packages = find_packages(),
+    name = 'vStream',
+    version = '0.1',
+    packages = find_packages(),
 
-  # Declare your packages' dependencies here, for eg:
-  install_requires = ['foo>=3'],
+    # Declare your packages' dependencies here, for eg:
+    install_requires = ['foo>=3'],
 
-  # Fill in these to make your Egg ready for upload to
-  # PyPI
-  author = 'Venom',
-  author_email = '',
+    # Fill in these to make your Egg ready for upload to
+    # PyPI
+    author = 'Venom',
+    author_email = '',
 
-  summary = 'Just another Python package for the cheese shop',
-  url = '',
-  license = '',
-  long_description= 'Long description of the package',
+    summary = 'Just another Python package for the cheese shop',
+    url = '',
+    license = '',
+    long_description= 'Long description of the package',
 
-  # could also include long_description, download_url, classifiers, etc.
+    # could also include long_description, download_url, classifiers, etc.
 )
