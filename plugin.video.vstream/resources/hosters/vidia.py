0634484726d65282cd186e1aee40d912fd21df91
razorex <individuel@free.fr>
2020-04-25 14:34:15 +0200

    Update vidia.py
    Nettoyage du code.

--- plugin.video.vstream/resources/hosters/vidia.py
+++ plugin.video.vstream/resources/hosters/vidia.py
@@ -1,32 +1,15 @@
-#-*- coding: utf-8 -*-
-#Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
-#Votre pseudo
-from resources.lib.handler.requestHandler import cRequestHandler #requete url
-from resources.lib.parser import cParser #recherche de code
+# -*- coding: utf-8 -*-
+# vStream https://github.com/Kodi-vStream/venom-xbmc-addons
+
+from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
 from resources.lib.packer import cPacker
-UA = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0'
 
-#from resources.lib.util import cUtil #Autres fonctions utiles
-#et comaddon, exemple
-#from resources.lib.comaddon import addon, dialog, VSlog, xbmcgui, xbmc
-
-#AAdecoder
-#from resources.lib.aadecode import AADecoder
-#Cpaker decoder
-#from resources.lib.packer import cPacker
-#Jdecoder
-#from resources.lib.jjdecode import JJDecoder
-#Si premium
-#from resources.lib.handler.premiumHandler import cPremiumHandler
-
-#Ne garder que celles qui vous servent
-import re, urllib2, urllib
 
 class cHoster(iHoster):
 
     def __init__(self):
-        #Nom a afficher dans Vstream
         self.__sDisplayName = 'Vidia'
         self.__sFileName = self.__sDisplayName
         self.__sHD = ''
@@ -46,7 +29,6 @@ class cHoster(iHoster):
         return self.__sFileName
 
     def getPluginIdentifier(self):
-        #Nom du fichier exact sans .py
         return 'vidia'
 
     def isDownloadable(self):
@@ -85,18 +67,14 @@ class cHoster(iHoster):
         sPattern = "(\s*eval\s*\(\s*function(?:.|\s)+?)<\/script>"
         oParser = cParser()
         aResult = oParser.parse(sHtmlContent, sPattern)
-        
 
         if (aResult[0] == True):
             sHtmlContent = cPacker().unpack(aResult[1][0])
             sPattern = '{file:"([^"]+)"}'
             aResult = oParser.parse(sHtmlContent, sPattern)
-        
+
         if (aResult[0] == True):
-             
-            api_call = aResult[1][0].replace(',','').replace('master.m3u8','index-v1-a1.m3u8')
-            
-            
+            api_call = aResult[1][0].replace(',', '').replace('master.m3u8', 'index-v1-a1.m3u8')
 
         if (api_call):
             return True, api_call
