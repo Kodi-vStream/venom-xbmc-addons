650b8256215347b1bbb0def9eff14c9e93c337d4
razorex <individuel@free.fr>
2020-05-19 20:01:21 +0200

    Update inputParameterHandler.py
    Python 3

--- plugin.video.vstream/resources/lib/handler/inputParameterHandler.py
+++ plugin.video.vstream/resources/lib/handler/inputParameterHandler.py
@@ -1,25 +1,30 @@
-import urllib
+# -*- coding: utf-8 -*-
+# vStream https://github.com/Kodi-vStream/venom-xbmc-addons
+
 import sys
+
+from resources.lib.util import UnquotePlus, Unquote
+
 
 class cInputParameterHandler:
 
-    def __init__(self):          
+    def __init__(self):
         aParams = dict()
-        if len(sys.argv)>=2 and len(sys.argv[2])>0:
+        if len(sys.argv) >= 2 and len(sys.argv[2])>0:
             aParams = dict(part.split('=') for part in sys.argv[2][1:].split('&'))
-          
+
         self.__aParams = aParams
 
     def getAllParameter(self):
-            return self.__aParams
+        return self.__aParams
 
     def getValue(self, sParamName):
         if self.exist(sParamName):
             sParamValue = self.__aParams[sParamName]
             if not sParamValue.startswith('http'):
-                return urllib.unquote_plus(sParamValue)
+                return UnquotePlus(sParamValue)
             else:
-                return urllib.unquote(sParamValue)
+                return Unquote(sParamValue)
         return False
 
     def exist(self, sParamName):
