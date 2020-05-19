2f78c5a34af64e97c08619140749ef7f76adde71
razorex <individuel@free.fr>
2020-05-19 14:16:50 +0200

    Update hosterHandler.py
    Python 3

--- plugin.video.vstream/resources/lib/handler/hosterHandler.py
+++ plugin.video.vstream/resources/lib/handler/hosterHandler.py
@@ -1,8 +1,9 @@
-#-*- coding: utf-8 -*-
-#Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
+# -*- coding: utf-8 -*-
+# vStream https://github.com/Kodi-vStream/venom-xbmc-addons
 from resources.lib.comaddon import VSlog
 from resources.lib.parser import cParser
 from resources.lib.handler.requestHandler import cRequestHandler
+
 
 class cHosterHandler:
 
@@ -10,16 +11,15 @@ class cHosterHandler:
         sUrl = oHoster.getUrl()
         VSlog("hosterhandler " + sUrl)
         if (oHoster.checkUrl(sUrl)):
-            oRequest = cRequestHandler(sUrl)            
+            oRequest = cRequestHandler(sUrl)
             sContent = oRequest.request()
-            
-            aMediaLink = cParser().parse(sContent, oHoster.getPattern())           
-            if (aMediaLink[0] == True):               
+
+            aMediaLink = cParser().parse(sContent, oHoster.getPattern())
+            if (aMediaLink[0] == True):
                 return True, aMediaLink[1][0]
         return False, ''
 
     def getHoster(self, sHosterFileName):
-        exec "from resources.hosters." + sHosterFileName + " import cHoster"
+        exec("from resources.hosters." + sHosterFileName + " import cHoster")
 
         return cHoster()
-    
