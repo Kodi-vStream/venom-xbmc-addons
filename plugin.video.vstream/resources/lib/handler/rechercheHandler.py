54d5cc6df9f3b71311d49438b283459a38e904cd
razorex <individuel@free.fr>
2020-05-19 20:27:19 +0200

    Update rechercheHandler.py

--- plugin.video.vstream/resources/lib/handler/rechercheHandler.py
+++ plugin.video.vstream/resources/lib/handler/rechercheHandler.py
@@ -1,13 +1,13 @@
-#-*- coding: utf-8 -*-
-#from resources.lib.config import cConfig
+# -*- coding: utf-8 -*-
+# vStream https://github.com/Kodi-vStream/venom-xbmc-addons
 from resources.lib.comaddon import addon, VSlog
 from resources.lib.db import cDb
 
 import sys
 import xbmcvfs
 
-class cRechercheHandler:
 
+class cRechercheHandler:
     Count = 0
 
     def __init__(self):
@@ -17,7 +17,7 @@ class cRechercheHandler:
 
     def getPluginHandle(self):
         try:
-            return int( sys.argv[ 1 ] )
+            return int(sys.argv[1])
         except:
             return 0
 
@@ -45,7 +45,6 @@ class cRechercheHandler:
     def getCat(self):
         return self.__sCat
 
-
     def setDisp(self, sDisp):
         if not sDisp:
             return False
@@ -57,32 +56,21 @@ class cRechercheHandler:
 
     def __getFileNamesFromFolder(self, sFolder):
         aNameList = []
-        #items = os.listdir(sFolder)
-        #items = os.listdir(unicode(sFolder, 'utf-8'))
         folder, items = xbmcvfs.listdir(sFolder)
         items.sort()
 
         for sItemName in items:
-            #sFilePath = os.path.join(sFolder, sItemName)
-            #sFilePath = os.path.join(unicode(sFolder, 'utf-8'), sItemName)
             sFilePath = "/".join([sFolder, sItemName])
-            # xbox hack
             sFilePath = sFilePath.replace('\\', '/')
 
             if (xbmcvfs.exists(sFilePath) == True):
-                #if (str(sFilePath.lower()).endswith('py')):
                 if (sFilePath.lower().endswith('py')):
                     sItemName = sItemName.replace('.py', '')
                     aNameList.append(sItemName)
         return aNameList
 
-
     def importPlugin(self, sName, sCat):
         pluginData = {}
-
-        #multicherche
-        # if sLabel == 'search5':
-        #     bPlugin = 'true'
 
         if sCat == '1':
             sSearch = 'URL_SEARCH_MOVIES'
@@ -94,32 +82,14 @@ class cRechercheHandler:
             sSearch = 'URL_SEARCH'
 
         try:
-
             plugin = __import__('resources.sites.%s' % sName, fromlist=[sName])
-
             pluginData['identifier'] = plugin.SITE_IDENTIFIER
             pluginData['name'] = plugin.SITE_NAME
             pluginData['search'] = getattr(plugin, sSearch)
             return pluginData
 
-        except Exception, e:
-            #VSlog("cant import plugin: " + str(sName))
+        except Exception:
             return False
-
-
-    # def getRootFolder(self):
-    #     sRootFolder = cConfig().getAddonPath()
-    #     cConfig().log("Root Folder: " + sRootFolder)
-    #     return sRootFolder
-
-    # def getRootArt(self):
-    #     oConfig = cConfig()
-
-    #     sFolder =  self.getRootFolder()
-    #     sFolder = os.path.join(sFolder, 'resources/art/').decode("utf-8")
-
-    #     sFolder = sFolder.replace('\\', '/')
-    #     return sFolder
 
     def getAvailablePlugins(self):
 
@@ -131,7 +101,7 @@ class cRechercheHandler:
         if not sCat:
             return False
 
-        #historique
+        # historique
         try:
             if (addons.getSetting("history-view") == 'true'):
                 meta = {}
@@ -140,12 +110,8 @@ class cRechercheHandler:
                 cDb().insert_history(meta)
         except: pass
 
-        #sFolder =  self.getRootFolder()
-        #sFolder = os.path.join(sFolder, 'resources/sites')
-        
         sFolder = "special://home/addons/plugin.video.vstream/resources/sites"
 
-        # xbox hack
         sFolder = sFolder.replace('\\', '/')
         VSlog("Sites Folder: " + sFolder)
 
