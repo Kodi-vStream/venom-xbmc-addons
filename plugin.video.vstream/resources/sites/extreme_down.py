2f9407e122931ba4ac21378eb60db28e17015dac
yodidon <39896943+yodidon@users.noreply.github.com>
2020-05-24 23:27:58 +0200

    Update extreme_down.py
    problème avec la recherche

--- plugin.video.vstream/resources/sites/extreme_down.py
+++ plugin.video.vstream/resources/sites/extreme_down.py
@@ -21,9 +21,9 @@ SITE_IDENTIFIER = 'extreme_down'
 SITE_NAME = '[COLOR violet]Extreme Down[/COLOR]'
 SITE_DESC = 'films en streaming, streaming hd, streaming 720p, Films/séries, récent'
 
-URL_MAIN = 'https://www.extreme-down.ninja/'
+URL_MAIN = 'https://ww1.extreme-down.ninja/'
 
-URL_SEARCH = (URL_MAIN + 'home.html', 'showMovies')
+URL_SEARCH = (URL_MAIN + 'index.php?', 'showMovies')
 URL_SEARCH_MOVIES = (URL_SEARCH[0], 'showMovies')
 URL_SEARCH_SERIES = (URL_SEARCH[0], 'showMovies')
 FUNCTION_SEARCH = 'showMovies'
