81d904399a2ff2b2839add482d5751bf7d633c1c
razorex <individuel@free.fr>
2020-05-27 18:43:46 +0200

    Update uplea.py
    PY3

--- plugin.video.vstream/resources/hosters/uplea.py
+++ plugin.video.vstream/resources/hosters/uplea.py
@@ -1,11 +1,17 @@
-#-*- coding: utf-8 -*-
-#Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
-#from resources.lib.handler.requestHandler import cRequestHandler
+# -*- coding: utf-8 -*-
+# Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
+
+try:  # Python 2
+    import urllib2
+
+except ImportError:  # Python 3
+    import urllib.request as urllib2
+
+import xbmc
+
 from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
-from resources.lib.comaddon import dialog, xbmc
-
-import urllib2
+from resources.lib.comaddon import dialog
 
 
 class cHoster(iHoster):
@@ -14,7 +20,7 @@ class cHoster(iHoster):
         self.__sFileName = self.__sDisplayName
 
     def getDisplayName(self):
-        return  self.__sDisplayName
+        return self.__sDisplayName
 
     def setDisplayName(self, sDisplayName):
         self.__sDisplayName = sDisplayName + ' [COLOR violet]' + self.__sDisplayName + '[/COLOR]'
@@ -64,7 +70,7 @@ class cHoster(iHoster):
         import sys
         if 'site=cDownload&function' not in sys.argv[2]:
             oDialog = dialog().VSok("ATTENTION, Pas de streaming sans premium\nPour voir le film passer par l'option 'Télécharger et Lire' du menu contextuel.")
-            return False,False
+            return False, False
         return self.__getMediaLinkForGuest()
 
     def __getMediaLinkForGuest(self):
@@ -72,12 +78,12 @@ class cHoster(iHoster):
         # http:///dl/12345XXYEEEEREERERE
 
         UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0'
-        headers = {'User-Agent': UA ,
+        headers = {'User-Agent': UA,
                    'Host': 'uplea.com',
-                   #'Referer': self.__sUrl ,
+                   # 'Referer': self.__sUrl ,
                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                    'Accept-Language': 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3'
-                   #'Content-Type': 'application/x-www-form-urlencoded'
+                   # 'Content-Type': 'application/x-www-form-urlencoded'
                    }
 
         req = urllib2.Request(self.__sUrl, None, headers)
@@ -90,7 +96,7 @@ class cHoster(iHoster):
 
         # get step
         urlstep = ''
-        sPattern =  '<a href="(\/step\/[^<>"]+)">'
+        sPattern = '<a href="(\/step\/[^<>"]+)">'
         aResult = oParser.parse(sHtmlContent, sPattern)
         if (aResult[0] == True):
             urlstep = aResult[1][0]
@@ -121,12 +127,12 @@ class cHoster(iHoster):
 
         # waiting time
         waitingtime = 20
-        sPattern =  "ulCounter\({'timer':([0-9]+)}\);"
+        sPattern = "ulCounter\({'timer':([0-9]+)}\);"
         aResult = oParser.parse(sHtmlContent, sPattern)
         if (aResult[0] == True):
             waitingtime = int(aResult[1][0]) + 2
 
-        sPattern =  '<a class="button-download" href="([^<>"]+?)">'
+        sPattern = '<a class="button-download" href="([^<>"]+?)">'
         aResult = oParser.parse(sHtmlContent, sPattern)
 
         if (aResult[0] == True):
