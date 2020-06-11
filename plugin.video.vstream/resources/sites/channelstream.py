132c11840f8a8f36f4e2eea6e3dd3024093bfef3
razorex <individuel@free.fr>
2020-06-11 12:05:59 +0200

    Update channelstream.py
    Mise en forme.

--- plugin.video.vstream/resources/sites/channelstream.py
+++ plugin.video.vstream/resources/sites/channelstream.py
@@ -1,23 +1,23 @@
-#-*- coding: utf-8 -*-
-#Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
+# -*- coding: utf-8 -*-
+# vStream https://github.com/Kodi-vStream/venom-xbmc-addons
 # Arias800
-from resources.lib.gui.hoster import cHosterGui
+import base64
+import re
+import requests
+
+from resources.lib.comaddon import progress
 from resources.lib.gui.gui import cGui
 from resources.lib.gui.guiElement import cGuiElement
 from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
 from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.packer import cPacker
 from resources.lib.parser import cParser
-from resources.lib.comaddon import progress, VSlog
 from resources.sites.freebox import play__
 from resources.lib.util import Quote
 
-from resources.lib.packer import cPacker
-
-import re, base64, requests
-
 SITE_IDENTIFIER = 'channelstream'
-SITE_NAME = 'Channelstream'
+SITE_NAME = 'Channel Stream'
 SITE_DESC = 'iptv'
 
 URL_MAIN = 'https://channelstream.me'
@@ -25,6 +25,7 @@ URL_MAIN = 'https://channelstream.me'
 TV_FRENCH = (URL_MAIN + "/chaine-tv.php", 'showMovies')
 
 UA = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0'
+
 
 def load():
     oGui = cGui()
@@ -35,9 +36,10 @@ def load():
 
     oGui.setEndOfDirectory()
 
+
 def showMovies():
     oGui = cGui()
-    
+
     oInputParameterHandler = cInputParameterHandler()
     sUrl = oInputParameterHandler.getValue('siteUrl')
 
@@ -72,12 +74,13 @@ def showMovies():
                 oOutputParameterHandler.addParameter('siteUrl', sUrl2)
                 oOutputParameterHandler.addParameter('sMovieTitle', sTitle)
                 oOutputParameterHandler.addParameter('sThumb', sThumb)
-                
+
                 oGui.addDir(SITE_IDENTIFIER, 'showHoster', sTitle, sThumb, oOutputParameterHandler)
 
         progress_.VSclose(progress_)
 
     oGui.setEndOfDirectory()
+
 
 def showHoster():
     oGui = cGui()
@@ -90,7 +93,7 @@ def showHoster():
     oRequestHandler = cRequestHandler(sUrl)
     sHtmlContent = oRequestHandler.request()
 
-    #Double Iframe a passer.
+    # Double Iframe a passer.
     oParser = cParser()
     sPattern = '<iframe.+?src="([^"]+)".+?</iframe>'
     iframeURL = oParser.parse(sHtmlContent, sPattern)[1][0]
@@ -117,45 +120,43 @@ def showHoster():
         strs = cPacker().unpack(str2)
 
     # Code source https://github.com/yannisKalt/Kodi_Remote_Manager/blob/master/addons/plugin.video.PLsportowo/resources/lib/mydecode.py
-    mainurlliczbacdurl=re.findall('url:\s*window\.atob\((.+?)\).slice\((.+?)\)\s*\+\s*window.atob\((.+?)\)',strs)[0]
-    mainurl=mainurlliczbacdurl[0]
-    liczba=mainurlliczbacdurl[1]
-    cdurl=mainurlliczbacdurl[2]
-    mainurlpattern=(mainurl+"""\=['"](.+?)['"]""")
-    liczbapattern=(liczba+'\=(\d+)')
-    cdurlpattern=(cdurl+"""\=['"](.+?)['"]""")
-    
-    murl=re.findall(mainurlpattern,strs)[0]
-    licz=re.findall(liczbapattern,strs)[0]
-    curl=re.findall(cdurlpattern,strs)[0]
+    mainurlliczbacdurl = re.findall('url:\s*window\.atob\((.+?)\).slice\((.+?)\)\s*\+\s*window.atob\((.+?)\)', strs)[0]
+    mainurl = mainurlliczbacdurl[0]
+    liczba = mainurlliczbacdurl[1]
+    cdurl = mainurlliczbacdurl[2]
+    mainurlpattern = (mainurl + """\=['"](.+?)['"]""")
+    liczbapattern = (liczba + '\=(\d+)')
+    cdurlpattern = (cdurl + """\=['"](.+?)['"]""")
 
-    d1=(base64.b64decode(murl))[int(licz):]
+    murl = re.findall(mainurlpattern, strs)[0]
+    licz = re.findall(liczbapattern, strs)[0]
+    curl = re.findall(cdurlpattern, strs)[0]
 
-    d2=base64.b64decode(curl)
-    basurl='https://telerium.tv'
-    
-    m3url=re.findall('{if\(esMobiliar\){(.+?)=.+?};function',strs)[0]
-    m3url=base64.b64decode(re.findall(m3url+"""=['"](.+?)['"]""",strs)[0])
+    d1 = (base64.b64decode(murl))[int(licz):]
+    d2 = base64.b64decode(curl)
+    basurl = 'https://telerium.tv'
 
-    speechx=re.findall("""}else\{(.+?)\=dameVuelta\((.+?)\[(.+?)]\);""",strs)[0][2]
-    
-    sppech = re.findall(speechx+'=(.+?)([-+])(.+?);',strs)[0]
-    reg1 = sppech[0]+'=(.+?)([-+])(.+?);'
-    reg2 = sppech[2]+'=(.+?)([-+])(.+?);'
-    
-    ob1 =re.compile(reg1).findall(strs)
-    ob2 =re.compile(reg2).findall(strs)
-    
-    ob11 = re.compile('var '+ob1[0][0]+'=(.+?);').findall(strs)
-    ob12 = re.compile('var '+ob1[0][2]+'=(.+?);').findall(strs)
-    
-    ob21 = re.compile('var '+ob2[0][0]+'=(.+?);').findall(strs)
-    ob22 = re.compile('var '+ob2[0][2]+'=(.+?);').findall(strs)
-    
-    
-    obliczob1 = eval('%s%s%s'%(ob11[0],ob1[0][1],ob12[0]))
-    obliczob2 = eval('%s%s%s'%(ob21[0],ob2[0][1],ob22[0]))
-    spech = eval('%s%s%s'%(obliczob1,sppech[1],obliczob2))
+    m3url = re.findall('{if\(esMobiliar\){(.+?)=.+?};function', strs)[0]
+    m3url = base64.b64decode(re.findall(m3url + """=['"](.+?)['"]""", strs)[0])
+
+    speechx = re.findall("""}else\{(.+?)\=dameVuelta\((.+?)\[(.+?)]\);""", strs)[0][2]
+
+    sppech = re.findall(speechx + '=(.+?)([-+])(.+?);', strs)[0]
+    reg1 = sppech[0] + '=(.+?)([-+])(.+?);'
+    reg2 = sppech[2] + '=(.+?)([-+])(.+?);'
+
+    ob1 = re.compile(reg1).findall(strs)
+    ob2 = re.compile(reg2).findall(strs)
+
+    ob11 = re.compile('var ' + ob1[0][0] + '=(.+?);').findall(strs)
+    ob12 = re.compile('var ' + ob1[0][2] + '=(.+?);').findall(strs)
+
+    ob21 = re.compile('var ' + ob2[0][0] + '=(.+?);').findall(strs)
+    ob22 = re.compile('var ' + ob2[0][2] + '=(.+?);').findall(strs)
+
+    obliczob1 = eval('%s%s%s' % (ob11[0], ob1[0][1], ob12[0]))
+    obliczob2 = eval('%s%s%s' % (ob21[0], ob2[0][1], ob22[0]))
+    spech = eval('%s%s%s' % (obliczob1, sppech[1], obliczob2))
 
     try:
         d1 = d1.decode('utf-8')
@@ -163,7 +164,7 @@ def showHoster():
     except:
         pass
 
-    nxturl=basurl+d1+d2
+    nxturl = basurl + d1 + d2
 
     realtoken = getRealToken(nxturl, iframeURL1, spech)
 
@@ -172,16 +173,16 @@ def showHoster():
     except:
         pass
 
-    sHosterUrl = 'https:'+m3url+realtoken
+    sHosterUrl = 'https:' + m3url + realtoken
 
-    sHosterUrl+='|User-Agent='+UA+'&Referer='+Quote(iframeURL1)+'&Sec-Fetch-Mode=cors&Origin=https://telerium.tv'
+    sHosterUrl += '|User-Agent=' + UA + '&Referer=' + Quote(iframeURL1) + '&Sec-Fetch-Mode=cors&Origin=https://telerium.tv'
 
     oOutputParameterHandler = cOutputParameterHandler()
     oOutputParameterHandler.addParameter('siteUrl', sHosterUrl)
     oOutputParameterHandler.addParameter('sMovieTitle', sTitle)
     oOutputParameterHandler.addParameter('sThumbnail', sThumb)
 
-    #oGui.addDirectTV(SITE_IDENTIFIER, 'play__', track.title, 'tv.png' , sRootArt + '/tv/' + sThumb, oOutputParameterHandler)
+    # oGui.addDirectTV(SITE_IDENTIFIER, 'play__', track.title, 'tv.png' , sRootArt + '/tv/' + sThumb, oOutputParameterHandler)
 
     oGuiElement = cGuiElement()
     oGuiElement.setSiteName(SITE_IDENTIFIER)
@@ -201,29 +202,24 @@ def showHoster():
     oGui.addFolder(oGuiElement, oOutputParameterHandler)
 
     oGui.setEndOfDirectory()
-    
-def getRealToken(link, referer,spech):
-    cookies = {
-        'ChorreameLaJa': '100',
-        'setVolumeSize': '100',
-        'NoldoTres': '100',
-    }
-    
-    cookies = {
-        'elVolumen': '100',
-    }
-    
-    headers = {
-        'Host': 'telerium.tv',
-        'User-Agent': UA,
-        'Accept': 'application/json, text/javascript, */*; q=0.01',
-        'Accept-Language': 'pl,en-US;q=0.7,en;q=0.3',
-        'X-Requested-With': 'XMLHttpRequest',
-        'Referer': referer,
-    }
-    
-    realResp = requests.get(link, headers=headers,cookies=cookies,verify=False).content#[1:-1]
-    
-    realResp=re.findall('"(.+?)"',str(realResp))[spech]  
 
-    return realResp[::-1]   
+
+def getRealToken(link, referer, spech):
+    cookies = {'ChorreameLaJa': '100',
+               'setVolumeSize': '100',
+               'NoldoTres': '100'}
+
+    cookies = {'elVolumen': '100'}
+
+    headers = {'Host': 'telerium.tv',
+               'User-Agent': UA,
+               'Accept': 'application/json, text/javascript, */*; q=0.01',
+               'Accept-Language': 'pl,en-US;q=0.7,en;q=0.3',
+               'X-Requested-With': 'XMLHttpRequest',
+               'Referer': referer}
+
+    realResp = requests.get(link, headers=headers, cookies=cookies, verify=False).content  # [1:-1]
+
+    realResp = re.findall('"(.+?)"', str(realResp))[spech]
+
+    return realResp[::-1]
