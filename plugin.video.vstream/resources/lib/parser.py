904332bae5604802feb13737a57f23677d8b3ff8
razorex <individuel@free.fr>
2020-06-19 19:42:47 +0200

    Update parser.py
    pb avec type d'url ex: xxx/0-9/page/1

--- plugin.video.vstream/resources/lib/parser.py
+++ plugin.video.vstream/resources/lib/parser.py
@@ -28,6 +28,12 @@ class cParser:
     def parse(self, sHtmlContent, sPattern, iMinFoundValue=1):
         sHtmlContent = self.__replaceSpecialCharacters(str(sHtmlContent))
         aMatches = re.compile(sPattern, re.IGNORECASE).findall(sHtmlContent)
+
+        # extrait la page html après retraitement vStream
+        # fh = open('c:\\test.txt', "w")
+        # fh.write(sHtmlContent)
+        # fh.close()
+
         if (len(aMatches) >= iMinFoundValue):
             return True, aMatches
         return False, aMatches
@@ -39,7 +45,10 @@ class cParser:
         return re.escape(sValue)
 
     def getNumberFromString(self, sValue):
-        sPattern = '\d+'
+        if '/0-9/' in sValue:
+            sPattern = '/0-9.+?(\d+)'
+        else:
+            sPattern = '\d+'
         aMatches = re.findall(sPattern, sValue)
         if (len(aMatches) > 0):
             return aMatches[0]
