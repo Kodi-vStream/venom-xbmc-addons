5a748fdcce6f2b04c31310ffa61e01294c8528ac
milkibreizh <65681507+milkibreizh@users.noreply.github.com>
2020-10-08 22:55:33 +0200

    Update libertyland_tv.py
    - changement urlmain
    - erreurs pattern showmovie série
    - erreurs causées par Url = URL_MAIN[:-1] + Url parfois plus forcément nécessaire
    - recherche : prise en compte mot composé sSearchText.replace(' ', '+')

--- plugin.video.vstream/resources/sites/libertyland_tv.py
+++ plugin.video.vstream/resources/sites/libertyland_tv.py
@@ -16,7 +16,7 @@ SITE_IDENTIFIER = 'libertyland_tv'
 SITE_NAME = 'Libertyland'
 SITE_DESC = 'Les films et séries récentes en streaming et en téléchargement'
 
-URL_MAIN = 'https://www.libertyvf.in/'
+URL_MAIN = 'https://www.libertyvf.one/'
 
 URL_SEARCH = (URL_MAIN + 'v2/recherche/', 'showMovies')
 URL_SEARCH_MOVIES = (URL_MAIN + 'v2/recherche/categorie=films&mot_search=', 'showMovies')
@@ -111,7 +111,7 @@ def showSearch():
 
     sSearchText = oGui.showKeyBoard()
     if (sSearchText != False):
-        sUrl = sUrl + sSearchText
+        sUrl = sUrl + sSearchText.replace(' ', '+')
         showMovies(sUrl)
         oGui.setEndOfDirectory()
         return
@@ -231,7 +231,7 @@ def showMovies(sSearch=''):
         oInputParameterHandler = cInputParameterHandler()
         sUrl = oInputParameterHandler.getValue('siteUrl')
         if '/series' in sUrl:
-            sPattern = '<div class="divtelecha.+?href="([^"]+)"><strong>([^<]+)<\/strong>.+?<img class="img-responsive".+?src="([^"]+)".+?serie de (\d{4})<.+?Synopsis : ([^<]+)<'
+            sPattern = '<div class="divtelecha.+?href="([^"]+)"><strong>([^<]+)<\/strong>.+?<img class="img-responsive".+?src="([^"]+)".+?serie de (\d{4})<.+?Synopsis :([^<]+)<'
         else:  # films
             sPattern = '<h2 class="heading"> *<a href="[^"]+">([^<]+)<.+?<img class="img-responsive" *src="([^"]+)" *alt.+?(?:<font color="#.+?">([^<]+)<\/font>.+?).+?>film de (\d{4})<.+?Synopsis : ([^<]+)<.+?<div class="divtelecha.+?href="([^"]+)"'
 
@@ -258,10 +258,10 @@ def showMovies(sSearch=''):
                 sThumb = URL_MAIN[:-1] + aEntry[0]
                 sTitle = aEntry[2].replace('télécharger ', '').replace('en Streaming', '')
                 sTitle = sTitle.replace(' TELECHARGEMENT GRATUIT', '').replace('gratuitement', '')
-                sUrl2 = URL_MAIN[:-1] + aEntry[1]
+                sUrl2 = aEntry[1]
             elif '/series' in sUrl:
                 sQual = ''
-                sUrl2 = URL_MAIN[:-1] + aEntry[0]
+                sUrl2 = aEntry[0]
                 sTitle = aEntry[1].replace('Regarder ', '').replace('en Streaming', '')
                 sThumb = URL_MAIN[:-1] + aEntry[2]
                 sYear = aEntry[3]
@@ -283,7 +283,7 @@ def showMovies(sSearch=''):
                     pass
 
                 sDesc = cUtil().unescape(sDesc).encode('utf-8')
-                sUrl2 = URL_MAIN[:-1] + aEntry[5]
+                sUrl2 = aEntry[5]
 
                 sQual = aEntry[2]
                 if sQual:
@@ -295,6 +295,9 @@ def showMovies(sSearch=''):
 
                     sQual = sQual.replace(u' qualit\u00E9', '').replace('et ', '/').replace('Haute', 'HD')\
                                  .replace(' ', '').replace('Bonne', 'DVD').replace('Mauvaise', 'SD').encode("utf-8")
+
+            if 'https' not in sUrl2:
+                    sUrl2 = URL_MAIN[:-1] + sUrl2
 
             sUrl2 = sUrl2.replace('telecharger', 'streaming')
 
@@ -393,8 +396,9 @@ def showSaisonsEpisodes():
             else:
                 ePisode = aEntry[1].replace(',', '')
                 sTitle = sMovieTitle + ' ' + ePisode
-                sUrl = URL_MAIN[:-1] + aEntry[2]
-
+                sUrl = aEntry[2]
+                if 'https' not in sUrl:
+                    sUrl = URL_MAIN[:-1] + sUrl
                 oOutputParameterHandler = cOutputParameterHandler()
                 oOutputParameterHandler.addParameter('siteUrl', sUrl)
                 oOutputParameterHandler.addParameter('sMovieTitle', sTitle)
