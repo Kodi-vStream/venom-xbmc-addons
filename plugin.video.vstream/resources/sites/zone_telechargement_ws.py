cf08499d42420e2ce28ab46957f3bd4c5b180403
Arias800 <azertypoil630@gmail.com>
2020-11-11 19:42:54 +0100

    Update zone_telechargement_ws.py

--- plugin.video.vstream/resources/sites/zone_telechargement_ws.py
+++ plugin.video.vstream/resources/sites/zone_telechargement_ws.py
@@ -518,11 +518,6 @@ def showMoviesLinks():
     except:
         pass
 
-    try:
-        sDesc = sDesc.encode('latin-1')
-    except:
-        pass
-
     # la qualité courante est le lien en cours ici-même
     oOutputParameterHandler = cOutputParameterHandler()
     oOutputParameterHandler.addParameter('siteUrl', sUrl)
@@ -590,11 +585,6 @@ def showSeriesLinks():
         aResult = oParser.parse(sHtmlContent, sPattern)
         if aResult[0]:
             sDesc = cUtil().removeHtmlTags(aResult[1][0][1])
-    except:
-        pass
-
-    try:
-        sDesc = sDesc.encode('latin-1')
     except:
         pass
 
