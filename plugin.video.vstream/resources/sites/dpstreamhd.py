679ca5429e9362289dc4356b8b478fd3a73d1512
razorex <individuel@free.fr>
2020-12-26 20:44:27 +0100

    Update dpstreamhd.py

--- plugin.video.vstream/resources/sites/dpstreamhd.py
+++ plugin.video.vstream/resources/sites/dpstreamhd.py
@@ -20,6 +20,7 @@ SITE_DESC = 'Films VF & VOSTFR en streaming.'
 URL_MAIN = 'https://serie.dpstreamhd.com/'
 URL_SEARCH = ('', 'showMovies')
 URL_SEARCH_MOVIES = (URL_SEARCH[0], 'showMovies')
+URL_SEARCH_SERIES = (URL_SEARCH[0], 'showMovies')
 FUNCTION_SEARCH = 'showMovies'
 
 MOVIE_MOVIE = (True, 'showMenuMovies')
