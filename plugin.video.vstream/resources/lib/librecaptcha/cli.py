3a265a1d2945fc1a73e046956b19a88c73e1a047
razorex <individuel@free.fr>
2020-12-28 21:49:04 +0100

    Update cli.py

--- plugin.video.vstream/resources/lib/librecaptcha/cli.py
+++ plugin.video.vstream/resources/lib/librecaptcha/cli.py
@@ -14,14 +14,15 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with librecaptcha.  If not, see <http://www.gnu.org/licenses/>.
-from __future__ import with_statement
 from __future__ import absolute_import
+from __future__ import with_statement
+
 from resources.lib.comaddon import dialog, VSlog
+from threading import Thread, RLock
 
 from .errors import UserError
 from .frontend import Frontend
 from .gui import cInputWindow, cInputWindowYesNo
-from threading import Thread, RLock
 
 try:
     from Queue import Queue
@@ -35,6 +36,7 @@ import xbmcvfs, re
 Objectif = ""
 DimTab = []
 STRINGS_PATH = 'special://home/userdata/addon_data/plugin.video.vstream'
+
 
 class CliSolver(object):
     def __init__(self, solver):
@@ -51,6 +53,7 @@ class CliSolver(object):
 
     def run(self):
         self.solver.run()
+
 
 class CliDynamicSolver(CliSolver):
     def __init__(self, solver):
@@ -74,7 +77,7 @@ class CliDynamicSolver(CliSolver):
         solver.finish()
 
     def show_imageNewTile(self, image):
-        oSolver = cInputWindowYesNo(captcha=image, msg="Est-ce que cette image est en lien avec le theme ?", roundnum=1)
+        oSolver = cInputWindowYesNo(captcha=image, msg="Est-ce que cette image est en lien avec le thème ?", roundnum=1)
         retArg = oSolver.get()
         return retArg
 
@@ -108,7 +111,7 @@ class CliDynamicSolver(CliSolver):
             delay and time.sleep(delay)
             with self.lock:
                 self.select_tile_sync(index)
-                
+
         myThread = Thread(target=target)
         myThread.daemon = True
         myThread.start()
@@ -125,6 +128,7 @@ class CliMultiCaptchaSolver(CliSolver):
         if indices == False:
             return False
         solver.select_indices(indices)
+
 
 BLOCKED_MSG = """\
 ERROR: Received challenge type "{}".
@@ -165,13 +169,13 @@ class Cli(Frontend):
 
         Objectif = re.findall('case '+ID+'.+?<strong>([^<]+)</strong>', content)
 
-        #Recupere le theme de maniere plus precis.
+        # Recupere le theme de maniere plus precis.
         if (int(json.dumps(meta).split(',')[3]) * int(json.dumps(meta).split(',')[4]) > 9):
             Objectif = Objectif[0].encode('utf-8').decode('unicode-escape')
         else:
-            Objectif = Objectif[1].encode('utf-8').decode('unicode-escape') 
-                      
-        DimTab = [int(json.dumps(meta).split(',')[3]),int(json.dumps(meta).split(u',')[4])]
+            Objectif = Objectif[1].encode('utf-8').decode('unicode-escape')
+
+        DimTab = [int(json.dumps(meta).split(',')[3]), int(json.dumps(meta).split(u',')[4])]
 
     def handle_challenge(self, ctype, **kwargs):
         if not self._first:
