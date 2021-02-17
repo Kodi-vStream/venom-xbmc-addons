8ec4a7276e6bb94921bd05d3f4aacc737856667f
razorex <individuel@free.fr>
2021-02-17 20:43:07 +0100

    Update serie_streaming_watch.py
    Remarque de @milkibreizh

--- plugin.video.vstream/resources/sites/serie_streaming_watch.py
+++ plugin.video.vstream/resources/sites/serie_streaming_watch.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 # vStream https://github.com/Kodi-vStream/venom-xbmc-addons
-return False
-"""
-error: HTTPSConnectionPool(host='ww1.series-en-streaming.xyz', port=443):
-Max retries exceeded with url: /category/series/?orderby=date
-(Caused by NewConnectionError('<urllib3.connection.VerifiedHTTPSConnection
-object at 0x0000000010234E80>: Failed to establish a new connection: [Errno 10061]
-Aucune connexion n\x92a pu \xeatre \xe9tablie car l\x92ordinateur cible l\x92a express\xe9ment refus\xe9e',))
-"""
 
 import re
 import xbmc
