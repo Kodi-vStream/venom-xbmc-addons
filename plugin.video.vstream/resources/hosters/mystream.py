061518cc21852f9d5ba44c56a8860044267ddc2d
Arias800 <azertypoil630@gmail.com>
2021-04-13 08:53:39 +0200

    Update mystream.py

--- plugin.video.vstream/resources/hosters/mystream.py
+++ plugin.video.vstream/resources/hosters/mystream.py
@@ -76,7 +76,7 @@ def temp_decode(data):
 
         pos = re.search(r"(\(!\[\]\+\"\"\)\[.+?\]\+)", first_group)
         if pos:
-            first_group = first_group.replace(pos.group(1), 'l').replace('$.__+', 't').replace('$._+', 'u').replace('$._$+', 'o')
+            first_group = first_group.replace('$.__+', 't').replace('$._+', 'u').replace('$._$+', 'o')
 
             tmplist = []
             js = re.search(r'(\$={.+?});', data)
@@ -111,6 +111,7 @@ def temp_decode(data):
                 first_group = first_group.replace('\\"', '\\').replace("\"\\\\\\\\\"", "\\\\") \
                                          .replace('\\"', '\\').replace('"', '').replace("+", "")
 
+                first_group = re.sub('(\(\!\[\]\)\[.+?\]+)','l',first_group) 
             try:
                 final_data = first_group.encode('ascii').decode('unicode-escape').encode('ascii').decode('unicode-escape')
                 return final_data
