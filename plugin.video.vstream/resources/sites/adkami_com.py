4f30b821e5cac4a4e9468f2efff5802bdeccf7c5
sizanic <47307622+sizanic@users.noreply.github.com>
2021-05-29 00:41:51 +0200

    adkami_com.py - Pouvoir annuler le chargement

--- plugin.video.vstream/resources/sites/adkami_com.py
+++ plugin.video.vstream/resources/sites/adkami_com.py
@@ -277,7 +277,7 @@ def showSeries(sSearch=''):
 
     if (aResult[0] == True):
         total = len(aResult[1])
-        progress_ = progress().VScreate(SITE_NAME)
+        progress_ = progress().VScreate(SITE_NAME, large=total>50)
         oOutputParameterHandler = cOutputParameterHandler()
 
         for aEntry in aResult[1]:
