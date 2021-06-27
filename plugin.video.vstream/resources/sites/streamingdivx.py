4bfa76051e270c7753d7940f885c3fc39648f8c4
TmpName <gihubtmp@yopmail.com>
2021-06-27 08:07:39 +0000

    Update streamingdivx.py

--- plugin.video.vstream/resources/sites/streamingdivx.py
+++ plugin.video.vstream/resources/sites/streamingdivx.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # vStream https://github.com/Kodi-vStream/venom-xbmc-addons
 
-return False  # 21/05/2021
-
 import re
 
 from resources.lib.gui.hoster import cHosterGui
@@ -20,7 +18,7 @@ SITE_IDENTIFIER = 'streamingdivx'
 SITE_NAME = 'Streamingdivx'
 SITE_DESC = 'Films VF en streaming.'
 
-URL_MAIN = 'https://ww1.streamingdivx.ws/'
+URL_MAIN = 'https://wvw.streamingdivx.stream/'
 
 MOVIE_NEWS = (URL_MAIN + 'films.html', 'showMovies')
 MOVIE_GENRES = (URL_MAIN + 'films/', 'showGenres')
