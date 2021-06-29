ca7c3a8b8807c1ee5813cbb96c5957a03e2a4a11
razorex <individuel@free.fr>
2021-06-29 17:25:05 +0200

    Update filmstreamingy.py

--- plugin.video.vstream/resources/sites/filmstreamingy.py
+++ plugin.video.vstream/resources/sites/filmstreamingy.py
@@ -15,7 +15,7 @@ SITE_IDENTIFIER = 'filmstreamingy'
 SITE_NAME = 'FilmStreamingY'
 SITE_DESC = 'stream HD, streaming Sans pub, streaming vf'
 
-URL_MAIN = 'https://www.film-streamingi.com/'
+URL_MAIN = "https://www.film-streaminge.com/"
 
 URL_SEARCH_MOVIES = (URL_MAIN + '?s=', 'showMovies')
 FUNCTION_SEARCH = 'showMovies'
