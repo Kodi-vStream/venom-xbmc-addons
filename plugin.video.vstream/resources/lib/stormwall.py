55d7bd666e702c7a9ef35b2174f11ab782b763c8
razorex <individuel@free.fr>
2021-08-25 16:13:59 +0200

    Update stormwall.py

--- plugin.video.vstream/resources/lib/stormwall.py
+++ plugin.video.vstream/resources/lib/stormwall.py
@@ -3,11 +3,12 @@ import os
 import xbmcaddon
 import time
 
-from resources.lib.comaddon import VSlog, xbmc, VSPath, dialog
+from resources.lib.comaddon import VSlog, dialog, VSPath, xbmc
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.config import GestionCookie
 
 UA = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0'
+
 
 class Stormwall(object):
     def __init__(self):
@@ -24,24 +25,24 @@ class Stormwall(object):
         self.url = ''
 
     def parseInt(self, sin):
-        return int(''.join([c for c in re.split(r'[,.]',str(sin))[0] if c.isdigit()])) if re.match(r'\d+', str(sin), re.M) and not callable(sin) else None
+        return int(''.join([c for c in re.split(r'[,.]', str(sin))[0] if c.isdigit()])) if re.match(r'\d+', str(sin), re.M) and not callable(sin) else None
 
     def func3(self, _0x32d742, _0x69aeb7):
         _0x5db4b0 = len(self.a) - 1
         _0x239b12 = ""
         _0x2b4227 = 0
         for _0x2b4227 in range(len(_0x69aeb7)):
-            _0x44dcfd = _0x69aeb7[_0x2b4227];
-            if not _0x44dcfd in self.b:
+            _0x44dcfd = _0x69aeb7[_0x2b4227]
+            if _0x44dcfd not in self.b:
                 _0x239b12 = _0x239b12 + _0x44dcfd
             else:
                 _0x52a03d = self.b[_0x44dcfd] + _0x32d742
                 if _0x52a03d > _0x5db4b0:
-                  _0x52a03d = _0x52a03d - _0x5db4b0 - 1
+                    _0x52a03d = _0x52a03d - _0x5db4b0 - 1
                 else:
                     if _0x52a03d < 0:
                         _0x52a03d = _0x5db4b0 + _0x52a03d + 1
-                _0x239b12 = _0x239b12 + self.a[_0x52a03d];
+                _0x239b12 = _0x239b12 + self.a[_0x52a03d]
 
         return _0x239b12
 
@@ -55,7 +56,7 @@ class Stormwall(object):
             _0x20559e = _0x20559e + self.func3(_0x530eb7 * -1, _0x1c696a)
             _0x530eb7 = _0x530eb7 + 1
             if _0x530eb7 > _0x31b1ca:
-              _0x530eb7 = 0
+                _0x530eb7 = 0
 
         return _0x20559e
 
@@ -65,7 +66,7 @@ class Stormwall(object):
         return False
 
     def DecryptCookie(self, content):
-        #Le nom peut varie selon les pages.
+        # Le nom peut varie selon les pages.
         if "const" in content:
             parseName = "const"
         else:
@@ -80,13 +81,12 @@ class Stormwall(object):
         self.b = {}
 
         for i in range(len(self._0xbd1168)):
-           self.a.append(self._0xbd1168[i])
-           self.b[self._0xbd1168[i]] = i
-
+            self.a.append(self._0xbd1168[i])
+            self.b[self._0xbd1168[i]] = i
 
         _0x3b45bc = self.func5(self.cK, self.cE)
 
-        VSlog ('cookie : '+ self.cN + "=" + _0x3b45bc)
+        VSlog('cookie : '+ self.cN + "=" + _0x3b45bc)
         return self.cN + "=" + _0x3b45bc
 
     def GetHtml(self, url, data=None):
@@ -125,9 +125,9 @@ class Stormwall(object):
         oRequestHandler.addHeaderEntry('Accept-Encoding', 'gzip, deflate')
         try:
             if cookies:
-                oRequestHandler.addCookieEntry(cookies.split('=')[0], cookies.split('=')[1])     
+                oRequestHandler.addCookieEntry(cookies.split('=')[0], cookies.split('=')[1])
         except:
-            pass   
+            pass
         oRequestHandler.addHeaderEntry('Referer', url)
         sHtmlContent = oRequestHandler.request()
         return sHtmlContent
