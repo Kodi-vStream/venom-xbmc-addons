19fe792bb9433f060950d515fc7ef1153960bac4
Arias800 <azertypoil630@gmail.com>
2021-10-17 09:11:36 +0200

    Nettoyage + support de HEAD.

--- plugin.video.vstream/resources/lib/proxy/ProxyHTTPRequestHandler.py
+++ plugin.video.vstream/resources/lib/proxy/ProxyHTTPRequestHandler.py
@@ -4,12 +4,10 @@ from resources.lib.comaddon import isMatrix, VSlog
 
 if isMatrix():
     from http.server import BaseHTTPRequestHandler
-    from socketserver import TCPServer
-    from urllib.parse import parse_qs, urlparse, parse_qsl
+    from urllib.parse import urlparse, parse_qsl
 else:
     from BaseHTTPServer import BaseHTTPRequestHandler
-    from SocketServer import TCPServer
-    from urlparse import urlparse, parse_qs,parse_qsl
+    from urlparse import urlparse,parse_qsl
 
 import requests
 
@@ -17,10 +15,9 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
     protocol_version = 'HTTP/1.0'
     headers = None
 
-    def do_GET(self):
+    def parseUrl(self):
         if "@" in self.path:
             url, h = self.path.split('@')
-            VSlog(h)
             ProxyHTTPRequestHandler.headers = dict(parse_qsl(h))
         else:
             url = self.path
@@ -28,6 +25,13 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
         p = urlparse(url)
         q = dict(parse_qsl(p.query))
         url = q['u']
+        return url
+
+    def do_HEAD(self):
+        self.send_response_only(200)
+
+    def do_GET(self):
+        url = self.parseUrl()
 
         res = requests.get(url,headers=ProxyHTTPRequestHandler.headers)
 
@@ -41,6 +45,5 @@ class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response_only(200)
         self.send_header('Content-Length', len(ret))
         self.send_header('Content-Type', 'application/vnd.apple.mpegurl')
-        self.send_header('Referer', 'https://lb.toonanime.xyz')
         self.end_headers()
         self.wfile.write(ret)
