8b42e17cceb103701604d40886f6436f3bb4a9c6
razorex <individuel@free.fr>
2021-11-01 17:31:04 +0100

    Update vudeo.py

--- plugin.video.vstream/resources/hosters/vudeo.py
+++ plugin.video.vstream/resources/hosters/vudeo.py
@@ -1,19 +1,19 @@
-#-*- coding: utf-8 -*-
-#Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
+# -*- coding: utf-8 -*-
+# vStream https://github.com/Kodi-vStream/venom-xbmc-addons
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
 UA = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0'
 
-class cHoster(iHoster):
 
+class cHoster(iHoster):
     def __init__(self):
         self.__sDisplayName = 'Vudeo'
         self.__sFileName = self.__sDisplayName
         self.__sHD = ''
 
     def getDisplayName(self):
-        return  self.__sDisplayName
+        return self.__sDisplayName
 
     def setDisplayName(self, sDisplayName):
         self.__sDisplayName = sDisplayName + ' [COLOR skyblue]' + self.__sDisplayName + '[/COLOR]'
@@ -55,7 +55,7 @@ class cHoster(iHoster):
         if (aResult[0] == True):
             api_call = aResult[1][0]
 
-        if (api_call):
+        if api_call:
             return True, api_call + '|User-Agent=' + UA + '&Referer=' + self.__sUrl
 
         return False, False
