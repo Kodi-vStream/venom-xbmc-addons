d044e841da1c1df496bf2bf1d599c44428be25be
razorex <individuel@free.fr>
2021-11-01 17:18:24 +0100

    Update streamlare.py
    Nettoyage

--- plugin.video.vstream/resources/hosters/streamlare.py
+++ plugin.video.vstream/resources/hosters/streamlare.py
@@ -1,20 +1,18 @@
-#-*- coding: utf-8 -*-
-#Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
+# -*- coding: utf-8 -*-
+# vStream https://github.com/Kodi-vStream/venom-xbmc-addons
 from resources.lib.handler.requestHandler import cRequestHandler
-from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
 UA = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
 
 
 class cHoster(iHoster):
-
     def __init__(self):
         self.__sDisplayName = 'Streamlare'
         self.__sFileName = self.__sDisplayName
         self.__sHD = ''
 
     def getDisplayName(self):
-        return  self.__sDisplayName
+        return self.__sDisplayName
 
     def setDisplayName(self, sDisplayName):
         self.__sDisplayName = sDisplayName + ' [COLOR skyblue]' + self.__sDisplayName + '[/COLOR]'
@@ -48,17 +46,16 @@ class cHoster(iHoster):
 
         oRequestHandler = cRequestHandler("https://streamlare.com/api/video/get")
         oRequestHandler.setRequestType(1)
-        oRequestHandler.addHeaderEntry('Referer',self.__sUrl)
+        oRequestHandler.addHeaderEntry('Referer', self.__sUrl)
         oRequestHandler.addHeaderEntry('User-Agent', UA)
-        oRequestHandler.addHeaderEntry('X-Requested-With','XMLHttpRequest')
+        oRequestHandler.addHeaderEntry('X-Requested-With', 'XMLHttpRequest')
         oRequestHandler.addHeaderEntry('Origin', 'https://{0}'.format(self.__sUrl.split('/')[2]))
-        oRequestHandler.addJSONEntry('id',self.__sUrl.split('/')[4])        
+        oRequestHandler.addJSONEntry('id', self.__sUrl.split('/')[4])
         sHtmlContent = oRequestHandler.request(jsonDecode=True)
 
         api_call = sHtmlContent['result']['Original']['src']
 
-        
-        if (api_call):
+        if api_call:
             return True, api_call + '|User-Agent=' + UA + '&Referer=' + self.__sUrl
 
         return False, False
