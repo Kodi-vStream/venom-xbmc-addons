427790ffb4fffd92d9b344f19008044c52c224ac
razorex <individuel@free.fr>
2021-11-20 18:41:29 +0100

    Update requestHandler.py
    Forme

--- plugin.video.vstream/resources/lib/handler/requestHandler.py
+++ plugin.video.vstream/resources/lib/handler/requestHandler.py
@@ -8,6 +8,7 @@ from resources.lib.util import urlEncode
 import requests.packages.urllib3.util.connection as urllib3_cn
 import socket
 
+
 class cRequestHandler:
     REQUEST_TYPE_GET = 0
     REQUEST_TYPE_POST = 1
@@ -36,11 +37,10 @@ class cRequestHandler:
         self.forceIPV4 = False
         self.oResponse = None
 
-
     def statusCode(self):
         return self.oResponse.status_code
-        
-    #Utile pour certains hebergeurs qui ne marche pas en ipv6.
+
+    # Utile pour certains hebergeurs qui ne marche pas en ipv6.
     def disableIPV6(self):
         self.forceIPV4 = True
 
@@ -50,14 +50,14 @@ class cRequestHandler:
         """
         family = socket.AF_INET
         if urllib3_cn.HAS_IPV6:
-            family = socket.AF_INET # force ipv6 only if it is available
+            family = socket.AF_INET  # force ipv6 only if it is available
         return family
 
-    #Desactive le ssl
+    # Desactive le ssl
     def disableSSL(self):
         self.verify = False
 
-    #Empeche les redirections 
+    # Empeche les redirections
     def disableRedirect(self):
         self.redirects = False
 
@@ -67,27 +67,27 @@ class cRequestHandler:
     def removeBreakLines(self, bRemoveBreakLines):
         self.__bRemoveBreakLines = bRemoveBreakLines
 
-    #Defini le type de requete
-    #0 : pour un requete GET
-    #1 : pour une requete POST
+    # Defini le type de requete
+    # 0 : pour un requete GET
+    # 1 : pour une requete POST
     def setRequestType(self, cType):
         self.__cType = cType
 
-    #Permets de definir un timeout
+    # Permets de definir un timeout
     def setTimeout(self, valeur):
         self.__timeout = valeur
 
-    #Ajouter un cookie dans le headers de la requete
+    # Ajouter un cookie dans le headers de la requete
     def addCookieEntry(self, sHeaderKey, sHeaderValue):
         aHeader = {sHeaderKey: sHeaderValue}
         self.__Cookie.update(aHeader)
 
-    #Ajouter des parametre JSON
+    # Ajouter des parametre JSON
     def addJSONEntry(self, sHeaderKey, sHeaderValue):
         aHeader = {sHeaderKey: sHeaderValue}
         self.json.update(aHeader)
 
-    #Ajouter un elements dans le headers de la requete
+    # Ajouter un elements dans le headers de la requete
     def addHeaderEntry(self, sHeaderKey, sHeaderValue):
         for sublist in list(self.__aHeaderEntries):
             if sHeaderKey in sublist:
@@ -99,11 +99,11 @@ class cRequestHandler:
         aHeader = {sHeaderKey: sHeaderValue}
         self.__aHeaderEntries.update(aHeader)
 
-    #Ajout un parametre dans la requete
+    # Ajout un parametre dans la requete
     def addParameters(self, sParameterKey, mParameterValue):
         self.__aParamaters[sParameterKey] = mParameterValue
 
-    #Ajoute une ligne de parametre
+    # Ajoute une ligne de parametre
     def addParametersLine(self, mParameterValue):
         self.__aParamatersLine = mParameterValue
 
@@ -127,7 +127,7 @@ class cRequestHandler:
         # self.__sUrl = self.__sUrl.replace(' ', '+')
         return self.__callRequest(jsonDecode)
 
-    #Recupere les cookies de la requete
+    # Recupere les cookies de la requete
     def GetCookies(self):
         if not self.__sResponseHeader:
             return ''
@@ -157,7 +157,7 @@ class cRequestHandler:
             socket.getaddrinfo = self.new_getaddrinfo
 
         if self.__aParamatersLine:
-            sParameters = self.__aParamatersLine            
+            sParameters = self.__aParamatersLine
         else:
             sParameters = self.__aParamaters
 
@@ -205,12 +205,12 @@ class cRequestHandler:
                 sContent = self.oResponse.json()
             else:
                 sContent = self.oResponse.content
-                #Necessaire pour Python 3
-                if isMatrix() and not 'youtube' in self.oResponse.url:
+                # Necessaire pour Python 3
+                if isMatrix() and 'youtube' not in self.oResponse.url:
                     try:
-                       sContent = sContent.decode()
+                        sContent = sContent.decode()
                     except:
-                        #Decodage minimum obligatoire.
+                        # Decodage minimum obligatoire.
                         try:
                             sContent = sContent.decode('unicode-escape')
                         except:
@@ -251,9 +251,9 @@ class cRequestHandler:
             sContent = ''
 
         if self.oResponse != None:
-            if self.oResponse.status_code in [503,403]:
-                if not "Forbidden" in sContent:
-                    #Default
+            if self.oResponse.status_code in [503, 403]:
+                if "Forbidden" not in sContent:
+                    # Default
                     CLOUDPROXY_ENDPOINT = 'http://' + addon().getSetting('ipaddress') + ':8191/v1'
 
                     json_session = False
@@ -264,7 +264,7 @@ class cRequestHandler:
                         dialog().VSerror("%s" % ("Page protege par Cloudflare, veuillez executer  FlareSolverr."))
 
                     if json_session:
-                        #On regarde si une session existe deja.
+                        # On regarde si une session existe deja.
                         if json_session.json()['sessions']:
                             cloudproxy_session = json_session.json()['sessions'][0]
                         else:
@@ -275,7 +275,7 @@ class cRequestHandler:
 
                         self.__aHeaderEntries['Content-Type'] = 'application/x-www-form-urlencoded' if (method == 'post') else 'application/json'
 
-                        #Ont fait une requete.
+                        # Ont fait une requete.
                         json_response = post(CLOUDPROXY_ENDPOINT, headers=self.__aHeaderEntries, json={
                             'cmd': 'request.%s' % method.lower(),
                             'url': self.__sUrl,
@@ -292,8 +292,8 @@ class cRequestHandler:
                             sContent = response['solution']['response']
 
             if self.oResponse and not sContent:
-                #Ignorer ces deux codes erreurs.
-                ignoreStatus = [200,302]
+                # Ignorer ces deux codes erreurs.
+                ignoreStatus = [200, 302]
                 if self.oResponse.status_code not in ignoreStatus:
                     dialog().VSerror("%s (%d),%s" % (addon().VSlang(30205), self.oResponse.status_code, self.__sUrl))
 
@@ -320,7 +320,7 @@ class cRequestHandler:
                 path = VSPath('special://home/addons/script.module.dnspython/lib/')
             else:
                 path = VSPath('special://home/addons/script.module.dnspython/lib/').decode('utf-8')
-                             
+
             if path not in sys.path:
                 sys.path.append(path)
             host = args[0]
@@ -341,6 +341,7 @@ class cRequestHandler:
         except Exception as e:
             VSlog("new_getaddrinfo ERROR: {0}".format(e))
             return self.save_getaddrinfo(*args)
+
 
 # ******************************************************************************
 # from https://github.com/eliellis/mpart.py
