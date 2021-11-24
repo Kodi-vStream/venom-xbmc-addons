a7eb3d9f67eb262c8c98bd91cc312c87769e4f65
Arias800 <azertypoil630@gmail.com>
2021-11-24 12:36:00 +0100

    Update pluginHandler.py

--- plugin.video.vstream/resources/lib/handler/pluginHandler.py
+++ plugin.video.vstream/resources/lib/handler/pluginHandler.py
@@ -3,8 +3,9 @@
 
 import sys
 import xbmcvfs
+import json
 
-from resources.lib.comaddon import addon, VSlog
+from resources.lib.comaddon import addon, VSlog, VSPath
 
 class cPluginHandler:
 
@@ -22,7 +23,7 @@ class cPluginHandler:
 
     def __getFileNamesFromFolder(self, sFolder):
         aNameList = []
-        folder, items = xbmcvfs.listdir(sFolder)
+        items = xbmcvfs.listdir(sFolder)[1][:-1]
         items.sort()
         for sItemName in items:
 
@@ -34,62 +35,71 @@ class cPluginHandler:
             # xbox hack
             sFilePath = sFilePath.replace('\\', '/')
 
-            VSlog("Load Plugin %s" % sItemName)
-
             if (xbmcvfs.exists(sFilePath) == True):
                 if (sFilePath.lower().endswith('py')):
                     sItemName = sItemName.replace('.py', '')
                     aNameList.append(sItemName)
         return aNameList
 
-    def __importPlugin(self, sName):
+    def __importPlugin(self, sName, sLabel=""):
         try:
-            exec("from resources.sites import " + sName, globals())
-            exec("sSiteName = " + sName + ".SITE_NAME", globals())
-            exec("sSiteDesc = " + sName + ".SITE_DESC", globals())
+            exec ("from resources.sites import " + sName, globals())             
+            exec ("sSiteName = " + sName + ".SITE_NAME", globals())
             sPluginSettingsName = 'plugin_' + sName
-            return sSiteName, sPluginSettingsName, sSiteDesc
+            if sLabel:
+                exec ("sSearch = " + sName + "." + sLabel, globals())
+                return sSearch[0], sPluginSettingsName, sSearch[1], sSiteName
+            else:
+                exec("sSiteName = " + sName + ".SITE_NAME", globals())
+                exec("sSiteDesc = " + sName + ".SITE_DESC", globals())
+                return sSiteName, sPluginSettingsName, sSiteDesc
         except Exception as e:
             VSlog("Cannot import plugin " + str(sName))
             VSlog("Detail de l\'erreur " + str(e))
             return False, False
-
-    def getAvailablePlugins(self, force=False):
+            
+    def getAvailablePlugins(self, sLabel="", force=False):
+        path = VSPath('special://home/addons/plugin.video.vstream/resources/sites.json')
+        with open(path) as f:
+            data = json.load(f)
         addons = addon()
 
         sFolder = "special://home/addons/plugin.video.vstream/resources/sites"
         sFolder = sFolder.replace('\\', '/')
-        VSlog("Sites Folder " + sFolder)
 
         aFileNames = self.__getFileNamesFromFolder(sFolder)
 
         aPlugins = []
         for sFileName in aFileNames:
-            if sFileName.startswith('__'):  # exclut par exemple __init__.py
-                continue
-            # wir versuchen das plugin zu importieren
-            aPlugin = self.__importPlugin(sFileName)
-            if (aPlugin[0] != False):
-                sSiteName = aPlugin[0]
-                sPluginSettingsName = aPlugin[1]
-                sSiteDesc = aPlugin[2]
+            # existieren zu diesem plugin die an/aus settings
+            bPlugin = data['site']["plugin_" + sFileName]['active']
+            if (bPlugin != ''):
+                # settings gefunden
+                if (bPlugin == 'true') or (force == True):
+                    # wir versuchen das plugin zu importieren
+                    if sLabel:
+                        aPlugin = self.__importPlugin(sFileName, sLabel)
+                    else:
+                        aPlugin = self.__importPlugin(sFileName)
 
-                # existieren zu diesem plugin die an/aus settings
-                bPlugin = addons.getSetting(sPluginSettingsName)
-                if (bPlugin != ''):
-                    # settings gefunden
-                    if (bPlugin == 'true') or (force == True):
-                        aPlugins.append(self.__createAvailablePluginsItem(sSiteName, sFileName, sSiteDesc))
-                else:
-                    # settings nicht gefunden, also schalten wir es trotzdem sichtbar
-                    aPlugins.append(self.__createAvailablePluginsItem(sSiteName, sFileName, sSiteDesc))
+                    if (aPlugin[0] != False):
+                        sPluginSettingsName = aPlugin[1]
+                        sSiteDesc = aPlugin[2]
+                        if sLabel:
+                            sSiteUrl = aPlugin[0]
+                            sSiteName = aPlugin[3]
+                            item = self.__createAvailablePluginsItem(sSiteUrl, sSiteName, sFileName, sSiteDesc)
+                        else:
+                            sSiteName = aPlugin[0]
+                            item = self.__createAvailablePluginsItem("", sSiteName, sFileName, sSiteDesc)
+
+                        aPlugins.append(item)
 
         return aPlugins
 
     def getAllPlugins(self):
         sFolder = "special://home/addons/plugin.video.vstream/resources/sites"
         sFolder = sFolder.replace('\\', '/')
-        VSlog("Sites Folder " + sFolder)
 
         aFileNames = self.__getFileNamesFromFolder(sFolder)
 
@@ -103,12 +113,14 @@ class cPluginHandler:
                 sSiteDesc = aPlugin[2]
 
                 # settings nicht gefunden, also schalten wir es trotzdem sichtbar
-                aPlugins.append(self.__createAvailablePluginsItem(sSiteName, sFileName, sSiteDesc))
+                aPlugins.append(self.__createAvailablePluginsItem("", sSiteName, sFileName, sSiteDesc))
 
         return aPlugins
 
-    def __createAvailablePluginsItem(self, sPluginName, sPluginIdentifier, sPluginDesc):
+    def __createAvailablePluginsItem(self, sSiteUrl, sPluginName, sPluginIdentifier, sPluginDesc):
         aPluginEntry = []
+        if sSiteUrl:
+            aPluginEntry.append(sSiteUrl)
         aPluginEntry.append(sPluginName)
         aPluginEntry.append(sPluginIdentifier)
         aPluginEntry.append(sPluginDesc)
