83cfbab728b0bef1061bd5861a3dc4fe56383491
sizanic <47307622+sizanic@users.noreply.github.com>
2021-12-22 01:46:24 +0100

    [FULLMATCH TV] - Ajout de 3 sports et gestion de la recherche.

--- plugin.video.vstream/resources/sites/fullmatchtv.py
+++ plugin.video.vstream/resources/sites/fullmatchtv.py
@@ -15,10 +15,13 @@ SITE_DESC = 'Regarder vos sports MLB NBA streaming complets, gratuit et illimit√
 
 URL_MAIN = 'https://fullmatchtv.com/'
 
-URL_SEARCH = (URL_MAIN + '?s/', 'showMovies')
+URL_SEARCH = (URL_MAIN + '?s=', 'showMovies')
 
+MOVIE_AFL = (URL_MAIN + 'afl/', 'showMovies')
+MOVIE_MOTOR = (URL_MAIN + 'motorsports/', 'showMovies')
 MOVIE_NBA = (URL_MAIN + 'nba/', 'showMovies')
 MOVIE_NFL = (URL_MAIN + 'nfl/', 'showMovies')
+MOVIE_NHL = (URL_MAIN + 'nhl/', 'showMovies')
 MOVIE_MLB = (URL_MAIN + 'mlb/', 'showMovies')
 MOVIE_RUGBY = (URL_MAIN + 'rugby/', 'showMovies')
 MOVIE_MMA = (URL_MAIN + 'wwe-mma/', 'showMovies')
@@ -31,11 +34,20 @@ def load():
     oOutputParameterHandler.addParameter('siteUrl', URL_SEARCH[0])
     oGui.addDir(SITE_IDENTIFIER, 'showSearch', 'Recherche', 'search.png', oOutputParameterHandler)
 
+    oOutputParameterHandler.addParameter('siteUrl', MOVIE_AFL[0])
+    oGui.addDir(SITE_IDENTIFIER, MOVIE_AFL[1], 'AFL', 'sport.png', oOutputParameterHandler)
+
+    oOutputParameterHandler.addParameter('siteUrl', MOVIE_MOTOR[0])
+    oGui.addDir(SITE_IDENTIFIER, MOVIE_MOTOR[1], 'MOTORSPORT', 'sport.png', oOutputParameterHandler)
+
     oOutputParameterHandler.addParameter('siteUrl', MOVIE_NBA[0])
     oGui.addDir(SITE_IDENTIFIER, MOVIE_NBA[1], 'NBA', 'sport.png', oOutputParameterHandler)
 
     oOutputParameterHandler.addParameter('siteUrl', MOVIE_NFL[0])
     oGui.addDir(SITE_IDENTIFIER, MOVIE_NFL[1], 'NFL', 'sport.png', oOutputParameterHandler)
+
+    oOutputParameterHandler.addParameter('siteUrl', MOVIE_NHL[0])
+    oGui.addDir(SITE_IDENTIFIER, MOVIE_NHL[1], 'NHL', 'sport.png', oOutputParameterHandler)
 
     oOutputParameterHandler.addParameter('siteUrl', MOVIE_MLB[0])
     oGui.addDir(SITE_IDENTIFIER, MOVIE_MLB[1], 'MLB', 'sport.png', oOutputParameterHandler)
@@ -53,7 +65,8 @@ def showSearch():
     oGui = cGui()
     sSearchText = oGui.showKeyBoard()
     if (sSearchText != False):
-        showMovies(sSearchText)
+        sUrl = URL_SEARCH[0] + sSearchText.replace(' ', '+')
+        showMovies(sUrl)
         oGui.setEndOfDirectory()
         return
 
@@ -61,13 +74,17 @@ def showSearch():
 def showMovies(sSearch=''):
     oGui = cGui()
 
-    oInputParameterHandler = cInputParameterHandler()
-    sUrl = oInputParameterHandler.getValue('siteUrl')
-    oRequestHandler = cRequestHandler(sUrl)
+    if sSearch:
+        sUrl = sSearch
+        sPattern = '(?:<div class="td_module_16 td_module_wrap td-animation-stack">|<div class="td-module-container td-category-pos-image">.+?<div class="td-module-thumb">).+?href="([^"]+).+?title="([^"]+).+?.+?(?:src="([^"]+)|url.+?([^\']+))'
+    else:
+        oInputParameterHandler = cInputParameterHandler()
+        sUrl = oInputParameterHandler.getValue('siteUrl')
+        sPattern = '(?:<div class="td_module_mx7 td_module_wrap td-animation-stack">|<div class="td-module-container td-category-pos-image">.+?<div class="td-module-thumb">).+?href="([^"]+).+?title="([^"]+).+?.+?(?:src="([^"]+)|url.+?([^\']+))'
 
+    oRequestHandler = cRequestHandler(sUrl)
     sHtmlContent = oRequestHandler.request()
 
-    sPattern = '(?:<div class="td_module_mx7 td_module_wrap td-animation-stack">|<div class="td-module-container td-category-pos-image">.+?<div class="td-module-thumb">).+?href="([^"]+).+?title="([^"]+).+?.+?(?:src="([^"]+)|url.+?([^\']+))'
 
     oParser = cParser()
     aResult = oParser.parse(sHtmlContent, sPattern)
