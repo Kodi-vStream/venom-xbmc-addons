ca547c76769811e08ec9805361095fd0773e774f
razorex <individuel@free.fr>
2022-03-18 22:07:10 +0100

    Update mesfilms.py
    new url

--- plugin.video.vstream/resources/sites/mesfilms.py
+++ plugin.video.vstream/resources/sites/mesfilms.py
@@ -8,7 +8,7 @@ from resources.lib.gui.gui import cGui
 from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
 from resources.lib.handler.requestHandler import cRequestHandler
-from resources.lib.comaddon import progress
+from resources.lib.comaddon import progress, siteManager
 from resources.lib.parser import cParser
 from resources.lib.util import QuotePlus, cUtil
 
@@ -16,7 +16,7 @@ SITE_IDENTIFIER = 'mesfilms'
 SITE_NAME = 'Mes Films'
 SITE_DESC = 'Mes Films - Films en streaming HD'
 
-URL_MAIN = "https://mesfilms.buzz/"
+URL_MAIN = siteManager().getUrlMain(SITE_IDENTIFIER)
 UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0'
 
 URL_SEARCH = (URL_MAIN + '?s=', 'showSearchResult')
