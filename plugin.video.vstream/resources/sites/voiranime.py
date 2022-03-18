c7318ea36cef71210387a535a40a254abb1d0c3d
razorex <individuel@free.fr>
2022-03-18 22:39:40 +0100

    Update voiranime.py
    new url

--- plugin.video.vstream/resources/sites/voiranime.py
+++ plugin.video.vstream/resources/sites/voiranime.py
@@ -9,7 +9,7 @@ from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
-from resources.lib.comaddon import progress
+from resources.lib.comaddon import progress, siteManager
 from resources.lib.util import Quote
 
 UA = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0'
@@ -18,7 +18,7 @@ SITE_IDENTIFIER = 'voiranime'
 SITE_NAME = 'VoirAnime'
 SITE_DESC = 'Animés en Streaming'
 
-URL_MAIN = 'https://voiranime.to/'
+URL_MAIN = siteManager().getUrlMain(SITE_IDENTIFIER)
 
 ANIM_ANIMS = (True, 'load')
 ANIM_NEWS = (URL_MAIN, 'showAnimes')
