f2e5fadbe00715ace3c39a3db67ce7de15304cc2
razorex <individuel@free.fr>
2022-03-18 22:28:18 +0100

    Update streampourvous.py
    new url

--- plugin.video.vstream/resources/sites/streampourvous.py
+++ plugin.video.vstream/resources/sites/streampourvous.py
@@ -7,7 +7,7 @@ from resources.lib.gui.gui import cGui
 from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
 from resources.lib.handler.requestHandler import cRequestHandler
-from resources.lib.comaddon import progress
+from resources.lib.comaddon import progress, siteManager
 from resources.lib.parser import cParser
 from resources.lib.util import cUtil
 
@@ -15,7 +15,7 @@ SITE_IDENTIFIER = 'streampourvous'
 SITE_NAME = 'StreampourVous'
 SITE_DESC = 'films,'
 
-URL_MAIN = "https://vvw.streampourvous.ws/"
+URL_MAIN = siteManager().getUrlMain(SITE_IDENTIFIER)
 
 MOVIE_MOVIE = (True, 'showMenuMovies')
 MOVIE_NEWS = (URL_MAIN + 'film-streaming/', 'showMovies')
