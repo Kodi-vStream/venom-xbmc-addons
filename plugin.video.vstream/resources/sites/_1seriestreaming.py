88dbed477747a5905e47f78545699fdaa8ec25c9
razorex <individuel@free.fr>
2022-03-18 23:20:25 +0100

    Update _1seriestreaming.py

--- plugin.video.vstream/resources/sites/_1seriestreaming.py
+++ plugin.video.vstream/resources/sites/_1seriestreaming.py
@@ -9,7 +9,7 @@ from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
-from resources.lib.comaddon import progress
+from resources.lib.comaddon import progress, siteManager
 from resources.lib.util import cUtil
 
 UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:70.0) Gecko/20100101 Firefox/70.0'
@@ -18,7 +18,7 @@ SITE_IDENTIFIER = '_1seriestreaming'
 SITE_NAME = '1 Serie Streaming'
 SITE_DESC = 'Séries & Animés en Streaming'
 
-URL_MAIN = "https://1seriestreaming.com/"
+URL_MAIN = siteManager().getUrlMain(SITE_IDENTIFIER)
 
 SERIE_SERIES = ('http://', 'load')
 SERIE_NEWS = (URL_MAIN + 'series-streaming', 'showSeries')
