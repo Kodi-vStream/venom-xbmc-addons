73eb2cb45b0dd75417877081ccd098bc2f4390b9
razorex <individuel@free.fr>
2022-03-29 19:43:35 +0200

    Update les_docus.py

--- plugin.video.vstream/resources/sites/les_docus.py
+++ plugin.video.vstream/resources/sites/les_docus.py
@@ -9,12 +9,14 @@ from resources.lib.handler.inputParameterHandler import cInputParameterHandler
 from resources.lib.handler.outputParameterHandler import cOutputParameterHandler
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
+from resources.lib.comaddon import siteManager
 
 SITE_IDENTIFIER = 'les_docus'
 SITE_NAME = 'Les docus'
 SITE_DESC = 'Documentaires reportages et vidéos en streaming en francais.'
 
-URL_MAIN = "https://www.les-docus.com/"
+URL_MAIN = siteManager().getUrlMain(SITE_IDENTIFIER)
+# URL_MAIN = dans sites.json
 
 URL_SEARCH = (URL_MAIN + '?s=', 'showMovies')
 URL_SEARCH_MISC = (URL_SEARCH[0], 'showMovies')
@@ -45,7 +47,7 @@ def showSearch():
     oGui = cGui()
 
     sSearchText = oGui.showKeyBoard()
-    if (sSearchText != False):
+    if sSearchText != False:
         sUrl = URL_SEARCH[0] + sSearchText
         showMovies(sUrl)
         oGui.setEndOfDirectory()
@@ -56,74 +58,74 @@ def showGenres():
     oGui = cGui()
 
     liste = []
-    liste.append(['[COLOR teal]ARTS[/COLOR]', URL_MAIN + 'arts/'])
-    liste.append(['Architecture', URL_MAIN + 'arts/architecture/'])
-    liste.append(['Cinéma', URL_MAIN + 'arts/cinema/'])
-    liste.append(['Dessin', URL_MAIN + 'arts/dessin/'])
-    liste.append(['Littérature', URL_MAIN + 'arts/litterature/'])
-    liste.append(['Musique', URL_MAIN + 'arts/musique/'])
-    liste.append(['Peinture', URL_MAIN + 'arts/peinture/'])
-    liste.append(['Sculpture', URL_MAIN + 'arts/sculpture/'])
+    liste.append(['[COLOR teal]ARTS[/COLOR]', 'arts/'])
+    liste.append(['Architecture', 'arts/architecture/'])
+    liste.append(['Cinéma', 'arts/cinema/'])
+    liste.append(['Dessin', 'arts/dessin/'])
+    liste.append(['Littérature', 'arts/litterature/'])
+    liste.append(['Musique', 'arts/musique/'])
+    liste.append(['Peinture', 'arts/peinture/'])
+    liste.append(['Sculpture', 'arts/sculpture/'])
 
-    liste.append(['[COLOR teal]HISTOIRE[/COLOR]', URL_MAIN + 'histoire/'])
-    liste.append(['Préhistoire', URL_MAIN + 'histoire/prehistoire/'])
-    liste.append(['Antiquité', URL_MAIN + 'histoire/antiquite/'])
-    liste.append(['Moyen age', URL_MAIN + 'histoire/moyen-age/'])
-    liste.append(['Temps modernes', URL_MAIN + 'histoire/temps-modernes/'])
-    liste.append(['Temps révolutionnaires', URL_MAIN + 'histoire/temps-revolutionnaires/'])
-    liste.append(['19 eme siecle', URL_MAIN + 'histoire/19eme-siecle/'])
-    liste.append(['20 eme siecle', URL_MAIN + 'histoire/20eme-siecle/'])
-    liste.append(['Epoque comtemporaine', URL_MAIN + 'histoire/epoque-contemporaine/'])
+    liste.append(['[COLOR teal]HISTOIRE[/COLOR]', 'histoire/'])
+    liste.append(['Préhistoire', 'histoire/prehistoire/'])
+    liste.append(['Antiquité', 'histoire/antiquite/'])
+    liste.append(['Moyen age', 'histoire/moyen-age/'])
+    liste.append(['Temps modernes', 'histoire/temps-modernes/'])
+    liste.append(['Temps révolutionnaires', 'histoire/temps-revolutionnaires/'])
+    liste.append(['19 eme siecle', 'histoire/19eme-siecle/'])
+    liste.append(['20 eme siecle', 'histoire/20eme-siecle/'])
+    liste.append(['Epoque contemporaine', 'histoire/epoque-contemporaine/'])
 
-    liste.append(['[COLOR teal]SOCIETE[/COLOR]', URL_MAIN + 'societe/'])
-    liste.append(['Argent', URL_MAIN + 'societe/argent/'])
-    liste.append(['Monde', URL_MAIN + 'societe/monde/'])
-    liste.append(['Politique', URL_MAIN + 'societe/politique/'])
-    liste.append(['Sexualité', URL_MAIN + 'societe/sexualite/'])
-    liste.append(['Social', URL_MAIN + 'societe/social/'])
+    liste.append(['[COLOR teal]SOCIETE[/COLOR]', 'societe/'])
+    liste.append(['Argent', 'societe/argent/'])
+    liste.append(['Monde', 'societe/monde/'])
+    liste.append(['Politique', 'societe/politique/'])
+    liste.append(['Sexualité', 'societe/sexualite/'])
+    liste.append(['Social', 'societe/social/'])
 
-    liste.append(['[COLOR teal]SCIENCES[/COLOR]', URL_MAIN + 'sciences/'])
-    liste.append(['Astronomie', URL_MAIN + 'sciences/astronomie/'])
-    liste.append(['Ecologie', URL_MAIN + 'sciences/ecologie/'])
-    liste.append(['Economie', URL_MAIN + 'sciences/economie/'])
-    liste.append(['Génétique', URL_MAIN + 'sciences/genetique/'])
-    liste.append(['Géographie', URL_MAIN + 'sciences/geographie/'])
-    liste.append(['Géologie', URL_MAIN + 'sciences/geologie/'])
-    liste.append(['Mathématiques', URL_MAIN + 'sciences/mathematique/'])
-    liste.append(['Médecine', URL_MAIN + 'sciences/medecine/'])
-    liste.append(['Physique', URL_MAIN + 'sciences/physique/'])
-    liste.append(['Psychologie', URL_MAIN + 'sciences/psychologie/'])
+    liste.append(['[COLOR teal]SCIENCES[/COLOR]', 'sciences/'])
+    liste.append(['Astronomie', 'sciences/astronomie/'])
+    liste.append(['Ecologie', 'sciences/ecologie/'])
+    liste.append(['Economie', 'sciences/economie/'])
+    liste.append(['Génétique', 'sciences/genetique/'])
+    liste.append(['Géographie', 'sciences/geographie/'])
+    liste.append(['Géologie', 'sciences/geologie/'])
+    liste.append(['Mathématiques', 'sciences/mathematique/'])
+    liste.append(['Médecine', 'sciences/medecine/'])
+    liste.append(['Physique', 'sciences/physique/'])
+    liste.append(['Psychologie', 'sciences/psychologie/'])
 
-    liste.append(['[COLOR teal]TECHNOLOGIE[/COLOR]', URL_MAIN + 'technologie/'])
-    liste.append(['Aviation', URL_MAIN + 'technologie/aviation/'])
-    liste.append(['Informatique', URL_MAIN + 'technologie/informatique/'])
-    liste.append(['Marine', URL_MAIN + 'technologie/marine/'])
-    liste.append(['Téléphonie', URL_MAIN + 'technologie/telephonie'])
+    liste.append(['[COLOR teal]TECHNOLOGIE[/COLOR]', 'technologie/'])
+    liste.append(['Aviation', 'technologie/aviation/'])
+    liste.append(['Informatique', 'technologie/informatique/'])
+    liste.append(['Marine', 'technologie/marine/'])
+    liste.append(['Téléphonie', 'technologie/telephonie'])
 
-    liste.append(['[COLOR teal]PARANORMAL[/COLOR]', URL_MAIN + 'paranormal/'])
-    liste.append(['Fantames et esprits', URL_MAIN + 'paranormal/fantomes-et-esprits/'])
-    liste.append(['OVNI et extraterrestres', URL_MAIN + 'paranormal/ovnis-et-extraterrestres/'])
-    liste.append(['Cryptozoologie', URL_MAIN + 'paranormal/cryptozoologie/'])
-    liste.append(['Mysteres et legendes', URL_MAIN + 'paranormal/mysteres-et-legendes/'])
-    liste.append(['Divers', URL_MAIN + 'paranormal/divers/'])
+    liste.append(['[COLOR teal]PARANORMAL[/COLOR]', 'paranormal/'])
+    liste.append(['Fantames et esprits', 'paranormal/fantomes-et-esprits/'])
+    liste.append(['OVNI et extraterrestres', 'paranormal/ovnis-et-extraterrestres/'])
+    liste.append(['Cryptozoologie', 'paranormal/cryptozoologie/'])
+    liste.append(['Mysteres et legendes', 'paranormal/mysteres-et-legendes/'])
+    liste.append(['Divers', 'paranormal/divers/'])
 
-    liste.append(['[COLOR teal]AUTRES[/COLOR]', URL_MAIN + 'autres/'])
-    liste.append(['Animaux', URL_MAIN + 'autres/animaux/'])
-    liste.append(['Gastronomie', URL_MAIN + 'autres/gastronomie/'])
-    liste.append(['Jeux video', URL_MAIN + 'autres/jeux-video/'])
-    liste.append(['Loisirs', URL_MAIN + 'autres/loisirs/'])
-    liste.append(['Métiers', URL_MAIN + 'autres/metiers/'])
-    liste.append(['Militaire', URL_MAIN + 'autres/militaire/'])
-    liste.append(['Nature', URL_MAIN + 'autres/nature/'])
-    liste.append(['Policier', URL_MAIN + 'autres/policier/'])
-    liste.append(['Religion', URL_MAIN + 'autres/religion/'])
-    liste.append(['Santé', URL_MAIN + 'autres/sante/'])
-    liste.append(['Sport', URL_MAIN + 'autres/sport/'])
-    liste.append(['Voyage', URL_MAIN + 'autres/voyage/'])
+    liste.append(['[COLOR teal]AUTRES[/COLOR]', 'autres/'])
+    liste.append(['Animaux', 'autres/animaux/'])
+    liste.append(['Gastronomie', 'autres/gastronomie/'])
+    liste.append(['Jeux video', 'autres/jeux-video/'])
+    liste.append(['Loisirs', 'autres/loisirs/'])
+    liste.append(['Métiers', 'autres/metiers/'])
+    liste.append(['Militaire', 'autres/militaire/'])
+    liste.append(['Nature', 'autres/nature/'])
+    liste.append(['Policier', 'autres/policier/'])
+    liste.append(['Religion', 'autres/religion/'])
+    liste.append(['Santé', 'autres/sante/'])
+    liste.append(['Sport', 'autres/sport/'])
+    liste.append(['Voyage', 'autres/voyage/'])
 
     oOutputParameterHandler = cOutputParameterHandler()
     for sTitle, sUrl in liste:
-        oOutputParameterHandler.addParameter('siteUrl', sUrl)
+        oOutputParameterHandler.addParameter('siteUrl', URL_MAIN + sUrl)
         oGui.addDir(SITE_IDENTIFIER, 'showMovies', sTitle, 'genres.png', oOutputParameterHandler)
 
     oGui.setEndOfDirectory()
@@ -145,10 +147,10 @@ def showMovies(sSearch=''):
 
     aResult = oParser.parse(sHtmlContent, sPattern)
 
-    if (aResult[0] == False):
+    if aResult[0] is False:
         oGui.addText(SITE_IDENTIFIER)
 
-    if (aResult[0] == True):
+    if aResult[0] is True:
         oOutputParameterHandler = cOutputParameterHandler()
         for aEntry in aResult[1]:
 
@@ -164,7 +166,7 @@ def showMovies(sSearch=''):
             oGui.addMisc(SITE_IDENTIFIER, 'showHosters', sTitle, 'doc.png', sThumb, sDesc, oOutputParameterHandler)
 
         sNextPage, sPaging = __checkForNextPage(sHtmlContent)
-        if (sNextPage != False):
+        if sNextPage != False:
             oOutputParameterHandler = cOutputParameterHandler()
             oOutputParameterHandler.addParameter('siteUrl', sNextPage)
             oGui.addNext(SITE_IDENTIFIER, 'showMovies', 'Page ' + sPaging, oOutputParameterHandler)
@@ -202,23 +204,23 @@ def showHosters():
     sPattern = '<iframe.+?src="(.+?)"'
     aResult = oParser.parse(sHtmlContent1, sPattern)
 
-    if not (aResult[0] == True):
+    if not (aResult[0] is True):
         sPattern = 'data-video_id="(.+?)"'
         aResult = oParser.parse(sHtmlContent1, sPattern)
-        if (aResult[0] == True):
+        if aResult[0] is True:
             sHosterUrl = 'https://www.youtube.com/embed/' + aResult[1][0]
             oHoster = cHosterGui().checkHoster(sHosterUrl)
-            if (oHoster != False):
+            if oHoster != False:
                 oHoster.setDisplayName(sMovieTitle)
                 oHoster.setFileName(sMovieTitle)
                 cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumb)
         else:
             sPattern = '<iframe.+?data-src="([^"]+)'
             aResult = oParser.parse(sHtmlContent, sPattern)
-            if (aResult[0] == True):
+            if aResult[0] is True:
                 sHosterUrl = aResult[1][0]
                 oHoster = cHosterGui().checkHoster(sHosterUrl)
-                if (oHoster != False):
+                if oHoster != False:
                     oHoster.setDisplayName(sMovieTitle)
                     oHoster.setFileName(sMovieTitle)
                     cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumb)
@@ -226,7 +228,7 @@ def showHosters():
         for aEntry in list(set(aResult[1])):
             sHosterUrl = aEntry
             oHoster = cHosterGui().checkHoster(sHosterUrl)
-            if (oHoster != False):
+            if oHoster != False:
                 oHoster.setDisplayName(sMovieTitle)
                 oHoster.setFileName(sMovieTitle)
                 cHosterGui().showHoster(oGui, oHoster, sHosterUrl, sThumb)
