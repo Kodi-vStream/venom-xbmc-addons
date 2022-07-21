58bdccfd4f0e39a01a93177e54b7fd93ba658a8f
razorex <individuel@free.fr>
2022-07-21 21:42:53 +0200

    Update dailymotion.py

--- plugin.video.vstream/resources/hosters/dailymotion.py
+++ plugin.video.vstream/resources/hosters/dailymotion.py
@@ -1,9 +1,10 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # https://github.com/Kodi-vStream/venom-xbmc-addons
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
-from resources.lib.comaddon import dialog, VSlog
+from resources.lib.comaddon import dialog
+
 
 class cHoster(iHoster):
 
@@ -20,21 +21,21 @@ class cHoster(iHoster):
 
     def _getMediaLinkForGuest(self):
         api_call = False
-        url=[]
-        qua=[]
+        url = []
+        qua = []
 
         oRequest = cRequestHandler(self._url)
         sHtmlContent = oRequest.request()
 
         oParser = cParser()
 
-        sPattern =  '{"type":"application.+?mpegURL","url":"([^"]+)"}'
+        sPattern = '{"type":"application.+?mpegURL","url":"([^"]+)"}'
         aResult = oParser.parse(sHtmlContent, sPattern)
 
         if aResult[0] is True:
             oRequest = cRequestHandler(aResult[1][0])
-            oRequest.addHeaderEntry('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:70.0) ' + \
-                'Gecko/20100101 Firefox/70.0')
+            oRequest.addHeaderEntry('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:70.0) ' +
+                                    'Gecko/20100101 Firefox/70.0')
             oRequest.addHeaderEntry('Accept-Language', 'fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3')
             sHtmlContent = oRequest.request()
 
@@ -45,9 +46,8 @@ class cHoster(iHoster):
                     quality = aEntry[0].replace('@60', '')
                     if quality not in qua:
                         qua.append(quality)
-                        link = aEntry[2] if aEntry[2]  else 'http' + aEntry[3]
+                        link = aEntry[2] if aEntry[2] else 'http' + aEntry[3]
                         url.append(link)
-
 
             api_call = dialog().VSselectqual(qua, url)
 
