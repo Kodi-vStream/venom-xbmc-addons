1c8b8f5a9b47ee82c471e0fea3c7ad59346a0c49
razorex <individuel@free.fr>
2022-07-21 22:29:48 +0200

    Update jawcloud.py

--- plugin.video.vstream/resources/hosters/jawcloud.py
+++ plugin.video.vstream/resources/hosters/jawcloud.py
@@ -1,8 +1,7 @@
-#-*- coding: utf-8 -*-
-#Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
+# -*- coding: utf-8 -*-
+# vStream https://github.com/Kodi-vStream/venom-xbmc-addons
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
-# from resources.lib.packer import cPacker
 from resources.hosters.hoster import iHoster
 
 
@@ -12,22 +11,18 @@ class cHoster(iHoster):
         iHoster.__init__(self, 'jawcloud', 'Jawcloud')
 
     def _getMediaLinkForGuest(self):
-        api_call =''
+        api_call = ''
 
         oRequest = cRequestHandler(self._url)
         sHtmlContent = oRequest.request()
-
-        #VSlog(str(self._url))
 
         oParser = cParser()
         sPattern = '<source.+?src="(.+?)"'
 
         aResult = oParser.parse(sHtmlContent, sPattern)
 
-        if (aResult[0]):
+        if aResult[0]:
             api_call = aResult[1][0]
-
-        #VSlog(str(api_call))
 
         if api_call:
             return True, api_call
