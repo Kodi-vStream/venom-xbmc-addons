67ac7f3a6fc2631fe453b72297d092c4a86f4747
razorex <individuel@free.fr>
2022-07-25 19:50:47 +0200

    Update vidplayer.py

--- plugin.video.vstream/resources/hosters/vidplayer.py
+++ plugin.video.vstream/resources/hosters/vidplayer.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # https://github.com/Kodi-vStream/venom-xbmc-addons
 # https://vidplayer.cz/v/xxxxxxx
 import json
@@ -9,20 +9,20 @@ from resources.lib.comaddon import dialog
 
 UA = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'
 
-class cHoster(iHoster):
 
+class cHoster(iHoster):
     def __init__(self):
         iHoster.__init__(self, 'vidplayer', 'Vidplayer')
 
     def _getMediaLinkForGuest(self):
-        req = self._url.replace('/v/','/api/source/')
+        req = self._url.replace('/v/', '/api/source/')
         pdata = 'r'
         oRequestHandler = cRequestHandler(req)
         oRequestHandler.setRequestType(1)
 
         oRequestHandler.addParametersLine(pdata)
         sHtmlContent = oRequestHandler.request()
-        jsonrsp  = json.loads(sHtmlContent )
+        jsonrsp = json.loads(sHtmlContent)
 
         list_url = []
         list_q = []
@@ -35,7 +35,7 @@ class cHoster(iHoster):
             list_q.append(q)
 
         if list_url:
-            api_call = dialog().VSselectqual(list_q,list_url)
+            api_call = dialog().VSselectqual(list_q, list_url)
 
         if api_call:
             return True, api_call
