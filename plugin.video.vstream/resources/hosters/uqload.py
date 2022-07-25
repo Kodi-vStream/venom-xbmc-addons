4576b94c7be89ea9f6514c3e322020ce638e486f
razorex <individuel@free.fr>
2022-07-25 19:17:24 +0200

    Update uqload.py

--- plugin.video.vstream/resources/hosters/uqload.py
+++ plugin.video.vstream/resources/hosters/uqload.py
@@ -1,10 +1,11 @@
-#-*- coding: utf-8 -*-
-#Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
-#http://uqload.com/embed-xxx.html
+# -*- coding: utf-8 -*-
+# vStream https://github.com/Kodi-vStream/venom-xbmc-addons
+# http://uqload.com/embed-xxx.html
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
 UA = 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
+
 
 class cHoster(iHoster):
 
