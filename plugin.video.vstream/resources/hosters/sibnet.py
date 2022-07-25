29a58cb1a65f4e4665324b0258008bb1ee25778e
razorex <individuel@free.fr>
2022-07-25 18:39:33 +0200

    Update sibnet.py

--- plugin.video.vstream/resources/hosters/sibnet.py
+++ plugin.video.vstream/resources/hosters/sibnet.py
@@ -1,15 +1,13 @@
-#-*- coding: utf-8 -*-
-#Vstream https://github.com/Kodi-vStream/venom-xbmc-addons
-#https://video.sibnet.ru/shell.php?videoid=xxxxxx
+# -*- coding: utf-8 -*-
+# vStream https://github.com/Kodi-vStream/venom-xbmc-addons
+# https://video.sibnet.ru/shell.php?videoid=xxxxxx
 
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.hosters.hoster import iHoster
 from resources.lib.parser import cParser
 
-#from resources.lib.comaddon import #,VSlog
-
-
 UA = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:72.0) Gecko/20100101 Firefox/72.0'
+
 
 class cHoster(iHoster):
 
@@ -24,7 +22,7 @@ class cHoster(iHoster):
         urlmain = 'https://video.sibnet.ru'
         oRequestHandler = cRequestHandler(self._url)
         oRequestHandler.addHeaderEntry('User-Agent', UA)
-        oRequestHandler.addHeaderEntry('Referer', urlmain + '/' )
+        oRequestHandler.addHeaderEntry('Referer', urlmain + '/')
         sHtmlContent = oRequestHandler.request()
 
         oParser = cParser()
