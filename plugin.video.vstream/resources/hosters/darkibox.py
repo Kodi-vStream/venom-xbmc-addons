0cb9402b11f64f80fa47ba94192ba91e1f9261b9
sizanic <47307622+sizanic@users.noreply.github.com>
2023-10-31 03:32:02 +0100

    DARKIBOX - Lecture de liens premiums

--- plugin.video.vstream/resources/hosters/darkibox.py
+++ plugin.video.vstream/resources/hosters/darkibox.py
@@ -34,6 +34,9 @@ class cHoster(iHoster):
         file_code = self._url.split('/')[-1].split('.')[0]
         apiUrl = 'https://darkibox.com/api/file/direct_link?key=%s&file_code=%s' % (sToken, file_code)
 
+#liens streaming hls ne se lancent pas
+#apiUrl = 'https://darkibox.com/api/file/direct_link?key=%s&file_code=%s&hls=1' % (sToken, file_code)
+
         oRequestHandler = cRequestHandler(apiUrl)
         sHtmlContent = oRequestHandler.request()
         content = json.loads(sHtmlContent)
@@ -46,22 +49,27 @@ class cHoster(iHoster):
         
         # plusieurs qualités
         links = content['versions']
-        if len(links)>1:
-            qua=[]
-            url=[]
+        qua=[]
+        url=[]
 
-            for link in links:
-                if link['name'] == 'o':
-                    qua.append('Original [%.1f Go]' % (float(link['size'])/1073741824))
-                    url.append(link['url'])
-                elif link['name'] == 'x':
-                    qua.append('Autre [%.1f Go]' % (float(link['size'])/1073741824))
-                    url.append(link['url'])
+        for link in links:
+            if link['name'] == 'o':
+                qua.append('Original [%.1f Go]' % (float(link['size'])/1073741824))
+                url.append(link['url'])
+            elif link['name'] == 'x':
+                qua.append('Autre [%.1f Go]' % (float(link['size'])/1073741824))
+                url.append(link['url'])
+        
+        # if 'hls_direct' in content:
+        #     qua.append('Streaming')
+        #     url.append(content['hls_direct'])
 
+
+        if len(url)>1:
             #Afichage du tableau
             api_call = dialog().VSselectqual(qua, url)
         else:
-            api_call = links[0]['url']
+            api_call = url[0]
         return True, api_call
 
 
