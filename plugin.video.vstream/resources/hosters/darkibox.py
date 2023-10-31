295e8c58bb86abbbd613fdc1fd89b6b122a622ce
sizanic <47307622+sizanic@users.noreply.github.com>
2023-10-31 02:58:38 +0100

    DARKIBOX - Lecture de liens premiums

--- plugin.video.vstream/resources/hosters/darkibox.py
+++ plugin.video.vstream/resources/hosters/darkibox.py
@@ -3,8 +3,11 @@
 # http://cloudvid.co/embed-xxxx.html
 # https://clipwatching.com/embed-xxx.html
 
+import json
 from resources.hosters.hoster import iHoster
+from resources.lib.comaddon import dialog
 from resources.lib.handler.requestHandler import cRequestHandler
+from resources.lib.handler.premiumHandler import cPremiumHandler
 from resources.lib.parser import cParser
 
 
@@ -18,6 +21,49 @@ class cHoster(iHoster):
 
     def setUrl(self, url):
         self._url = str(url)
+
+
+    # avec un compte
+    def getMediaLink(self):
+        oPremiumHandler = cPremiumHandler('darkibox')
+        sToken = oPremiumHandler.getToken()
+        
+        if not sToken:
+            return self._getMediaLinkForGuest()
+        
+        file_code = self._url.split('/')[-1].split('.')[0]
+        apiUrl = 'https://darkibox.com/api/file/direct_link?key=%s&file_code=%s' % (sToken, file_code)
+
+        oRequestHandler = cRequestHandler(apiUrl)
+        sHtmlContent = oRequestHandler.request()
+        content = json.loads(sHtmlContent)
+        if content['status'] != 200:
+            return self._getMediaLinkForGuest()
+        
+        content = content['result']
+        if not content:
+            return self._getMediaLinkForGuest()
+        
+        # plusieurs qualités
+        links = content['versions']
+        if len(links)>1:
+            qua=[]
+            url=[]
+
+            for link in links:
+                if link['name'] == 'o':
+                    qua.append('Original [%.1f Go]' % (float(link['size'])/1073741824))
+                    url.append(link['url'])
+                elif link['name'] == 'x':
+                    qua.append('Autre [%.1f Go]' % (float(link['size'])/1073741824))
+                    url.append(link['url'])
+
+            #Afichage du tableau
+            api_call = dialog().VSselectqual(qua, url)
+        else:
+            api_call = links[0]['url']
+        return True, api_call
+
 
     def _getMediaLinkForGuest(self, api_call=None):
 
@@ -37,7 +83,7 @@ class cHoster(iHoster):
         sPattern = 'sources: *\[{src: "([^"]+)"'#, *type: "video/mp4"'
         aResult = oParser.parse(sHtmlContent, sPattern)
 
-        if aResult[0] is True:
-            api_call = aResult[1][0]
+        if aResult[0]:
+            return True, aResult[1][0]
 
-        return api_call != None, api_call
+        return False, False
