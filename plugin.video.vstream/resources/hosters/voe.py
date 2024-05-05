d645f480685954b0879792edda6bb65187ce77d0
sizanic <47307622+sizanic@users.noreply.github.com>
2024-05-05 02:59:10 +0200

    voe - support nouveau format

--- plugin.video.vstream/resources/hosters/voe.py
+++ plugin.video.vstream/resources/hosters/voe.py
@@ -11,20 +11,30 @@ class cHoster(iHoster):
         iHoster.__init__(self, 'voe', 'Voe')
 
     def _getMediaLinkForGuest(self):
+        api_call = ''
+        oParser = cParser()
+        
         oRequest = cRequestHandler(self._url)
         sHtmlContent = oRequest.request()
-
-        api_call = ''
-
-        oParser = cParser()
+        
         sPattern = '["\']hls["\']:\s*["\']([^"\']+)["\']'
         aResult = oParser.parse(sHtmlContent, sPattern)
-        aResult1 = base64.b64decode(aResult[1][0])
-
-        if aResult1:
-
-            api_call = aResult1.decode("utf-8")
-
+        if aResult[0]:
+            aResult1 = base64.b64decode(aResult[1][0])
+            if aResult1:
+                api_call = aResult1.decode("utf-8")
+        else:
+            sPattern = " let .+?= '([^']+)"
+            aResult = oParser.parse(sHtmlContent, sPattern)
+            if aResult[0]:
+                aResult1 = base64.b64decode(aResult[1][0])
+                aResult1 = aResult1[::-1]
+                if aResult1:
+                    sPattern = '"file":"([^"]+)"'
+                    aResult = oParser.parse(aResult1, sPattern)
+                    if aResult[0]:
+                        api_call = aResult[1][0].replace('\\','')
+        
         if api_call:
             return True, api_call
 
