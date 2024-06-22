4c8280291bca0c5d7f8bcd62ab6c551e061b310d
sizanic <47307622+sizanic@users.noreply.github.com>
2024-06-22 21:47:50 +0200

    livetv - resolution de liens

--- plugin.video.vstream/resources/sites/livetv.py
+++ plugin.video.vstream/resources/sites/livetv.py
@@ -1413,15 +1413,17 @@ def getHosterIframe(url, referer):
     if aResult:
         url = aResult[0]
         if '.m3u8' in url:
-            return url
+            return url + '|referer=' + referer
 
     sPattern = '[^/]source.+?["\'](https.+?)["\']'
     aResult = re.findall(sPattern, sHtmlContent)
     if aResult:
         oRequestHandler = cRequestHandler(aResult[0])
+        if referer:
+            oRequestHandler.addHeaderEntry('Referer', referer)
         oRequestHandler.request()
         sHosterUrl = oRequestHandler.getRealUrl()
-#        sHosterUrl = sHosterUrl.replace('index', 'mono')
+#        sHosterUrl = sHosterUrl.replace('playlist.m3u8', 'tracks-v1a1/mono.m3u8')
         return sHosterUrl + '|referer=' + referer
 
     sPattern = 'file: *["\'](https.+?\.m3u8)["\']'
