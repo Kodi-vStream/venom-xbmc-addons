f6a936d88a8747610b996086120398fac452b49a
sizanic <47307622+sizanic@users.noreply.github.com>
2024-12-09 02:16:55 +0100

    Nettoyage

--- plugin.video.vstream/resources/hosters/streamhide.py
+++ plugin.video.vstream/resources/hosters/streamhide.py
@@ -2,12 +2,9 @@
 # vStream https://github.com/Kodi-vStream/venom-xbmc-addons
 
 from resources.hosters.hoster import iHoster
-# from resources.lib.comaddon import VSlog
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.packer import cPacker
 from resources.lib.parser import cParser
-
-UA = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0'
 
 
 class cHoster(iHoster):
@@ -15,21 +12,16 @@ class cHoster(iHoster):
         iHoster.__init__(self, 'streamhide', 'StreamHide')
 
     def _getMediaLinkForGuest(self):
+        oParser = cParser()
         oRequest = cRequestHandler(self._url)
         sHtmlContent = oRequest.request()
-
-        oParser = cParser()
         sPattern = '(eval\(function\(p,a,c,k,e(?:.|\s)+?\))<\/script>'
         aResult = oParser.parse(sHtmlContent, sPattern)
-        if aResult[0] is True:
+        if aResult[0]:
             sHtmlContent = cPacker().unpack(aResult[1][0])
-
-        # VSlog(sHtmlContent)
-
-        sPattern = '{file:"([^"]+)"}]'
-        aResult = oParser.parse(sHtmlContent, sPattern)
-        if aResult[0] is True:
-            api_call = aResult[1][0]
-            return True, api_call
+            sPattern = '{file:"([^"]+)"}]'
+            aResult = oParser.parse(sHtmlContent, sPattern)
+            if aResult[0]:
+                return True, aResult[1][0]
 
         return False, False
