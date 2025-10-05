f9824645bc38703040b0e3b7c2821c1846bbf1cd
sizanic <47307622+sizanic@users.noreply.github.com>
2025-10-06 00:15:24 +0200

    Supporter plus de hosters

--- plugin.video.vstream/resources/hosters/sendvid.py
+++ plugin.video.vstream/resources/hosters/sendvid.py
@@ -1,32 +1,28 @@
 # -*- coding: utf-8 -*-
 # https://github.com/Kodi-vStream/venom-xbmc-addons
-
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
-from resources.lib.comaddon import dialog
-import json
+
 
 class cHoster(iHoster):
 
     def __init__(self):
-        iHoster.__init__(self, 'sendvid', 'Sendvid')
+        iHoster.__init__(self, 'sendvid', 'SendVid')
 
     def _getMediaLinkForGuest(self):
-        
-        api_call = False
-
         oRequest = cRequestHandler(self._url)
         sHtmlContent = oRequest.request()
-
         oParser = cParser()
+        
+        sPattern = 'og:video" content="(.+?)"'
+        aResult = oParser.parse(sHtmlContent, sPattern)
+        if aResult[0]:
+            return True, aResult[1][0]
+
         sPattern = '<source src="([^"]+)"'
         aResult = oParser.parse(sHtmlContent, sPattern)
-
         if aResult[0]:
-            api_call = aResult[1][0]
-
-        if api_call:
-            return True, api_call
+            return True, aResult[1][0]
 
         return False, False
