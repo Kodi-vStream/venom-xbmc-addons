85ed2e32bdc29f26ee4f7eb7bb2fd78447e2bad4
sizanic <47307622+sizanic@users.noreply.github.com>
2025-11-01 15:57:27 +0100

    TMDB : Correction pour la recherche d'acteurs

--- plugin.video.vstream/resources/sites/themoviedb_org.py
+++ plugin.video.vstream/resources/sites/themoviedb_org.py
@@ -1203,7 +1203,7 @@ def searchActors(sSearch=''):
         actorFamous = []
         for actor in result['results']:
             # Ne garder que les acteurs
-            if actor['known_for_department'] != 'Acting':
+            if actor.get('known_for_department', None) != 'Acting':
                 continue
             
             # Ne pas garder garder les éléments non-genrés
