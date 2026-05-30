3c366d723f9ae2dd67ac2b75a5de0efd4e3205cd
TmpName <gihubtmp@yopmail.com>
2026-05-30 14:28:57 +0000

    Update uqload.py

--- plugin.video.vstream/resources/hosters/uqload.py
+++ plugin.video.vstream/resources/hosters/uqload.py
@@ -4,6 +4,9 @@
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
+from resources.lib.comaddon import VSlog
+from resources.lib.packer import cPacker
+
 UA = 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
 
 
@@ -24,6 +27,19 @@ class cHoster(iHoster):
         aResult = oParser.parse(sHtmlContent, sPattern1)
         if aResult[0] is True:
             api_call = aResult[1][0]
+        else:
+            sPattern = '(eval\(function\(p,a,c,k,e(?:.|\s)+?\)\))<'
+            aResult = oParser.parse(sHtmlContent, sPattern)
+            if aResult[0] is True:
+                sUnpacked = cPacker().unpack(aResult[1][0])
+                sHtmlContent = sUnpacked
+                
+                sPattern1 = 'file:"([^"]+)"'
+                aResult = oParser.parse(sHtmlContent, sPattern1)
+                if aResult[0] is True:
+                    for url in aResult[1]:
+                        if "mp4" in url or "m3u8" in url:
+                            api_call = aResult[1][0]
 
         if api_call:
             return True, api_call + '|User-Agent=' + UA + '&Referer=' + self._url
