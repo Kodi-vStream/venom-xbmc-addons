e2f5beb4359495ce3981328983db0ba37c5c3e54
TmpName <gihubtmp@yopmail.com>
2026-06-06 13:06:16 +0000

    Update uqload.py

--- plugin.video.vstream/resources/hosters/uqload.py
+++ plugin.video.vstream/resources/hosters/uqload.py
@@ -4,6 +4,7 @@
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
+#from resources.lib.comaddon import VSlog
 from resources.lib.packer import cPacker
 
 UA = 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
@@ -14,12 +15,18 @@ class cHoster(iHoster):
     def __init__(self):
         iHoster.__init__(self, 'uqload', 'Uqload')
 
+    def __getHost(self, url):
+        parts = url.split('//', 1)
+        host = parts[0] + '//' + parts[1].split('/', 1)[0]
+        return host
+
     def _getMediaLinkForGuest(self):
         api_call = ''
         oParser = cParser()
 
         oRequest = cRequestHandler(self._url)
         sHtmlContent = oRequest.request()
+        url2 = self.__getHost(oRequest.getRealUrl()) + '/'
 
         sPattern1 = 'sources.+?"([^"]+mp4)"'
 
@@ -30,6 +37,7 @@ class cHoster(iHoster):
             sPattern = '(eval\(function\(p,a,c,k,e(?:.|\s)+?\)\))<'
             aResult = oParser.parse(sHtmlContent, sPattern)
             if aResult[0] is True:
+
                 sUnpacked = cPacker().unpack(aResult[1][0])
                 sHtmlContent = sUnpacked
                 
@@ -41,6 +49,6 @@ class cHoster(iHoster):
                             api_call = aResult[1][0]
 
         if api_call:
-            return True, api_call + '|User-Agent=' + UA + '&Referer=' + self._url
+            return True, api_call + '|User-Agent=' + UA + '&Referer=' + url2
 
         return False, False
