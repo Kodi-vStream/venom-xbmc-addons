66ed6ec58027a7a43aa767b9531b9880a0238e8f
TmpName <gihubtmp@yopmail.com>
2026-06-09 19:33:33 +0000

    Update filelions.py

--- plugin.video.vstream/resources/hosters/filelions.py
+++ plugin.video.vstream/resources/hosters/filelions.py
@@ -1,45 +1,76 @@
-# coding: utf-8
-
+# -*- coding: utf-8 -*-
+# vStream https://github.com/Kodi-vStream/venom-xbmc-addons
 from resources.lib.handler.requestHandler import cRequestHandler
 from resources.lib.parser import cParser
 from resources.hosters.hoster import iHoster
-#from resources.lib.comaddon import VSlog
+from resources.lib.util import urlEncode
 from resources.lib.packer import cPacker
+from resources.hosters.hoster import iHoster
+import urllib.parse
+import ast  
 
+UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
 
 class cHoster(iHoster):
-
     def __init__(self):
         iHoster.__init__(self, 'filelions', 'FileLions')
 
     def _getMediaLinkForGuest(self):
-        oRequest = cRequestHandler(self._url)
-        sHtmlContent = oRequest.request()
-
-        api_call = ''
-
         oParser = cParser()
         
-        sPattern = '(\s*eval\s*\(\s*function\(p,a,c,k,e(?:.|\s)+?)<\/script>'
+        sBaseUrl = '/'.join(self._url.split('/')[:3]) + '/'
+        sOrigin = sBaseUrl.rstrip('/')
+        oRequest = cRequestHandler(self._url)
+        oRequest.addHeaderEntry('User-Agent', UA)
+        oRequest.addHeaderEntry('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
+        oRequest.addHeaderEntry('Accept-Language', 'fr-FR,fr;q=0.9')
+        oRequest.addHeaderEntry('Sec-Fetch-Dest', 'document')
+        oRequest.addHeaderEntry('Sec-Fetch-Mode', 'navigate')
+        oRequest.addHeaderEntry('Sec-Fetch-Site', 'none')
+        oRequest.addHeaderEntry('Sec-Fetch-User', '?1')
+        sHtmlContent = oRequest.request()
+
+        cookies = oRequest.GetCookies()
+        write_debug("->  Cockies sauvegarde avec succes : " + str(cookies))
+        sPattern = 'sources:\\s*\\[\\{file:\\s*["\']([^"\']+\\.m3u8[^"\']*)["\']'
         aResult = oParser.parse(sHtmlContent, sPattern)
 
-        if aResult[0] is True:
-            sHtmlContent = cPacker().unpack(aResult[1][0])
-        
-        sPattern = 'sources:\s*\[{file:\s*["\']([^"\']+)'
-        aResult = oParser.parse(sHtmlContent, sPattern)
-
-        if aResult[0] is True:
-            api_call = aResult[1][0]
-        else:
-            #Deuxieme essais
-            sPattern = '["\']hls[24]["\']:\s*["\']([^"\']+)'
+        if not aResult[0] is True:
+            sPattern = '(eval\(function\(p,a,c,k,e(?:.|\s)+?\)\)\s*)<\/script>'
             aResult = oParser.parse(sHtmlContent, sPattern)
-
+            write_debug("-> HTML sauvegarde avec succes : " + str(sHtmlContent))
             if aResult[0] is True:
-                api_call = aResult[1][0]
+                sHtmlContent2 = cPacker().unpack(aResult[1][0])
+                write_debug("-> sHtmlContent2 sauvegarder avec succes")
+                
+                sPattern = '"hls2":"([^"]+)"'
+                aResult = oParser.parse(sHtmlContent2, sPattern)
+                
+                if not aResult[0] is True:
+                    write_debug("-> hls2 absent, bascule sur le plan B (hls4)...")
+                    sPattern = '"hls4":"([^"]+)"'
+                    aResult = oParser.parse(sHtmlContent2, sPattern)
+                    
+                    if aResult[0] is True:
+                        api_call_raw = aResult[1][0]
+                        api_call = sOrigin + api_call_raw
+                        write_debug("-> Réserve hls4 reconstruite avec succès : " + str(api_call))
+                    else:
+                        api_call = None
+                else:
+                    api_call = aResult[1][0]
 
         if api_call:
-            return True, api_call
+            write_debug("-> api_call final envoyé à Kodi : " + str(api_call))
+            headers = {
+                'User-Agent': UA,
+                'Referer': self._url,
+                'Origin': sOrigin,
+                'Accept': '*/*',
+                'Accept-Language': 'fr-FR,fr;q=0.9',
+                'Cookie': cookies  
+            }
+
+            return True, api_call + '|' + urlEncode(headers) + '&verifypeer=false'
 
         return False, False
