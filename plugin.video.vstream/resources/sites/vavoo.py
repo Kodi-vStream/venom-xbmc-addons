dbea2766b582ece1e08f5d71db38a05f2968d2da
sizanic <47307622+sizanic@users.noreply.github.com>
2026-09-22 01:32:27 +0200

    Nettoyage

--- plugin.video.vstream/resources/sites/vavoo.py
+++ plugin.video.vstream/resources/sites/vavoo.py
@@ -20,7 +20,7 @@ from resources.lib.comaddon import VSlog
 
 
 SITE_IDENTIFIER = 'vavoo'
-SITE_NAME = 'Vavoo TV'
+SITE_NAME = 'Vavoo'
 SITE_DESC = 'Chaines TV de vavoo'
 
 URL_MAIN = siteManager().getUrlMain(SITE_IDENTIFIER)
